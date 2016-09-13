8990937a0c7c6890abd8d9b7acd9551c4f84e20a
Louis Pilfold <louis@lpil.uk>
2016-09-13 14:36:54 +0100

    Wobpack compiles JS

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+	entry: {
+		'publish-service': './publish-service/handlers.js',
+	},
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: "[name]/index.js",
+	},
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel'],
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+
+      },
+    ],
+  },
+  target: 'node',
+  externals: [
+    'aws-sdk'
+  ],
+  devtool: 'source-map',
+  plugins: [
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        unused: true,
+        dead_code: true,
+        warnings: false,
+        drop_debugger: true
+      },
+    }),
+  ],
+};
