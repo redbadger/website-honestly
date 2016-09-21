2ad18f9c82baaaaa3900460becee94ebc02aefb4
Alex Savin <karismafilms@gmail.com>
2016-09-21 16:42:11 +0100

    Enabling require hook for css modules

--- test/mocha-env.js
+++ test/mocha-env.js
@@ -1,3 +1,23 @@
+import cssHook from 'css-modules-require-hook';
+import { hook as requireHook } from 'node-hook';
+
 const chai = require('chai'); // eslint-disable-line import/no-extraneous-dependencies
 
 global.expect = chai.expect; // Register test tools globally
+
+/* Polyfill CSS Modules */
+cssHook({
+  generateScopedName: '[name]__[hash:base64:5]',
+});
+
+/* Polyfill File Loader */
+
+const fileTypes = ['png', 'jpg', 'jpeg', 'eot', 'ttf', 'woff', 'svg'];
+
+const loadAsset = (source, name) => {
+  return JSON.stringify(name);
+};
+
+fileTypes.forEach(ext => {
+  requireHook(`.${ext}`, loadAsset);
+});
