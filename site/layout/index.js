30db698c79436935b042f22829ec34199ab51a96
Zoe Bryant <zoeabryant@gmail.com>
2016-09-26 17:00:28 +0100

    Footer markup, basic styling

--- site/layout/index.js
+++ site/layout/index.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import Header from '../components/header';
+import Footer from '../components/footer';
 import styles from './style.css';
 
 export default function Layout({ children }) {
@@ -8,6 +9,7 @@ export default function Layout({ children }) {
       <Header />
       <h1>Website. Honestly.</h1>
       <div>{children}</div>
+      <Footer />
     </div>
   );
 }
