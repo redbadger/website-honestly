16fa72083cc31e4ba626b4ce7dd7dc760a283de0
Alex Savin <karismafilms@gmail.com>
2016-09-26 17:36:17 +0100

    Mobile navigation

--- site/components/header/index.js
+++ site/components/header/index.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo} />
-      <nav>
+      <nav className={styles.desktopNav}>
         <ul className={styles.nav}>
           <li><a href="/services/">Services</a></li>
           <li><a href="/about-us/">About us</a></li>
@@ -13,6 +13,24 @@ const Header = () => {
           <li><a href="/about-us/events/">Events</a></li>
         </ul>
       </nav>
+      <div className={styles.triggerContainer}>
+        <label htmlFor="burger" className={styles.menu}>MENU</label>
+      </div>
+      <input type="checkbox" className={styles.trigger} id="burger" />
+      <div className={styles.overlay}>
+        <label htmlFor="burger" className={styles.menuCloseButton}>Close</label>
+        <nav className={styles.navContainer}>
+          <ul className={styles.mobileNav}>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about-us/">About us</a></li>
+            <li><a href="/services/">Services</a></li>
+            <li><a href="/blog/">Blog</a></li>
+            <li><a href="/about-us/events/">Events</a></li>
+            <li><a href="/about-us/join-us/">Jobs</a></li>
+            <li><a href="/about-us/contact-us/">Contact us</a></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
