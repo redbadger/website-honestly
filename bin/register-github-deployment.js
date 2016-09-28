94883e46ebb6c6abbaad2d43bcc9c1668da03afa
Louis Pilfold <louis@lpil.uk>
2016-09-28 15:34:14 +0100

    GitHub can be alerted to deployments

---
+++ bin/register-github-deployment.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+
+const fetch = require('node-fetch');
+
+const ref = process.argv[2];
+
+if (!ref) {
+  console.log('Ref not passed as argv');
+  process.exit(1);
+}
+
+const env = key => {
+  const value = process.env[key];
+  if (value) { return value; }
+  console.error(`env var ${key} not set`);
+  process.exit(1);
+};
+
+const gitHubUsername = env('GITHUB_USERNAME');
+const gitHubToken = env('GITHUB_TOKEN');
+const targetUrl = `http://rb-website-honestly--louis.s3-website-eu-west-1.amazonaws.com/${ref}/`;
+const environmentUrl = targetUrl;
+
+const creds = `${gitHubUsername}:${gitHubToken}`;
+const auth = new Buffer(creds).toString('base64');
+const headers = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+  Authorization: `Basic ${auth}`,
+};
+
+const deploymentsUrl =
+  'https://api.github.com/repos/redbadger/website-honestly/deployments';
+
+const assertStatusOK = response => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
+};
+
+const registerDeployment = () =>
+  fetch(deploymentsUrl, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      auto_merge: false, ref,
+    }),
+  });
+
+const addDeploymentLink = args =>
+  fetch(`${deploymentsUrl}/${args.id}/statuses`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      target_url: targetUrl,
+      environment_url: environmentUrl,
+      state: 'success',
+    }),
+  });
+
+const parseJson = blob => blob.json();
+
+registerDeployment()
+  .then(parseJson)
+  .then(assertStatusOK)
+  .then(addDeploymentLink)
+  .then(parseJson)
+  .then(assertStatusOK)
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
