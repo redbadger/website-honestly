d9ea5d95326c75c27992e719a8a3be8b224539eb
Anna Doubkova <anna.doubkova@red-badger.com>
2016-09-30 14:13:11 +0100

    Correct badger image

--- site/layout/index.js
+++ site/layout/index.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 import Header from '../components/header';
-// import Footer from '../components/footer';
+import Footer from '../components/footer';
 import styles from './style.css';
 
 export default function Layout({ children }) {
@@ -9,6 +9,7 @@ export default function Layout({ children }) {
       <Header />
       <h1>Website. Honestly.</h1>
       <div>{children}</div>
+      <Footer />
     </div>
   );
 }
