46ac90f352effd3dd8c9cf25e6347e0ab052e109
Alex Savin <karismafilms@gmail.com>
2016-10-03 15:23:59 +0100

    Homepage top slice animation

--- site/components/homepage-top-slice/index.js
+++ site/components/homepage-top-slice/index.js
@@ -1,12 +1,15 @@
+import classnames from 'classnames/bind';
 import React from 'react';
 import styles from './style.css';
+
+const cx = classnames.bind(styles);
 
 const HomepageTopSlice = () => {
   return (
     <section className={styles.homepageTopSlice}>
       <div className={styles.sliceContainer}>
-        <h1 className={styles.badgerSlogan}>Let’s make things better.</h1>
-        <p className={styles.sloganDescription}>
+        <h1 className={cx('badgerSlogan', 'fadeInUp')}>Let’s make things better.</h1>
+        <p className={cx('sloganDescription', 'fadeInUp')}>
           We work with you to deliver digital products that
           make a difference to people.
         </p>
