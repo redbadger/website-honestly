29dbff33126fb3800f4fa8dc556d165cf5a62eb8
Alex Savin <karismafilms@gmail.com>
2016-10-03 14:24:40 +0100

    Homepage top slice big screen styles

--- site/components/homepage-top-slice/index.js
+++ site/components/homepage-top-slice/index.js
@@ -4,11 +4,13 @@ import styles from './style.css';
 const HomepageTopSlice = () => {
   return (
     <section className={styles.homepageTopSlice}>
-      <h1 className={styles.badgerSlogan}>Let's make things better.</h1>
-      <p className={styles.sloganDescription}>
-        We work with you to deliver digital products that
-        make a difference to people.
-      </p>
+      <div className={styles.sliceContainer}>
+        <h1 className={styles.badgerSlogan}>Let’s make things better.</h1>
+        <p className={styles.sloganDescription}>
+          We work with you to deliver digital products that
+          make a difference to people.
+        </p>
+      </div>
     </section>
   );
 };
