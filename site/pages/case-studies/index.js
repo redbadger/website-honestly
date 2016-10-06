872e1e3272abcd3c8316e4dcfe01dd020d8f4f92
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-10-06 16:11:32 +0100

    Laid foundation for new case studies page

---
+++ site/pages/case-studies/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default function caseStudies() {
+  return (
+    <div>
+      <div>
+        <h2>The proof is in the pudding.</h2>
+        <h3>Read some of our case studies.</h3>
+      </div>
+      <div>
+        <h2>Fortnum & Mason</h2>
+        <h2>Elegant e-commerce in eight months</h2>
+        <h2>
+          Improving online and mobile conversion rates on the new fortnumandmason.com
+          site with great customer experience and innovative tech.
+        </h2>
+      </div>
+      <div>
+        <h2>Camden Market</h2>
+        <h2>Taking steps towards a digital future</h2>
+        <h2>
+          Built in just ten weeks, Camdenmarket.com relaunched in May 2016 to drive
+          more footfall from Londoners to the physical market by
+          showcasing the eclectic range of goods, food and events.
+        </h2>
+      </div>
+      <div>
+        <h2>Financial Times</h2>
+        <h2>Lasting change for a media giant</h2>
+        <h2>Improving online and mobile conversion rates on the new fortnumandmason.com
+          site with great customer experience and innovative tech.
+        </h2>
+      </div>
+    </div>
+  );
+}
