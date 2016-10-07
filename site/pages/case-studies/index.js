8fc1ccc505d765fade320e9ec0656e5db03b9ea0
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-10-07 15:48:45 +0100

    Added desktop view for the case studies page

--- site/pages/case-studies/index.js
+++ site/pages/case-studies/index.js
@@ -17,32 +17,44 @@ export default function caseStudies() {
         <h2>Read some of our case studies.</h2>
       </div>
       <div className={styles.caseStudyContainer}>
-        <img alt="The logo of Fortnum & Masons" src={fortnumJPG} />
-        <h2 className={styles.caseStudyCompany}>Fortnum & Mason</h2>
-        <h2 className={styles.caseStudyTitle}>Elegant e-commerce in eight months</h2>
-        <h2 className={styles.caseStudyDescription}>
-          Improving online and mobile conversion rates on the new fortnumandmason.com
-          site with great customer experience and innovative tech.
-        </h2>
+        <div className={styles.imageContainer}>
+          <img alt="The logo of Fortnum & Masons" src={fortnumJPG} />
+        </div>
+        <div className={styles.caseStudyTextContainer}>
+          <h2 className={styles.caseStudyCompany}>Fortnum & Mason</h2>
+          <h2 className={styles.caseStudyTitle}>Elegant e-commerce in eight months</h2>
+          <h2 className={styles.caseStudyDescription}>
+            Improving online and mobile conversion rates on the new fortnumandmason.com
+            site with great customer experience and innovative tech.
+          </h2>
+        </div>
       </div>
       <div className={cx('caseStudyContainer', 'inverse')}>
-        <img alt="The Camden Market App" src={camdenJPG} />
-        <h2 className={styles.caseStudyCompany}>Camden Market</h2>
-        <h2 className={styles.caseStudyTitle}>Taking steps towards a digital future</h2>
-        <h2 className={styles.caseStudyDescription}>
-          Built in just ten weeks, Camdenmarket.com relaunched in May 2016 to drive
-          more footfall from Londoners to the physical market by
-          showcasing the eclectic range of goods, food and events.
-        </h2>
+        <div className={cx('imageContainer', 'camden')}>
+          <img alt="The Camden Market App" src={camdenJPG} />
+        </div>
+        <div className={styles.caseStudyTextContainer}>
+          <h2 className={styles.caseStudyCompany}>Camden Market</h2>
+          <h2 className={styles.caseStudyTitle}>Taking steps towards a digital future</h2>
+          <h2 className={styles.caseStudyDescription}>
+            Built in just ten weeks, Camdenmarket.com relaunched in May 2016 to drive
+            more footfall from Londoners to the physical market by
+            showcasing the eclectic range of goods, food and events.
+          </h2>
+        </div>
       </div>
       <div className={styles.caseStudyContainer}>
-        <img alt="Financial Times website on a tablet" src={financialTimesJPG} />
-        <h2 className={styles.caseStudyCompany}>Financial Times</h2>
-        <h2 className={styles.caseStudyTitle}>Lasting change for a media giant</h2>
-        <h2 className={styles.caseStudyDescription}>
-          Improving online and mobile conversion rates on the new fortnumandmason.com
-          site with great customer experience and innovative tech.
-        </h2>
+        <div className={styles.imageContainer}>
+          <img alt="Financial Times website on a tablet" src={financialTimesJPG} />
+        </div>
+        <div className={styles.caseStudyTextContainer}>
+          <h2 className={styles.caseStudyCompany}>Financial Times</h2>
+          <h2 className={styles.caseStudyTitle}>Lasting change for a media giant</h2>
+          <h2 className={styles.caseStudyDescription}>
+            Improving online and mobile conversion rates on the new fortnumandmason.com
+            site with great customer experience and innovative tech.
+          </h2>
+        </div>
       </div>
     </div>
   );
