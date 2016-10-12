91026592fbdcfb03ab1d86c8b15cc609be59a8a5
Zoe Bryant <zoeabryant@gmail.com>
2016-10-12 15:04:19 +0100

    Steps to include success screen

---
+++ site/pages/home/contact-us-slice/success.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './style.css';
+
+const Success = () => (
+  <section className={styles.successContainer}>
+    <h2 className={styles.heading}>
+      Success
+    </h2>
+  </section>
+);
+
+export default Success;
