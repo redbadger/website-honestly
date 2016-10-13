86bc32739a0bdb873bd5238c49d43909dcdc5cf7
Anna Doubkova <anna.doubkova@red-badger.com>
2016-10-13 14:03:05 +0100

    Style noscript area

--- site/pages/home/newsletter-slice/noscript.js
+++ site/pages/home/newsletter-slice/noscript.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from './style.css';
 
 import BeforeSignUpTitles from './before-sign-up-titles';
 
 const NoScript = () => (
-  <noscript>
+  <noscript className={styles.newsletter}>
     <BeforeSignUpTitles />
-    Link to MailChimp
+    <a href="http://eepurl.com/bibY5P" className={styles.link}>
+      Sing up through MailChimp
+    </a>
   </noscript>
 );
 
