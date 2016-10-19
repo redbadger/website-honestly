abbaabae8bd99eda38d3fd3946b7afb26a67977d
Zoe Bryant <zoeabryant@gmail.com>
2016-10-19 12:32:02 +0100

    Contact Us unit tests  - start

---
+++ site/pages/home/contact-us-slice/test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ContactUs from '.';
+import Form from './form/';
+import Success from './success/';
+
+describe('Contact Us Slice', () => {
+  it('shows pre-signup content if the form has not been submitted yet - default state', () => {
+    const wrapper = shallow(<ContactUs />);
+    expect(wrapper.find(Form)).to.have.length(1);
+  });
+
+  it('shows success content if the form has been submitted successfully', () => {
+    const wrapper = shallow(<ContactUs />);
+    wrapper.setState({
+      success: true,
+    });
+    expect(wrapper.find(Success)).to.have.length(1);
+  });
+
+  describe('submitForm', () => {
+    it('catches fatal errors, and changes the state to fatalError = true');
+    it('POSTs to our contact us endpoint with JSONified formData with the right headers');
+    it('changes the state after a successful post');
+  });
+});
