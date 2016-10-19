50fcc1f051b5b9c22b2d65f7970a5561b6787c6d
Alex Savin <karismafilms@gmail.com>
2016-10-19 12:23:29 +0100

    Triangle slice list styling

--- site/pages/case-studies/triangle-slice/index.js
+++ site/pages/case-studies/triangle-slice/index.js
@@ -13,23 +13,19 @@ const triangleSlice = () => {
       <h2 className={styles.triangleHeader}>How do we get to the right thing?</h2>
       <ol className={styles.rightThingsList}>
         <li>
-          <div>
-            <h3 className={styles.listNumber}>1</h3>
-            <div>
-              <h3 className={styles.triangleHeader}>Vision</h3>
-              <p className={styles.rightThingDescription}>
-                A strong, aspirational vision is needed to keep
-                the project on track; ensuring all features are
-                continuously getting closer to this end goal.
-              </p>
-            </div>
+          <div className={styles.rightThingText}>
+            <h3 className={styles.triangleThingHeader}>Vision</h3>
+            <p className={styles.rightThingDescription}>
+              A strong, aspirational vision is needed to keep
+              the project on track; ensuring all features are
+              continuously getting closer to this end goal.
+            </p>
           </div>
           <InlineSVG src={triangleTopSVG} />
         </li>
         <li>
-          <h3 className={styles.listNumber}>2</h3>
-          <div>
-            <h3 className={styles.triangleHeader}>Validation</h3>
+          <div className={styles.rightThingText}>
+            <h3 className={styles.triangleThingHeader}>Validation</h3>
             <p className={styles.rightThingDescription}>
               Validating assumptions by collecting data and
               customer research means we can prioritise work in
@@ -40,9 +36,8 @@ const triangleSlice = () => {
           <InlineSVG src={triangleMidSVG} />
         </li>
         <li>
-          <h3 className={styles.listNumber}>3</h3>
-          <div>
-            <h3 className={styles.triangleHeader}>Delivery</h3>
+          <div className={styles.rightThingText}>
+            <h3 className={styles.triangleThingHeader}>Delivery</h3>
             <p className={styles.rightThingDescription}>
               Setting up the project in the right way increases
               efficiency and velocity, allowing the focus to be
