b50a0505e3b60d4d0ce6a834b001e822621be61b
Alex Savin <karismafilms@gmail.com>
2016-10-24 13:55:30 +0100

    Triangle slice large and medium screen layouts

--- site/pages/what-we-do/triangle-slice/index.js
+++ site/pages/what-we-do/triangle-slice/index.js
@@ -5,47 +5,51 @@ import styles from './style.css';
 import triangleTopPNG from './PNG/triangleTop.png';
 import triangleMidPNG from './PNG/triangleMid.png';
 import triangleEndPNG from './PNG/triangleEnd.png';
+import triangleFullPNG from './PNG/triangleFull.png';
 
 const triangleSlice = () => {
   return (
     <section className={styles.triangleSlice}>
       <h2 className={styles.triangleHeader}>How do we get to the right thing?</h2>
-      <ol className={styles.rightThingsList}>
-        <li>
-          <div className={styles.rightThingText}>
-            <h3 className={styles.triangleThingHeader}>Vision</h3>
-            <p className={styles.rightThingDescription}>
-              A strong, aspirational vision is needed to keep
-              the project on track; ensuring all features are
-              continuously getting closer to this end goal.
-            </p>
-          </div>
-          <img className={styles.mobileTriangleImage} alt="Triangle Vision section" src={triangleTopPNG} />
-        </li>
-        <li>
-          <div className={styles.rightThingText}>
-            <h3 className={styles.triangleThingHeader}>Validation</h3>
-            <p className={styles.rightThingDescription}>
-              Validating assumptions by collecting data and
-              customer research means we can prioritise work in
-              the right way; ensuring features with the most
-              customer value are built first.
-            </p>
-          </div>
-          <img className={styles.mobileTriangleImage} alt="Triangle Validation section" src={triangleMidPNG} />
-        </li>
-        <li>
-          <div className={styles.rightThingText}>
-            <h3 className={styles.triangleThingHeader}>Delivery</h3>
-            <p className={styles.rightThingDescription}>
-              Setting up the project in the right way increases
-              efficiency and velocity, allowing the focus to be
-              on feature delivery.
-            </p>
-          </div>
-          <img className={styles.mobileTriangleImage} alt="Triangle Delivery section" src={triangleEndPNG} />
-        </li>
-      </ol>
+      <div className={styles.triangleContainer}>
+        <img className={styles.fullTriangleImage} alt="Triangle of vision, validation and delivery" src={triangleFullPNG} />
+        <ol className={styles.rightThingsList}>
+          <li>
+            <div className={styles.rightThingText}>
+              <h3 className={styles.triangleThingHeader}>Vision</h3>
+              <p className={styles.rightThingDescription}>
+                A strong, aspirational vision is needed to keep
+                the8 project on track; ensuring all features are
+                continuously getting closer to this end goal.
+              </p>
+            </div>
+            <img className={styles.mobileTriangleImage} alt="Triangle Vision section" src={triangleTopPNG} />
+          </li>
+          <li>
+            <div className={styles.rightThingText}>
+              <h3 className={styles.triangleThingHeader}>Validation</h3>
+              <p className={styles.rightThingDescription}>
+                Validating assumptions by collecting data and
+                customer research means we can prioritise work in
+                the right way; ensuring features with the most
+                customer value are built first.
+              </p>
+            </div>
+            <img className={styles.mobileTriangleImage} alt="Triangle Validation section" src={triangleMidPNG} />
+          </li>
+          <li>
+            <div className={styles.rightThingText}>
+              <h3 className={styles.triangleThingHeader}>Delivery</h3>
+              <p className={styles.rightThingDescription}>
+                Setting up the project in the right way increases
+                efficiency and velocity, allowing the focus to be
+                on feature delivery.
+              </p>
+            </div>
+            <img className={styles.mobileTriangleImage} alt="Triangle Delivery section" src={triangleEndPNG} />
+          </li>
+        </ol>
+      </div>
     </section>
   );
 };
