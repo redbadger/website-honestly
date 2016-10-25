6282f32762b17e337ea22165cc3307a59e453743
Louis Pilfold <louis@lpil.uk>
2016-10-25 15:18:30 +0100

    Lean slice desktop

--- site/pages/what-we-do/lean-slice/index.js
+++ site/pages/what-we-do/lean-slice/index.js
@@ -10,40 +10,44 @@ export default function LeanSlice() {
         How do we get to the right thing?
       </h2>
 
-      <div className={styles.leanImageWrapper} >
-        <img role="presentation" className={styles.leanImage} src={leanImg} />
+      <div className={styles.content}>
+        <div className={styles.leanImageWrapper} >
+          <img role="presentation" className={styles.leanImage} src={leanImg} />
+        </div>
+
+        <div className={styles.pointsWrapper}>
+          <p className={styles.point}>
+            <span className={styles.bold}>Visualise the workflow</span> on a
+            physical wall for complete transparency.
+          </p>
+
+          <p className={styles.point}>
+            <span className={styles.bold}>Build a shared understanding</span> by
+            working in the same place as our clients.
+          </p>
+
+          <p className={styles.point}>
+            <span className={styles.bold}>Learn fast</span> by getting to something
+            we can test quickly.
+          </p>
+
+          <p className={styles.point}>
+            <span className={styles.bold}>Minimise waste</span> by using real data
+            to forecast and make decisions.
+          </p>
+
+          <p className={styles.point}>
+            <span className={styles.bold}>Work in cross-functional teams</span> who
+            have the autonomy to make the right decisions.
+          </p>
+
+          <p className={styles.point}>
+            <span className={styles.bold}>Focus on what the users need
+            </span> and deliver improvements incrementally whilst getting continual
+            feedback.
+          </p>
+        </div>
       </div>
-
-      <p className={styles.point}>
-        <span className={styles.bold}>Visualise the workflow</span> on a
-        physical wall for complete transparency.
-      </p>
-
-      <p className={styles.point}>
-        <span className={styles.bold}>Build a shared understanding</span> by
-        working in the same place as our clients.
-      </p>
-
-      <p className={styles.point}>
-        <span className={styles.bold}>Learn fast</span> by getting to something
-        we can test quickly.
-      </p>
-
-      <p className={styles.point}>
-        <span className={styles.bold}>Minimise waste</span> by using real data
-        to forecast and make decisions.
-      </p>
-
-      <p className={styles.point}>
-        <span className={styles.bold}>Work in cross-functional teams</span> who
-        have the autonomy to make the right decisions.
-      </p>
-
-      <p className={styles.point}>
-        <span className={styles.bold}>Focus on what the users need
-        </span> and deliver improvements incrementally whilst getting continual
-        feedback.
-      </p>
     </section>
   );
 }
