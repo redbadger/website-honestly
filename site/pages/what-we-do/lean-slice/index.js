89c639e8137ff02e67ee86e8149a6fa2671d025e
Louis Pilfold <louis@lpil.uk>
2016-10-27 13:42:45 +0100

    Make lean HTML more semantic

--- site/pages/what-we-do/lean-slice/index.js
+++ site/pages/what-we-do/lean-slice/index.js
@@ -17,33 +17,33 @@ export default function LeanSlice() {
 
         <div className={styles.pointsWrapper}>
           <p className={styles.point}>
-            <span className={styles.bold}>Visualise the workflow</span> on a
+            <strong className={styles.bold}>Visualise the workflow</strong> on a
             physical wall for complete transparency.
           </p>
 
           <p className={styles.point}>
-            <span className={styles.bold}>Build a shared understanding</span> by
+            <strong className={styles.bold}>Build a shared understanding</strong> by
             working in the same place as our clients.
           </p>
 
           <p className={styles.point}>
-            <span className={styles.bold}>Learn fast</span> by getting to something
+            <strong className={styles.bold}>Learn fast</strong> by getting to something
             we can test quickly.
           </p>
 
           <p className={styles.point}>
-            <span className={styles.bold}>Minimise waste</span> by using real data
+            <strong className={styles.bold}>Minimise waste</strong> by using real data
             to forecast and make decisions.
           </p>
 
           <p className={styles.point}>
-            <span className={styles.bold}>Work in cross-functional teams</span> who
+            <strong className={styles.bold}>Work in cross-functional teams</strong> who
             have the autonomy to make the right decisions.
           </p>
 
           <p className={styles.point}>
-            <span className={styles.bold}>Focus on what the users need
-            </span> and deliver improvements incrementally whilst getting continual
+            <strong className={styles.bold}>Focus on what the users need
+            </strong> and deliver improvements incrementally whilst getting continual
             feedback.
           </p>
         </div>
