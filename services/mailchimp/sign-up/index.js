a45ce5aa8d0d037738744c19147fe3626fd26d5c
Zoe Bryant <zoeabryant@gmail.com>
2016-11-01 17:20:11 +0000

    Renamed some env variables to be more descriptive, added logging

--- services/mailchimp/sign-up/index.js
+++ services/mailchimp/sign-up/index.js
@@ -1,6 +1,7 @@
 import { mailchimpApi, formatFormInput, formatSignUpResponse } from '../utilities';
 
 export default function signUp(event, cb) {
+  console.log('sign up service event:', event);
   const body = formatFormInput(event);
   return mailchimpApi(
     'https://us6.api.mailchimp.com/3.0/lists/2affe6fb11/members/',
@@ -8,10 +9,12 @@ export default function signUp(event, cb) {
     JSON.stringify(body)
   )
     .then(json => {
+      console.log('sign up service post ok:', json);
       const result = formatSignUpResponse(json, body);
       cb(null, result);
     })
     .catch(err => {
+      console.error('sign up service error:', err, err.stack);
       cb(err);
     });
 }
