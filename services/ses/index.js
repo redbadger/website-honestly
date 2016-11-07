cd9821fca0ef576949f6ffb74622744d21dd76f2
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-11-07 11:41:02 +0000

    Simplified the contact us regex

--- services/ses/index.js
+++ services/ses/index.js
@@ -17,9 +17,9 @@ function filterInternalErrors(error) {
   throw error;
 }
 
-function validateContact(contact) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // eslint-disable-line max-len
-  return re.test(contact);
+export function validateContact(contact) {
+  const regex = /.+@.+\..+/;
+  return regex.test(contact);
 }
 
 function throwValidationError(errors) {
