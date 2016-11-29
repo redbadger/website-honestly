31ea048a33ddfe846f6372ef07a316011d4fc288
Joe Stanton <joe.stanton@red-badger.com>
2016-11-29 12:52:11 +0000

    Workable Feed - Dynamic Content Spike

--- state/index.js
+++ state/index.js
@@ -1,7 +1,11 @@
+import fetch from 'node-fetch';
+import WorkableAPI from '../site/fetchers/workable';
+
 const state = {
   contactUsURL: process.env.CONTACT_US_SERVICE_URL,
 };
 
 module.exports = function getSiteState() {
-  return new Promise(resolve => resolve(state));
+  const workable = new WorkableAPI(fetch, process.env.WORKABLE_API_KEY);
+  return workable.getJobs().then(jobs => ({ jobs, ...state }));
 };
