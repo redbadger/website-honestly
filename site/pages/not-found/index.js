2c466842097fee634932f1fcbf714292344147ec
Tiago Azevedo <tabazevedo@gmail.com>
2016-11-30 14:09:04 +0000

    Extract error page into component for reuse in 500 page

--- site/pages/not-found/index.js
+++ site/pages/not-found/index.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import Link from '../../components/link';
 import styles from './style.css';
-import confusedBadger from './png/confused-badger.png';
+import ErrorPage from '../../components/error-page';
 
 export default function NotFoundPage() {
+  const content = ([
+    'Sorry, the page you’re looking for isn’t here. ',
+    <span>Try our <a className={styles.link} href="/blog">blog</a>, or find out more <a className={styles.link} href="/about-us">about us</a>.</span>,
+  ]);
+
   return (
-    <div className={styles.section}>
-      <div className={styles.sectionContent}>
-        <h1 className={styles.header}>Whaaaaaat?!</h1>
-        <p className={styles.content}>
-          <img alt="confused badger" className={styles.badger} src={confusedBadger} />
-          <span className={styles.copy}>
-            Sorry, the page you’re looking for isn’t here.&nbsp;
-            <span>Try our <a className={styles.link} href="/blog">blog</a>, or find out more <a className={styles.link} href="/about-us">about us</a>.</span>
-          </span>
-          <span className={styles.buttonContainer}>
-            <Link className={styles.button} to="homePage">Go to home page</Link>
-          </span>
-          <span className={styles.clear} />
-        </p>
-      </div>
-    </div>
+    <ErrorPage
+      title="Whaaaaaat!?"
+      content={content}
+      linkText="Go to home page"
+      linkHref="homePage"
+    />
   );
 }
