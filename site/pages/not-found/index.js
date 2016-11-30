8726e2504c1173045cda4a6810e41b4e0b8cd1e7
Tiago Azevedo <tabazevedo@gmail.com>
2016-11-30 13:40:31 +0000

    First pass of responsive 404 design

--- site/pages/not-found/index.js
+++ site/pages/not-found/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Link from '../../components/link';
+import styles from './style.css';
+import confusedBadger from './png/confused-badger.png';
 
 export default function NotFoundPage() {
   return (
-    <div>
-      <h1>Nothing here...</h1>
-      <Link to="homePage">Go home?</Link>
+    <div className={styles.section}>
+      <div className={styles.sectionContent}>
+        <h1 className={styles.header}>Whaaaaaat?!</h1>
+        <p className={styles.content}>
+          <img alt="confused badger" className={styles.badger} src={confusedBadger} />
+          <span className={styles.copy}>
+            Sorry, the page you’re looking for isn’t here.&nbsp;
+            <span>Try our <a className={styles.link} href="/blog">blog</a>, or find out more <a className={styles.link} href="/about-us">about us</a>.</span>
+          </span>
+          <span className={styles.buttonContainer}>
+            <Link className={styles.button} to="homePage">Go to home page</Link>
+          </span>
+          <span className={styles.clear} />
+        </p>
+      </div>
     </div>
   );
 }
