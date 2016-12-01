209d0ee8ab79434efc83af9fb65bde4c4e976751
Joe Stanton <joe.stanton@red-badger.com>
2016-12-01 08:09:13 +0000

    Parallel fetching for state

--- state/index.js
+++ state/index.js
@@ -1,4 +1,6 @@
 import fetch from 'node-fetch';
+import Promise from 'bluebird';
+
 import WorkableAPI from '../site/fetchers/workable';
 import { getEvents } from '../site/fetchers/badger-brain';
 
@@ -6,10 +8,13 @@ const state = {
   contactUsURL: process.env.CONTACT_US_SERVICE_URL
 };
 
+const workable = new WorkableAPI(fetch, process.env.WORKABLE_API_KEY);
+
 const getSiteState = () => {
-  const workable = new WorkableAPI(fetch, process.env.WORKABLE_API_KEY);
-  // return workable.getJobs().then(jobs => ({ jobs, ...state }));
-  return getEvents().then(events => ({events, ...state}));
+  return Promise.props({
+    events: getEvents(),
+    jobs: workable.getJobs()
+  }).then((fetched) => ({ ...fetched, ...state }));
 };
 
 export default getSiteState;
