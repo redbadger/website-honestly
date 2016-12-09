75c320e94da6beea79fce37ac12c89ff1a156937
Graham Mendick <graham.mendick@gmail.com>
2016-12-09 15:37:58 +0000

    Added events to site state

--- state/index.js
+++ state/index.js
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 import { getJobs } from '../site/fetchers/workable';
+import { getEvents } from '../site/fetchers/badger-brain';
 
 const state = {
   contactUsURL: process.env.CONTACT_US_SERVICE_URL,
@@ -10,10 +11,11 @@ const toDict = (array, keyFn) => array.reduce((obj, item) => ({
   [keyFn(item)]: item,
 }), {});
 
-const getSiteState = () => {
-  return getJobs(fetch, process.env.WORKABLE_API_KEY).then(jobs =>
-    ({ jobs, job: toDict(jobs, j => j.slug), ...state })
-  );
-};
+const getSiteState = () => (
+  getJobs(fetch, process.env.WORKABLE_API_KEY)
+    .then(jobs => ({ jobs, job: toDict(jobs, j => j.slug), ...state }))
+    .then(jobState => getEvents().then(events => ({ ...jobState, events }))
+  )
+);
 
 export default getSiteState;
