c14bd9aefcbf0f50f66468d3662ff980c3d1d25e
Graham Mendick <graham.mendick@gmail.com>
2016-12-12 15:33:25 +0000

    Overrode styles.css path in dev environment
    The dev static site didn't like prepending / on styles. So created override in webpack dev and reverted webpack base config

--- webpack.dev.browser.config.js
+++ webpack.dev.browser.config.js
@@ -1,6 +1,7 @@
 const baseConfig = require('./webpack.base.config');
 const webpackMerge = require('webpack-merge').smart;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const devAppConfig = webpackMerge(baseConfig, {
   entry: {
@@ -11,6 +12,10 @@ const devAppConfig = webpackMerge(baseConfig, {
     new HtmlWebpackPlugin({
       template: './site/index.ejs',
     }),
+    new ExtractTextPlugin(
+      '/assets-honestly/styles-[contenthash:base64:5].css',
+      { allChunks: true }
+    ),
   ],
 });
 
