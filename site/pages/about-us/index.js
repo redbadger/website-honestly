d9781a70bf52458bdb59c0c92de457b6054d99e1
Fabio Volpe <volpefabio@gmail.com>
2016-12-12 16:33:52 +0000

    contact us section copied to the about us page

--- site/pages/about-us/index.js
+++ site/pages/about-us/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import PrinciplesSlice from './principles-slice';
 
-export default function AboutUs() {
+import PrinciplesSlice from './principles-slice';
+import ContactUs from '../../slices/contact-us-slice';
+
+export default function AboutUs({ contactUsURL }) {
   return (
     <div>
       <PrinciplesSlice />
+      <ContactUs postURL={contactUsURL} />
     </div>
   );
 }
