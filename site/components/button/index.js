ddf542fe2df85db37060c41fbffa89218354427f
JR Granell <john-robert.granell@red-badger.com>
2016-12-13 13:40:09 +0000

    Handle different background colours for button

--- site/components/button/index.js
+++ site/components/button/index.js
@@ -2,12 +2,26 @@ import React from 'react';
 import styles from './style.css';
 
 
-const Button = ({ label }) => {
-  return <button className={styles.button}>{label}</button>;
+const Button = ({ label, background }) => {
+  let className;
+  switch (background) {
+    case 'black':
+      className = styles.dark;
+      break;
+    case 'yellow':
+      className = styles.yellow;
+      break;
+    default:
+      className = styles.light;
+      break;
+  }
+
+  return <button className={className}>{label}</button>;
 };
 
 Button.propTypes = {
   label: React.PropTypes.string,
+  background: React.PropTypes.string,
 };
 
 export default Button;
