386f4b5b0c06e2b8536b1c403d4f536cc73d2013
Etiene Dalcol <dalcol@etiene.net>
2016-12-13 16:59:55 +0000

    Refactor error handling on services

--- services/mailchimp/update-user/index.js
+++ services/mailchimp/update-user/index.js
@@ -1,7 +1,7 @@
 import md5 from 'md5';
 import { mailchimpApi, formatUpdateResponse, formatFormInput } from '../utilities';
 
-export default function doUpdateUser(event, cb) {
+export default function doUpdateUser(event, context, cb) {
   const mailingListId = process.env.MAILING_LIST_ID;
   const body = formatFormInput(event, true);
   const emailAddress = body.email_address.toLowerCase();
@@ -19,4 +19,3 @@ export default function doUpdateUser(event, cb) {
       cb(err);
     });
 }
-
