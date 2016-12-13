55c97dd442b686c764835c353694aac582ef1ed8
JR Granell <john-robert.granell@red-badger.com>
2016-12-13 09:27:19 +0000

    Added buttons to principles-slice

--- site/pages/about-us/principles-slice/index.js
+++ site/pages/about-us/principles-slice/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import style from './style.css';
 import Principle from './principle';
+import Button from '../../../components/button';
+import Link from '../../../components/link';
 
 export default function Principles() {
   return (
@@ -18,6 +20,10 @@ export default function Principles() {
         <Principle number="06" title="Tell it like it is." description="We’re honest, approachable and down-to-earth. We don’t sell, or spin. This is also about good communication, making sure we have a shared understanding." />
         <Principle number="07" title="It’s about people." description="We’re empathetic. We put people at the centre. We’re driven by user value, user-centered design and accessibility. We encourage mutual respect, growth and development." />
         <Principle number="08" title="Give a monkey’s. Don’t be a jackass." description="We care about what we do and we care about each other. We’re humble. No egos." />
+        <div className={style.buttons}>
+          <Link to="meetOurTeam" style={{ marginRight: 30 }}><Button label="Meet our team" /></Link>
+          <Link to="joinUs"><Button label="Join us" /></Link>
+        </div>
       </div>
     </div>
   );
