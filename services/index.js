310353f9b86fe8aeb96f180e9a8fa80f21330158
Etiene Dalcol <dalcol@etiene.net>
2016-12-13 16:00:19 +0000

    Insert 500 status code to lambda callback error message

--- services/index.js
+++ services/index.js
@@ -3,38 +3,46 @@ import doContactUs from './contact_us';
 import doSignUp from './mailchimp/sign-up/index';
 import doUpdateUser from './mailchimp/update-user/index';
 
+const cbWithErrorHandling = cb => (e, body) => {
+  if (e) {
+    e.message = `[500] ${e.message}`; // eslint-disable-line no-param-reassign
+    return cb(e);
+  }
+  return cb(null, body);
+};
+
 export function publish(event, context, cb) {
   if (event.query && event.query.auth_token !== process.env.PRIVATE_LAMBDA_API_KEY) {
     return cb(new Error('[403] Forbidden'));
   }
 
   try {
-    doPublish(cb);
+    doPublish(cbWithErrorHandling(cb));
   } catch (e) {
-    cb(e);
+    cbWithErrorHandling(cb)(e);
   }
 }
 
 export function contactUs(event, context, cb) {
   try {
-    doContactUs(event, cb);
+    doContactUs(event, cbWithErrorHandling(cb));
   } catch (e) {
-    cb(e);
+    cbWithErrorHandling(cb)(e);
   }
 }
 
 export function signUp(event, context, cb) {
   try {
-    doSignUp(event, cb);
+    doSignUp(event, cbWithErrorHandling(cb));
   } catch (e) {
-    cb(e);
+    cbWithErrorHandling(cb)(e);
   }
 }
 
 export function updateUser(event, context, cb) {
   try {
-    doUpdateUser(event, cb);
+    doUpdateUser(event, cbWithErrorHandling(cb));
   } catch (e) {
-    cb(e);
+    cbWithErrorHandling(cb)(e);
   }
 }
