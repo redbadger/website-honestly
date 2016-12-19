598d07d70ec0ed0089751e409e99feab212493f0
Graham Mendick <graham.mendick@gmail.com>
2016-12-19 14:35:38 +0000

    Added test for featured blogs on home page

--- site/compiler/test.js
+++ site/compiler/test.js
@@ -69,5 +69,48 @@ describe('site/compiler', () => {
       expect(pages[9].path).to.equal('about-us/join-us/ux-designer/index.html');
       expect(pages[9].body).to.match(/UX Designer/);
     });
+
+    it('renders the featured blogs of the site on the home page', () => {
+      const pages = compileSite({
+        jobs: [],
+        job: {},
+        contactUsURL: '',
+        featuredBlogPosts: [
+          {
+            slug: '2016/12/7/how-we-use-service-workers-on-red-badgers-new-website',
+            title: 'Service Worker support on Red Badgers new website',
+            author: {
+            },
+          },
+          {
+            slug: '016/11/29/gitgithub-in-plain-english',
+            title: 'Git and Github in Plain English',
+            author: {
+            },
+          },
+        ],
+        events: [],
+      });
+
+      expect(pages.length).to.equal(8);
+      expect(pages[0].path).to.equal('index.html');
+      expect(pages[0].body).to.match(/We work with you to deliver digital products/);
+      expect(pages[0].body).to.match(/Service Worker support on Red Badgers new website/);
+      expect(pages[0].body).to.match(/Git and Github in Plain English/);
+      expect(pages[1].path).to.equal('what-we-do/index.html');
+      expect(pages[1].body).to.match(/How do we do the thing right\?/);
+      expect(pages[2].path).to.equal('about-us/index.html');
+      expect(pages[2].body).to.match(/This is what we believe/);
+      expect(pages[3].path).to.equal('about-us/join-us/index.html');
+      expect(pages[3].body).to.match(/Join us/);
+      expect(pages[4].path).to.equal('about-us/events/index.html');
+      expect(pages[4].body).to.match(/React London 2017/);
+      expect(pages[5].path).to.equal('404.html');
+      expect(pages[5].body).to.match(/Whaaaaaat!\?/);
+      expect(pages[6].path).to.equal('50x/index.html');
+      expect(pages[6].body).to.match(/Oops!/);
+      expect(pages[7].path).to.equal('offline/index.html');
+      expect(pages[7].body).to.match(/No internet connection/);
+    });
   });
 });
