c791f9b82e710fe576d49da628c0590752c3b0d3
JR <jrgranell@msn.com>
2016-12-20 17:05:08 +0000

    Contact us anchor (#276)
    * Added contact us anchor and link on homepage
    
    * Construct contact us link using stateNavigator
    
    * Include any preceding URL segments in generation
    
    * Alternative focus solution relying on JavaScript
    
    * Added state navigator workaround for contact us hash
    
    * Refactored state navigator creation and compile tests
    
    * Added test for featured blogs on home page
    
    * Added test for rendering dynamic event pages
    
    * Removed irrelevant asserts to make tests clearer
    
    * Removed blank line
    
    * Added comment to explain hash workaround
    
    * Generalised the hash workaround so that other data in the query string is kept
    
    * Improved workaround comment
    
    * The focus in the contact us didn't work because the onNavigating fired before the component was unloaded, and caused odd scrolling when navigating browser history between two pages with contact us forms on. Moved the focus into the menu, with a settimeout so it runs after the window.scrollTo in the main onNavigate
    
    * Moved the focus out of menu into onNavigate
    
    * Removed context from contact us components
    
    * Fixed flow error
    
    * Moved contact us focus into ReactDOM render callback to avoid setTimeout
    
    * Added tests for contact us hash
    
    * Removed component props warnings from tests
    
    * Moved scrollTo into callback so it's called after render - in ie11 it caused intermediate scroll when pressing back
    
    * Removed autofocus and contactus param from component and just kept the focus in the onNavigate
    
    * Added scrollIntoView for safety - safari focused without scrolling sometimes
    
    * Scrolled the section into view so that details are visible to user
    
    * Moved scroll and focus into separate function as per code review comment
    
    * Moved code nearer to use for clarity

--- site/routes/index.js
+++ site/routes/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HTML5HistoryManager, StateNavigator } from 'navigation';
 
 import { routeDefinitions } from './definitions';
 import L from '../layout';
@@ -27,7 +28,7 @@ const componentMap = {
   aboutUsPage: AboutUsPage,
 };
 
-export default function routes() {
+function routes() {
   return routeDefinitions.map(
     route => ({
       ...route,
@@ -37,3 +38,62 @@ export default function routes() {
       },
     }));
 }
+
+/*
+  When the contact us menu item (only appears in the mobile menu) is clicked
+  the window should scroll to the contacts us section of the home page (see
+  https://github.com/redbadger/website-honestly/issues/274).
+
+  There's a contactUs hash in the URL so that the scrolling works without
+  JavaScript. The problem is that the state navigator doesn't cater for hashes
+  in the URL. What follows is a workaround for using hashes with the state
+  navigator.
+
+  The HistoryManager handles converting Urls to Hrefs and Hrefs to Urls. That
+  means we can pass the contactUs indicator in the query string internally, to
+  keep the state navigator happy, and only convert it to and from a hash when
+  it appears in an href (anchor tag or browser location).
+
+  Also need a custom addHistory to include the hash in the check to see if the
+  url's changed. Otherwise clicking on contact us and then the home page won't
+  change the url because they only differ on the hash.
+*/
+const handleContactUsHash = stateNavigator => {
+  const { historyManager } = stateNavigator;
+  const getUrl = historyManager.getUrl.bind(historyManager);
+  historyManager.getUrl = hrefElement => {
+    let url = getUrl(hrefElement);
+    if (hrefElement.hash === '#contactUs') {
+      const { state, data } = stateNavigator.parseLink(url);
+      url = stateNavigator.getNavigationLink(state.key, { ...data, contactUs: true });
+    }
+    return url;
+  };
+  const getHref = historyManager.getHref.bind(historyManager);
+  historyManager.getHref = url => {
+    let newUrl = url;
+    let hash = '';
+    const { state, data: { contactUs, ...rest } } = stateNavigator.parseLink(url);
+    if (contactUs) {
+      newUrl = stateNavigator.getNavigationLink(state.key, rest);
+      hash = '#contactUs';
+    }
+    return getHref(newUrl) + hash;
+  };
+  historyManager.addHistory = url => {
+    const href = historyManager.getHref(url);
+    if (location.pathname + location.search + location.hash !== href) {
+      window.history.pushState(null, null, href);
+    }
+  };
+  return historyManager;
+};
+
+export default () => {
+  const stateNavigator = new StateNavigator(
+    routes(),
+    new HTML5HistoryManager((process.env.URL_BASENAME || '').slice(0, -1))
+  );
+  handleContactUsHash(stateNavigator);
+  return stateNavigator;
+};
