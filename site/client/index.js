ff36f1753a5f5a373b3239d6b1a939da301ffb2c
Graham Mendick <graham.mendick@gmail.com>
2016-12-20 10:27:06 +0000

    Moved contact us focus into ReactDOM render callback to avoid setTimeout

--- site/client/index.js
+++ site/client/index.js
@@ -13,18 +13,19 @@ export function makeApp({ element, state }) {
 
   stateNavigator.onNavigate((oldRoute, route, params) => {
     window.scrollTo(0, 0);
-    if (oldRoute === route && params.contactUs) {
-      const el = document.getElementById('contactUsMessage');
-      if (el) {
-        setTimeout(() => el.focus(), 0);
-      }
-    }
     const props = route.stateToProps && route.stateToProps(state, params);
     const pageTitle = typeof route.title === 'function' ? route.title(props) : route.title;
     const title = `${pageTitle} | ${TITLE_SUFFIX}`;
     stateNavigator.stateContext.title = title;
     const component = route.component({ stateNavigator, title }, { ...props, ...params });
-    ReactDOM.render(component, element);
+    ReactDOM.render(component, element, () => {
+      if (oldRoute === route && params.contactUs) {
+        const el = document.getElementById('contactUsMessage');
+        if (el) {
+          el.focus();
+        }
+      }
+    });
   });
   return stateNavigator;
 }
