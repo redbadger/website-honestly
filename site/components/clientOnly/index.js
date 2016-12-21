4f04c98dca15290e9089f4c25ee133e3d0814e87
JR Granell <john-robert.granell@red-badger.com>
2016-12-21 16:30:04 +0000

    Updated clientOnly to set state as class property

--- site/components/clientOnly/index.js
+++ site/components/clientOnly/index.js
@@ -3,11 +3,9 @@ import React, { PropTypes, Component } from 'react';
 /** Only render the children of this component if JavaScript is enabled and we are in the browser */
 class ClientOnly extends Component {
 
-  componentWillMount() {
-    this.setState({
-      javaScriptSupported: false,
-    });
-  }
+  state = {
+    javaScriptSupported: false,
+  };
 
   componentDidMount() {
     this.setState({
