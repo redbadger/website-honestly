a41748fd67dcbef267f9fed01b8f10a9bc0a83dd
Joe Stanton <joe.stanton@red-badger.com>
2016-12-21 12:20:49 +0000

    Social slice WIP

--- site/pages/about-us/index.js
+++ site/pages/about-us/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import TimelineSlice from './timeline-slice';
 import PrinciplesSlice from './principles-slice';
+import SocialSlice from '../../slices/social-slice';
 import ContactUs from '../../slices/contact-us-slice';
 
 const AboutUs = ({ contactUsURL }) => {
   return (
     <div>
+      <SocialSlice />
       <PrinciplesSlice />
       <TimelineSlice />
       <ContactUs postURL={contactUsURL} />
---
+++ site/slices/social-slice/index.js
@@ -0,0 +1,63 @@
+import React, { PropTypes } from 'react';
+import cx from 'classnames';
+
+import styles from './styles.css';
+
+const IntroCard = ({ name }) => (
+  <li className={styles.intro}>
+    <span>
+      From our social feed
+    </span>
+  </li>
+);
+
+const Twitter = ({handle, tweet, retweetCount, likeCount}) => (
+  <li className={styles.twitter}>
+    <div className={styles.handle}>{handle}</div>
+    <div className={styles.tweet}>
+      {tweet}
+    </div>
+    <div className={styles.meta}>
+      <span className={styles.retweets}>{retweetCount}</span>
+      <span className={styles.likes}>{likeCount}</span>
+    </div>
+  </li>
+);
+
+const Instagram = ({handle, image, alt, likeCount, commentCount}) => (
+  <li className={styles.instagram}>
+    <div className={styles.handle}>{handle}</div>
+    <img
+      className={styles.image}
+      alt={alt}
+      src={image}
+    />
+    <div className={styles.meta}>
+      <span className={styles.likes}>{likeCount}</span>
+      <span className={styles.comments}>{commentCount}</span>
+    </div>
+  </li>
+);
+
+const SocialSlice = () => (
+  <section className={styles.socialSlice}>
+    <ul className={styles.cards}>
+      <IntroCard />
+      <Twitter
+        handle="@redbadgerteam"
+        tweet="What on earth is #git?! @nicollecastrog explains all in plain english here – http://bit.ly/2gWDcyk #versioncontrol #techexplained #github"
+        retweetCount={88}
+        likeCount={88}
+      />
+      <Instagram
+        handle="@redbadgerteam"
+        image="https://cdn.zeplin.io/57c8297fc3cf63795b6a4520/assets/90B70B12-B358-48D5-BA5E-DCA61BEE2A03.png"
+        alt="pic from the gram"
+        likeCount={88}
+        commentCount={88}
+      />
+    </ul>
+  </section>
+);
+
+export default SocialSlice;
