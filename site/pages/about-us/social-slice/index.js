4b2e22f11bab7fa0d60e67320c93b522ec1f575c
Joe Stanton <joe.stanton@red-badger.com>
2016-12-21 15:43:24 +0000

    Added icons, not aligned yet

--- site/pages/about-us/social-slice/index.js
+++ site/pages/about-us/social-slice/index.js
@@ -1,7 +1,16 @@
 import React, { PropTypes } from 'react';
 import cx from 'classnames';
 
+import InlineSVG from 'svg-inline-react';
+
 import styles from './styles.css';
+
+import instagramIconSVG from './icons/instagram/instagram.svg';
+import twitterIconSVG from './icons/twitter/twitter.svg';
+
+import likeIconSVG from './icons/like/like.svg';
+import retweetIconSVG from './icons/retweet/retweet.svg';
+import commentIconSVG from './icons/comment/comment.svg';
 
 const IntroCard = ({ name }) => (
   <li className={styles.intro}>
@@ -14,28 +23,46 @@ const IntroCard = ({ name }) => (
 const colours = [styles.blue, styles.purple, styles.green];
 const Twitter = ({ handle, tweet, retweetCount, likeCount, index = 1 }) => (
   <li className={cx(styles.twitter, colours[index % 3])}>
-    <div className={styles.handle}>{handle}</div>
+    <div className={styles.handle}>
+      <InlineSVG src={twitterIconSVG} className={styles.twitterIcon} />
+      {handle}
+    </div>
     <div className={styles.tweet}>
       {tweet}
     </div>
     <div className={styles.meta}>
-      <span className={styles.retweets}>{retweetCount}</span>
-      <span className={styles.likes}>{likeCount}</span>
+      <span className={styles.retweets}>
+        <InlineSVG src={retweetIconSVG} className={styles.icon} />
+        {retweetCount}
+      </span>
+      <span className={styles.likes}>
+        <InlineSVG src={likeIconSVG} className={styles.icon} />
+        {likeCount}
+      </span>
     </div>
   </li>
 );
 
 const Instagram = ({handle, image, alt, likeCount, commentCount}) => (
   <li className={styles.instagram}>
-    <div className={styles.handle}>{handle}</div>
+    <div className={styles.handle}>
+      <InlineSVG src={instagramIconSVG} className={styles.instagramIcon} />
+      {handle}
+    </div>
     <img
       className={styles.image}
       alt={alt}
       src={image}
     />
     <div className={styles.meta}>
-      <span className={styles.likes}>{likeCount}</span>
-      <span className={styles.comments}>{commentCount}</span>
+      <span className={styles.likes}>
+        <InlineSVG src={likeIconSVG} className={styles.icon} />
+        {likeCount}
+      </span>
+      <span className={styles.comments}>
+        <InlineSVG src={commentIconSVG} className={styles.icon} />
+        {commentCount}
+      </span>
     </div>
   </li>
 );
