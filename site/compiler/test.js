3f17b7925b21f28c841ccc78e8d318be3e310ced
Graham Mendick <graham.mendick@gmail.com>
2016-12-22 14:30:51 +0000

    Removed asserts for non-badgers pages

--- site/compiler/test.js
+++ site/compiler/test.js
@@ -176,22 +176,6 @@ describe('site/compiler', () => {
       });
 
       expect(pages.length).to.equal(11);
-      expect(pages[0].path).to.equal('index.html');
-      expect(pages[0].body).to.match(/We work with you to deliver digital products/);
-      expect(pages[1].path).to.equal('what-we-do/index.html');
-      expect(pages[1].body).to.match(/How do we do the thing right\?/);
-      expect(pages[2].path).to.equal('about-us/index.html');
-      expect(pages[2].body).to.match(/This is what we believe/);
-      expect(pages[3].path).to.equal('about-us/join-us/index.html');
-      expect(pages[3].body).to.match(/Join us/);
-      expect(pages[4].path).to.equal('about-us/events/index.html');
-      expect(pages[4].body).to.match(/React London 2017/);
-      expect(pages[5].path).to.equal('404.html');
-      expect(pages[5].body).to.match(/Whaaaaaat!\?/);
-      expect(pages[6].path).to.equal('50x/index.html');
-      expect(pages[6].body).to.match(/Oops!/);
-      expect(pages[7].path).to.equal('offline/index.html');
-      expect(pages[7].body).to.match(/No internet connection/);
       expect(pages[8].path).to.equal('about-us/people/index.html');
       expect(pages[8].body).to.match(/everyone/);
       expect(pages[9].path).to.equal('about-us/people/engineering/index.html');
