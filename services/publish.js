03dbd79c8bf21b1ae51d714cbc8d2ea286984846
Alex Savin <karismafilms@gmail.com>
2017-01-04 17:07:52 +0000

    Testing bugsnag in publish function

--- services/publish.js
+++ services/publish.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-console */
+import bugsnag from 'bugsnag';
+
 import getSiteState from '../state';
 import { compileSite } from '../site/compiler';
 import { makeUploader } from './s3';
 
 const bucketName = process.env.BUCKET_NAME;
 const uploadPage = makeUploader({ bucketName });
+
+bugsnag.register(process.env.BUGSNAG_KEY);
+bugsnag.notify(new Error('Non-fatal'));
 
 if (!bucketName) {
   throw new Error('bucketName environment variable not set!');
