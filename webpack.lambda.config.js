f8d3e10210a694bac8f0a8651a2cd57db86bebf6
JR Granell <john-robert.granell@red-badger.com>
2017-01-05 12:21:33 +0000

    Removed babel-plugin-typecheck

--- webpack.lambda.config.js
+++ webpack.lambda.config.js
@@ -1,8 +1,6 @@
 const baseConfig = require('./webpack.base.config');
 const webpackMerge = require('webpack-merge').smart;
 
-process.env.BABEL_ENV = 'typecheck';
-
 const lambdaConfig = webpackMerge(baseConfig, {
   entry: {
     services: './services/index.js',
