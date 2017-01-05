3066f803d659606261e088f3dc1eb164bce4528a
Graham Mendick <graham.mendick@gmail.com>
2017-01-05 11:36:38 +0000

    Put typecheck back because it didn't fix circle ci

--- webpack.lambda.config.js
+++ webpack.lambda.config.js
@@ -1,6 +1,8 @@
 const baseConfig = require('./webpack.base.config');
 const webpackMerge = require('webpack-merge').smart;
 
+process.env.BABEL_ENV = 'typecheck';
+
 const lambdaConfig = webpackMerge(baseConfig, {
   entry: {
     services: './services/index.js',
