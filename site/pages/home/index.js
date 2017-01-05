67e3156ca114ada367dd977684220ef8cfe21b1a
Graham Mendick <graham.mendick@gmail.com>
2017-01-05 11:11:10 +0000

    Another circle ci test

--- site/pages/home/index.js
+++ site/pages/home/index.js
@@ -10,7 +10,7 @@ import ContactUs from '../../slices/contact-us-slice';
 import NewsLetter from './newsletter-slice';
 
 export type HomePageProps = {
-  contactUsURL: void,
+  contactUsURL: string,
   featuredBlogPosts: Array<Object>,
 };
 
