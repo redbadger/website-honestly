b2becd1dac5f4adcd83f6da203f1b1593a816b88
Fabio Volpe <volpefabio@gmail.com>
2017-01-06 15:38:30 +0000

    everyone category link

--- site/pages/meet-our-team/index.js
+++ site/pages/meet-our-team/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Link from '../../components/link';
 
-const MeetOurTeam = ({ category, categories, badgers }) => {
+const MeetOurTeam = ({ categories, badgers }) => {
   return (
     <div>
       <h1>Meet out team</h1>
       <div>
+        <Link
+          disableActive
+          to="badgers"
+          navigationData={{ category: 'everyone' }}
+        >
+          Everyone
+        </Link>
         {categories.map(b =>
-          <Link key={b.slug} to="badgers" navigationData={{ category: b.slug }}>{b.name}</Link>
+          <Link
+            key={b.slug}
+            disableActive
+            to="badgers"
+            navigationData={{ category: b.slug }}
+          >
+            {b.name}
+          </Link>
         )}
       </div>
       <ul>
