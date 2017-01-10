e765a2dd4c881c323c40a62d935ff68f2eb37e42
JR Granell <john-robert.granell@red-badger.com>
2017-01-10 15:39:20 +0000

    Change base64 encoding to use node<6 syntax

--- site/fetchers/twitter.js
+++ site/fetchers/twitter.js
@@ -62,7 +62,7 @@ export const isValidTweet = (tweet: TwitterResponse) => {
 };
 
 const baseUrl = 'https://api.twitter.com';
-const base64 = str => Buffer.from(str).toString('base64');
+const base64 = str => new Buffer(str).toString('base64');
 
 const getBearerToken = (fetch, key, secret) =>
   fetch(baseUrl + '/oauth2/token', {
