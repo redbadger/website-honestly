e8222eff8d76ed5373ce32259dea10625b27bd60
JR Granell <john-robert.granell@red-badger.com>
2017-01-10 13:28:22 +0000

    Fixed destktop view by calculating width

--- site/pages/about-us/social-slice/index.js
+++ site/pages/about-us/social-slice/index.js
@@ -17,18 +17,53 @@ type SocialSliceProps = {
 type SocialSliceState = {
   tile: number;
   totalTiles: number;
+  viewWidth: number;
 }
 
 class SocialSlice extends React.Component {
+
   state: SocialSliceState;
   componentWillMount() {
     this.state = {
       tile: 0,
       totalTiles: (this.props.instagramPosts.length + this.props.tweets.length) - 1,
+      viewWidth: window.innerWidth,
     };
+    window.addEventListener('resize', this.updateViewWidth);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateViewWidth);
   }
 
   setTile = (tile: number) => this.setState({ tile });
+
+  /**
+   * Called on every browser resize this updates the width and determines
+   * the swipableTileTotal tile total
+   */
+  updateViewWidth = () => {
+    const newWidth = window.innerWidth;
+    this.setState({ viewWidth: newWidth });
+  };
+
+  /**
+    * Calulate the number of total tiles that can be swiped
+    * If we have a view width larger thatn the intro tile + 1 we would get
+    * black space at the end. By returning a lower number than total tiles this is prevented
+    */
+  calculateSwipableTotal = () => {
+    const { viewWidth, totalTiles } = this.state;
+
+    if (viewWidth > this.tileSize * 2) {
+      // Adjust for intro slide + 1 slide;
+      const adjustedWidth = viewWidth - (this.tileSize * 2);
+      // Calculate the number of tiles that are in view and then remove that from the total tiles
+      const tilesToRemove = Math.floor(adjustedWidth / this.tileSize);
+      return totalTiles - tilesToRemove;
+    }
+    return totalTiles;
+  }
   prevTile = () => {
     const prevTile = this.state.tile - 1;
     if (prevTile < 0) {
@@ -38,12 +73,28 @@ class SocialSlice extends React.Component {
   }
   nextTile = () => {
     const nextTile = this.state.tile + 1;
-    if (nextTile > this.state.totalTiles) {
+    const swipableTotal = this.calculateSwipableTotal();
+    console.log(swipableTotal);
+    if (nextTile > swipableTotal) {
       return;
     }
     this.setState({ tile: nextTile });
   }
   props: SocialSliceProps;
+  /** This is the tile width used for calculations - also the value in the CSS styling */
+  tileSize = 350;
+
+  /**
+   * Calculate the padding that should be added onto the swipe container
+   * to ensure that the correct distance is swiped. This is dynamic and
+   * responds to a browser resize. The calculation is based on
+   * the intro tile + one tile + padding offset of 60
+   */
+  calculateSwipePadding() {
+    const { viewWidth } = this.state;
+    const swipePadding = (viewWidth - (this.tileSize * 2)) + 60;
+    return swipePadding;
+  }
 
   renderTiles = () => {
     const { tweets, instagramPosts } = this.props;
@@ -68,6 +119,7 @@ class SocialSlice extends React.Component {
   }
 
   render() {
+    const swipePadding = this.calculateSwipePadding();
     return (
       <section className={styles.socialSlice}>
         <div className={styles.desktopView}>
@@ -75,7 +127,7 @@ class SocialSlice extends React.Component {
           <SwipeableViews
             index={this.state.tile}
             onChangeIndex={this.setTile}
-            style={{ paddingRight: '15%' }}
+            style={{ paddingRight: swipePadding }}
             slideStyle={{ width: 350 }}
           >
             {this.renderTiles()}
