3bf7dc6d5558a12984d4f9f2f2ef462732990180
Dominik Stodolny <dom@dmk.st>
2017-01-11 12:10:52 +0000

    make next button disabled when there is no more pages left to go next

--- site/pages/meet-our-team/index.js
+++ site/pages/meet-our-team/index.js
@@ -58,7 +58,8 @@ const MeetOurTeam = ({ categories, category, badgers, page }) => {
           to="badgers"
           includeCurrentData
           className={styles.pagingButton}
-          navigationData={{ page: page + 1 }}
+          navigationData={{ page: Math.min(Math.ceil(badgers.length / 20, page + 1)) }}
+          disableActive
         >
           Next page
         </Link>
