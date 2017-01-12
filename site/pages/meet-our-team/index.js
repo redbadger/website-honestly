730e569f045f7d01a3a59c913cdf3f02bd241923
Dominik Stodolny <dom@dmk.st>
2017-01-12 13:29:25 +0000

    fix next page

--- site/pages/meet-our-team/index.js
+++ site/pages/meet-our-team/index.js
@@ -60,7 +60,7 @@ const MeetOurTeam = ({ categories, category, badgers, page }) => {
             to="badgers"
             includeCurrentData
             className={styles.pagingButton}
-            navigationData={{ page: Math.min(Math.ceil(badgers.length / 20, page + 1)) }}
+            navigationData={{ page: Math.min(Math.ceil(badgers.length / 20), page + 1) }}
             disableActive
           >
             Next page
