b06ee5c5b7e6044d38a8fdbe5bdd65f421143e07
Graham Mendick <graham.mendick@gmail.com>
2017-01-13 14:46:49 +0000

    Move ad and auto returned jsx from component

--- site/pages/meet-our-team/index.js
+++ site/pages/meet-our-team/index.js
@@ -34,7 +34,7 @@ const getTeam = (badgers, category) => {
     loaded: true,
   };
 
-  return [...badgers.slice(0, 7), jobAdvert, ...badgers.slice(7)];
+  return [...badgers.slice(0, 6), jobAdvert, ...badgers.slice(6)];
 };
 
 type MeetOurTeamProps = {
@@ -44,44 +44,42 @@ type MeetOurTeamProps = {
   badgers: Array<Badger>,
 };
 
-const MeetOurTeam = ({ categories, category, badgers, page }: MeetOurTeamProps) => {
-  return (
-    <div className={styles.meetOurTeam}>
-      <div className={styles.teamContainer}>
-        <h1 className={styles.title}>Meet our team</h1>
-        <h2 className={styles.subtitle}>
-          Red Badger is all about the people. We work brilliantly as a team.
-          We inspire and bring out the best in one another.
-        </h2>
-        <ul className={styles.categories}>
-          <li className={styles.categoryItem}>
+const MeetOurTeam = ({ categories, category, badgers, page }: MeetOurTeamProps) => (
+  <div className={styles.meetOurTeam}>
+    <div className={styles.teamContainer}>
+      <h1 className={styles.title}>Meet our team</h1>
+      <h2 className={styles.subtitle}>
+        Red Badger is all about the people. We work brilliantly as a team.
+        We inspire and bring out the best in one another.
+      </h2>
+      <ul className={styles.categories}>
+        <li className={styles.categoryItem}>
+          <Link
+            to="badgers"
+            navigationData={{ category: 'everyone' }}
+            className={styles.category}
+            activeCssClass={styles.active}
+          >
+            Everyone
+          </Link>
+        </li>
+        {categories.map(c =>
+          <li key={c.slug} className={styles.categoryItem}>
             <Link
+              key={c.slug}
               to="badgers"
-              navigationData={{ category: 'everyone' }}
-              className={styles.category}
+              navigationData={{ category: c.slug }}
               activeCssClass={styles.active}
+              className={styles.category}
             >
-              Everyone
+              {c.name}
             </Link>
           </li>
-          {categories.map(c =>
-            <li key={c.slug} className={styles.categoryItem}>
-              <Link
-                key={c.slug}
-                to="badgers"
-                navigationData={{ category: c.slug }}
-                activeCssClass={styles.active}
-                className={styles.category}
-              >
-                {c.name}
-              </Link>
-            </li>
-          )}
-        </ul>
-        <TeamSlice badgers={getTeam(badgers, category)} page={page} />
-      </div>
+        )}
+      </ul>
+      <TeamSlice badgers={getTeam(badgers, category)} page={page} />
     </div>
-  );
-};
+  </div>
+);
 
 export default MeetOurTeam;
