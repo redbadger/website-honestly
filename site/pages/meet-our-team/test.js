23698dd3b851e5ca94a900d201fb806e8e646da5
Graham Mendick <graham.mendick@gmail.com>
2017-01-24 14:34:12 +0000

    Added another team slice test

--- site/pages/meet-our-team/test.js
+++ site/pages/meet-our-team/test.js
@@ -7,6 +7,7 @@ describe('site/team-slice', () => {
   it('should render badgers list', () => {
     const stateNavigator = createStateNavigator();
     stateNavigator.navigate('badgers', null, 'none');
+
     const context = {
       context: { stateNavigator },
       childContextTypes: { stateNavigator: React.PropTypes.object },
@@ -25,9 +26,10 @@ describe('site/team-slice', () => {
   });
 
   describe('with 20 Badgers, first page of Everyone', () => {
-    it('should render badgers list with link to page 2', () => {
+    it('should render badgers list with advert and link to page 2', () => {
       const stateNavigator = createStateNavigator();
       stateNavigator.navigate('badgers', null, 'none');
+
       const context = {
         context: { stateNavigator },
         childContextTypes: { stateNavigator: React.PropTypes.object },
@@ -56,9 +58,10 @@ describe('site/team-slice', () => {
   });
 
   describe('with 20 Badgers, second page of Everyone', () => {
-    it('should render badgers list with link to page 1', () => {
+    it('should render badgers list with no advert and link to page 1', () => {
       const stateNavigator = createStateNavigator();
       stateNavigator.navigate('badgers', { page: 2 }, 'none');
+
       const context = {
         context: { stateNavigator },
         childContextTypes: { stateNavigator: React.PropTypes.object },
@@ -81,6 +84,42 @@ describe('site/team-slice', () => {
       expect(teamSlice.find('ul').last().text()).to.not.match(/Are you a potential Badger/);
       expect(teamSlice.find('a').find({ children: 'Previous page' }).props().href)
         .to.equal('/about-us/people');
+      expect(teamSlice.find('a').find({ children: 'Next page' }).props().href).to.equal(null);
+    });
+  });
+
+  describe('with 20 Badgers, first page of Engineering', () => {
+    it('should render badgers list with no advert and no link to page 2', () => {
+      const stateNavigator = createStateNavigator();
+      stateNavigator.navigate('badgers', { category: 'engineering' }, 'none');
+
+      const context = {
+        context: { stateNavigator },
+        childContextTypes: { stateNavigator: React.PropTypes.object },
+      };
+      const badgers = [];
+      for (let i = 0; i < 20; i += 1) {
+        badgers.push({
+          firstName: 'Alex ' + i,
+          categories: [
+            { name: 'Engineering', slug: 'engineering' },
+          ],
+        });
+      }
+
+      const teamSlice = mount(
+        <MeetOurTeam
+          categories={['everyone', 'engineering']}
+          category="engineering"
+          badgers={badgers}
+          page={1}
+        />, context);
+
+      expect(teamSlice.find('ul').last().text()).to.match(/Alex 0/);
+      expect(teamSlice.find('ul').last().text()).to.match(/Alex 18/);
+      expect(teamSlice.find('ul').last().text()).to.match(/Alex 19/);
+      expect(teamSlice.find('ul').last().text()).to.not.match(/Are you a potential Badger/);
+      expect(teamSlice.find('a').find({ children: 'Previous page' }).props().href).to.equal(null);
       expect(teamSlice.find('a').find({ children: 'Next page' }).props().href).to.equal(null);
     });
   });
