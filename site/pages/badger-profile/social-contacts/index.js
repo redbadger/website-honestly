b08e9bc2a3c4b92793c4237d397652eac84a6ad7
Etiene Dalcol <dalcol@etiene.net>
2017-01-26 10:24:51 +0000

    Adopt filter/map approach on badger social contacts

--- site/pages/badger-profile/social-contacts/index.js
+++ site/pages/badger-profile/social-contacts/index.js
@@ -11,32 +11,28 @@ import linkedinSVG from './SVG/linked-in.svg';
 import type { Badger } from '../../../types';
 
 const SocialContacts = ({ badger }: { badger: Badger }) => {
-  const socialItems = [];
   const socialPages = [
     { social: 'Twitter', img: twitterSVG },
     { social: 'Github', img: githubSVG },
     { social: 'LinkedIn', img: linkedinSVG },
   ];
 
-  for (const s of socialPages) {
-    if (badger[s.social.toLowerCase()]) {
-      socialItems.push(<li key={s.social}>
-        <a
-          href={badger[s.social.toLowerCase()]}
-          title={s.social}
-          className={styles.badgerSocialIcon}
-        >
-          <InlineSVG
-            src={s.img}
-            title={s.social}
-          />
-        </a>
-      </li>);
-    }
-  }
   return (
     <ul className={styles.socialLinks}>
-      {socialItems}
+      {socialPages.filter(s => (badger[s.social.toLowerCase()])).map(s =>
+        <li key={s.social}>
+          <a
+            href={badger[s.social.toLowerCase()]}
+            title={s.social}
+            className={styles.badgerSocialIcon}
+          >
+            <InlineSVG
+              src={s.img}
+              title={s.social}
+            />
+          </a>
+        </li>
+        )}
     </ul>
   );
 };
