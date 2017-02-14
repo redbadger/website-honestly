2f11578fa1acd8cf5f35056b553a3c18b17e1f32
Etiene Dalcol <dalcol@etiene.net>
2017-02-14 10:13:40 +0000

    Fix links on team page

--- site/pages/meet-our-team/team-slice/badger-profile/index.js
+++ site/pages/meet-our-team/team-slice/badger-profile/index.js
@@ -22,10 +22,9 @@ export type Badger = {
 const BadgerProfile = ({ badger }: { badger: Badger }) => {
   const fullName = [badger.firstName, badger.lastName].join(' ');
   return (
-    <div>
-      <div className={styles.badgerWrapper} >
-        <Link to="badger" navigationData={{ slug: badger.slug }} className={styles.badgerProfile}>
-
+    <Link to="badger" navigationData={{ slug: badger.slug }}>
+      <div className={styles.backerProfile}>
+        <div className={styles.badgerWrapper} >
           {badger.loaded ?
             <img
               src={badger.primaryImageUrl}
@@ -34,19 +33,19 @@ const BadgerProfile = ({ badger }: { badger: Badger }) => {
               aria-hidden
             /> : <div className={styles.placeholder} />
           }
-        </Link>
-      </div>
-      <div className={styles.description}>
-        <div className={styles.name}>
-          {`${badger.firstName} `}
-          <div className={styles.lastName}>
-            {badger.lastName}
-            <InlineSVG src={arrowSVG} className={styles.arrow} aria-hidden />
-          </div>
         </div>
-        <div className={styles.jobDescription}>{badger.jobTitle}</div>
+        <div className={styles.description}>
+          <div className={styles.name}>
+            {`${badger.firstName} `}
+            <div className={styles.lastName}>
+              {badger.lastName}
+              <InlineSVG src={arrowSVG} className={styles.arrow} aria-hidden />
+            </div>
+          </div>
+          <div className={styles.jobDescription}>{badger.jobTitle}</div>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
