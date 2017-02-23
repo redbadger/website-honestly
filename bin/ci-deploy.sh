29c4e6b88560dfefb12f6bee327fde4738361af4
Dominik Stodolny <dom@dmk.st>
2017-02-23 11:37:38 +0000

    echo last commit sha to the version.txt file

--- bin/ci-deploy.sh
+++ bin/ci-deploy.sh
@@ -9,16 +9,21 @@ createCommitSite() {
   export ENVIRONMENT_NAME="staging"
   export URL_BASENAME="$COMMIT_REF/"
 
+
   echo Deploying commit preview site to $URL_BASENAME
   make clean
   make fetch
   make dev-commit
+
+  echo $COMMIT_REF > ./dist/version.txt
+
   echo Copying assets to S3
   aws s3 sync ./dist/assets-honestly s3://$BUCKET_NAME/$COMMIT_REF/assets-honestly
   aws s3 cp ./dist/manifest.json s3://$BUCKET_NAME/$COMMIT_REF/manifest.json
   aws s3 cp ./dist/robots.txt s3://$BUCKET_NAME/$COMMIT_REF/robots.txt
   aws s3 cp ./dist/sw.js s3://$BUCKET_NAME/$COMMIT_REF/sw.js
   aws s3 sync ./dist/static-site/$COMMIT_REF/ s3://$BUCKET_NAME/$COMMIT_REF/
+  aws s3 cp ./dist/version.txt s3://$BUCKET_NAME/$COMMIT_REF/version.txt
   echo Done!
 
   echo Registering deployment with GitHub
@@ -27,14 +32,19 @@ createCommitSite() {
 }
 
 deployMaster() {
+  COMMIT_REF=$(git rev-parse --short HEAD)
   echo Deploying current master to $1
   make clean
   make build
+
+  echo $COMMIT_REF > ./dist/version.txt
+
   echo Copying assets to S3
   aws s3 sync ./dist/assets-honestly s3://$BUCKET_NAME/assets-honestly
   aws s3 cp ./dist/manifest.json s3://$BUCKET_NAME/manifest.json
   aws s3 cp ./dist/robots.txt s3://$BUCKET_NAME/robots.txt
   aws s3 cp ./dist/sw.js s3://$BUCKET_NAME/sw.js
+  aws s3 cp ./dist/version.txt s3://$BUCKET_NAME/version.txt
   make services-deploy
   make publish-service-invoke
   ./bin/register-github-release.js $1
