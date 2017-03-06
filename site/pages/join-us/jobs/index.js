7342daf84bd46a0de03f99da6901b0e936733e19
Alex Savin <karismafilms@gmail.com>
2017-03-06 14:30:13 +0000

    Refactoring Jobs component

--- site/pages/join-us/jobs/index.js
+++ site/pages/join-us/jobs/index.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 
 import Note from '../../../components/note';
 import HtmlParser from '../../../components/html-parser';
@@ -7,30 +6,36 @@ import styles from './style.css';
 import Link from '../../../components/link';
 import Wall from '../../../components/wall';
 
-export default class Jobs extends Component {
-  static propTypes = {
-    jobs: React.PropTypes.arrayOf(React.PropTypes.shape({
+export default function Jobs({ jobs }) {
+  const listings = jobs.map((job, index) => (
+    <Note key={index}>
+      <Link
+        className={styles.title}
+        to="job"
+        navigationData={{ slug: job.slug }}
+      >
+        {job.title}
+      </Link>
+      <HtmlParser>{job.description}</HtmlParser>
+    </Note>
+  ));
+
+  const jobsClasses = `jobs ${styles.jobs}`;
+
+  return (
+    <div className={jobsClasses}>
+      <Wall cols={3}>
+        {listings}
+      </Wall>
+    </div>
+  );
+}
+
+Jobs.propTypes = {
+  jobs: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
       description: React.PropTypes.string,
       title: React.PropTypes.string,
-    })) // eslint-disable-line comma-dangle
-  };
-
-  render() {
-    const listings = this.props.jobs.map((job, index) => (
-      <Note key={index}>
-        <Link className={styles.title} to='job' navigationData={{ slug: job.slug }}>
-          {job.title}
-        </Link>
-        <HtmlParser>{job.description}</HtmlParser>
-      </Note>
-    ));
-
-    return (
-      <div className="jobs ${styles.jobs}">
-        <Wall cols={3}>
-          {listings}
-        </Wall>
-      </div>
-    );
-  }
-}
+    }),
+  ),
+};
