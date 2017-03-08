9887f122f649930f5fd21bd8d6c3948e49cce40a
Graham Mendick <graham.mendick@gmail.com>
2017-03-08 12:07:46 +0000

    Styled the awards image

--- site/pages/about-us/awards-slice/index.js
+++ site/pages/about-us/awards-slice/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import awardsImage from './awards.jpg';
+import styles from './style.css';
 
 export default () => (
-  <img src={awardsImage} alt="Awards" />
+  <img src={awardsImage} className={styles.awardsImage} alt="Awards" />
 );
