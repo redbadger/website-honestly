b6c7692caf9e807707cbc89dfeb576fa54d86f1e
Dominik Stodolny <dom@dmk.st>
2017-03-13 12:07:32 +0000

    fix opengraph image urls

--- site/components/social/index.js
+++ site/components/social/index.js
@@ -16,11 +16,11 @@ const Social = ({ title, description, metaImage, url }: SocialProps) => (
       { name: 'twitter:site', content: '@redbadgerteam' },
       { name: 'twitter:title', content: title },
       { name: 'twitter:description', content: description },
-      { name: 'twitter:image', content: metaImage },
+      { name: 'twitter:image', content: `https://red-badger.com${metaImage}` },
       { property: 'og:type', content: 'website' },
       { property: 'og:url', content: url },
       { property: 'og:title', content: title },
-      { property: 'og:image', content: metaImage },
+      { property: 'og:image', content: `https://red-badger.com${metaImage}` },
       { property: 'og:description', content: description },
     ]}
   />
