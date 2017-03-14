0a4a7a1b33eb5b11c9a588e6fb25365b6ae41b9e
Graham Mendick <graham.mendick@gmail.com>
2017-03-14 15:18:17 +0000

    Copied code over from ft slice

---
+++ site/pages/our-work/case-study-retailer-slice/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './style.css';
+
+import retailerImage from './images/retailer.png';
+
+export default function CaseStudyRetailerSlice() {
+  return (
+    <div className={styles.caseStudyContainer} >
+      <div className={styles.caseStudyContent}>
+        <div className={styles.caseStudyTextContainer} >
+          <a href="">
+            <h2 className={styles.caseStudyTextContainerHeader}>
+              Next generation platform for retail giant
+            </h2>
+            <p className={styles.description}>
+              Find out how we helped clear a five year backlog in eight months
+              and delivered a mobile first application across seven countries.
+            </p>
+            <div className={styles.links}>
+              <p className={styles.readmore}>Read more</p>
+            </div>
+          </a>
+        </div>
+        <a className={styles.imageLink} href="">
+          <img src={retailerImage} alt="Retail giant project snapshot" />
+        </a>
+      </div>
+    </div>
+  );
+}
