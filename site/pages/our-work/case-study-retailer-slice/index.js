4ddbc000f4304cdf0cdd4f2481e0c8a9e9765015
Graham Mendick <graham.mendick@gmail.com>
2017-03-14 15:41:27 +0000

    Changed anchors to Links

--- site/pages/our-work/case-study-retailer-slice/index.js
+++ site/pages/our-work/case-study-retailer-slice/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './style.css';
+import Link from '../../../components/link';
 
 import retailerImage from './images/retailer.png';
 
@@ -7,7 +8,7 @@ export default () => (
   <div className={styles.caseStudyContainer} >
     <div className={styles.caseStudyContent}>
       <div className={styles.caseStudyTextContainer} >
-        <a href="">
+        <Link to="ourWorkPage">
           <h2 className={styles.caseStudyTextContainerHeader}>
             Next generation platform for retail giant
           </h2>
@@ -18,11 +19,11 @@ export default () => (
           <div className={styles.links}>
             <p className={styles.readmore}>Read more</p>
           </div>
-        </a>
+        </Link>
       </div>
-      <a className={styles.imageLink} href="">
+      <Link to="ourWorkPage" className={styles.imageLink}>
         <img src={retailerImage} alt="Retail giant project snapshot" />
-      </a>
+      </Link>
     </div>
   </div>
 );
