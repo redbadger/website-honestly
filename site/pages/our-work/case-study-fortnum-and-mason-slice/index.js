a7cc6aa8d6bd72fae37e5bde007dfed90b7e85c8
Graham Mendick <graham.mendick@gmail.com>
2017-03-14 15:25:31 +0000

    Refactored components to use =>

--- site/pages/our-work/case-study-fortnum-and-mason-slice/index.js
+++ site/pages/our-work/case-study-fortnum-and-mason-slice/index.js
@@ -7,30 +7,28 @@ import fmProjectMediumSnapshot from './images/fortnum-medium.png';
 
 const caseStudyUrl = '/our-work/case-study/fortnum-and-mason/';
 
-export default function CaseStudyFortnumSlice() {
-  return (
-    <div className={styles.caseStudyContainer} >
-      <div className={styles.caseStudyContent}>
-        <div className={styles.caseStudyTextContainer} >
-          <a href={caseStudyUrl}>
-            <img src={fmLogo} className={styles.clientLogo} alt="Fortnum and Mason logo" />
-            <h2 className={styles.caseStudyTextContainerHeader}>
-              Fortnum & Mason’s new, elegant website increases revenue and conversion rates
-            </h2>
-            <p className={styles.description}>
-              A scalable, highly flexible new site was needed,
-              and very quickly to avoid more lost revenue.
-            </p>
-            <div className={styles.links}>
-              <p className={styles.readmore}>Read more</p>
-            </div>
-          </a>
-        </div>
-        <a className={styles.imageLink} href={caseStudyUrl}>
-          <img className={styles.projectBigSmallSnapshot} src={fmProjectSnapshot} alt="Fortnum and Mason project snapshot" />
-          <img className={styles.projectMediumSnapshot} src={fmProjectMediumSnapshot} alt="Fortnum and Mason project snapshot" />
+export default () => (
+  <div className={styles.caseStudyContainer} >
+    <div className={styles.caseStudyContent}>
+      <div className={styles.caseStudyTextContainer} >
+        <a href={caseStudyUrl}>
+          <img src={fmLogo} className={styles.clientLogo} alt="Fortnum and Mason logo" />
+          <h2 className={styles.caseStudyTextContainerHeader}>
+            Fortnum & Mason’s new, elegant website increases revenue and conversion rates
+          </h2>
+          <p className={styles.description}>
+            A scalable, highly flexible new site was needed,
+            and very quickly to avoid more lost revenue.
+          </p>
+          <div className={styles.links}>
+            <p className={styles.readmore}>Read more</p>
+          </div>
         </a>
       </div>
+      <a className={styles.imageLink} href={caseStudyUrl}>
+        <img className={styles.projectBigSmallSnapshot} src={fmProjectSnapshot} alt="Fortnum and Mason project snapshot" />
+        <img className={styles.projectMediumSnapshot} src={fmProjectMediumSnapshot} alt="Fortnum and Mason project snapshot" />
+      </a>
     </div>
-  );
-}
+  </div>
+);
