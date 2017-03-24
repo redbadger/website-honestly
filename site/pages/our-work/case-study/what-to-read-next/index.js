d8423d329be4b1076f0d57209450a1c309c9f90b
Dominik Stodolny <dominik.stodolny@red-badger.com>
2017-03-24 17:23:55 +0000

    add financial times and camden market data

--- site/pages/our-work/case-study/what-to-read-next/index.js
+++ site/pages/our-work/case-study/what-to-read-next/index.js
@@ -4,6 +4,8 @@ import InlineSVG from 'svg-inline-react';
 import styles from './style.css';
 
 import fortnumImage from './images/fortnumImage.jpg';
+import financialTimesImage from './images/financialTimesImage.png';
+import camdenMarketImage from './images/camdenMarketImage.jpg';
 import arrowSVG from '../../../../../assets/images/SVG/arrow.svg';
 
 const cx = classnames.bind(styles);
@@ -22,21 +24,21 @@ const WhatToReadNext = () => (
       </div>
       <div className={cx('whatNext__tile', 'whatNext__tile--financialTimes')}>
         <figure>
-          <img src={fortnumImage} alt="Fortnum & Mason" className={styles.whatNext__image} />
-          <figcaption className={styles.whatNext__caption}>Fortnum &amp; Mason</figcaption>
+          <img src={financialTimesImage} alt="Financial Times" className={styles.whatNext__image} />
+          <figcaption className={styles.whatNext__caption}>Financial Times</figcaption>
         </figure>
         <h2 className={styles.whatNext__title}>
-          Elegant e-commerce in eight months
+          Lasting change for a media giant
           <InlineSVG src={arrowSVG} className={styles.whatNext__arrow} />
         </h2>
       </div>
       <div className={cx('whatNext__tile', 'whatNext__tile--camden')}>
         <figure>
-          <img src={fortnumImage} alt="Fortnum & Mason" className={styles.whatNext__image} />
-          <figcaption className={styles.whatNext__caption}>Fortnum &amp; Mason</figcaption>
+          <img src={camdenMarketImage} alt="Camden Market" className={styles.whatNext__image} />
+          <figcaption className={styles.whatNext__caption}>Camden Market</figcaption>
         </figure>
         <h2 className={styles.whatNext__title}>
-          Elegant e-commerce in eight months
+          Taking steps towards a digital future
           <InlineSVG src={arrowSVG} className={styles.whatNext__arrow} />
         </h2>
       </div>
