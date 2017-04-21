ed414efcfb2e58e4ee2748101a0ee774e9305877
Graham Mendick <graham.mendick@gmail.com>
2017-04-21 13:34:33 +0100

    Fixed padding calcs and added wrapper so it works when sized over 1400px

--- site/pages/about-us/q-and-a-slice/index.js
+++ site/pages/about-us/q-and-a-slice/index.js
@@ -6,15 +6,17 @@ import type { CategoryProps } from './category';
 
 const QAndASlice = ({ qAndAs }: { qAndAs: Array<CategoryProps> }) => (
   qAndAs.length ? <div className={styles.qAndA}>
-    <div className={styles.qAndA__container}>
-      <h2 className={styles.qAndA__heading}>Answers to common questions</h2>
-      <ul className={styles.qAndA_categoryList}>
-        {qAndAs.map(category => (
-          <li key={category.name}>
-            <Category category={category} />
-          </li>
-        ))}
-      </ul>
+    <div className={styles.qAndA__wrapper}>
+      <div className={styles.qAndA__container}>
+        <h2 className={styles.qAndA__heading}>Answers to common questions</h2>
+        <ul className={styles.qAndA_categoryList}>
+          {qAndAs.map(category => (
+            <li key={category.name}>
+              <Category category={category} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   </div> : null
 );
