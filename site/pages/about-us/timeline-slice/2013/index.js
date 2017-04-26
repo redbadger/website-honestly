981e20f2f86929899edc2d68140fbff73fbf713d
mveritym <mveritym@gmail.com>
2017-04-26 14:56:24 +0100

    2013 content

--- site/pages/about-us/timeline-slice/2013/index.js
+++ site/pages/about-us/timeline-slice/2013/index.js
@@ -3,21 +3,19 @@ import mobile from './2013-mobile.jpg';
 import Content from '../content';
 
 const Three = () => {
-  const title = '2013';
-  const text = 'As we began to grow, we realised we were outgrowing our Clerkenwell office and moved to new digs at Mallow St, right by Old St Roundabout. We also took on our biggest client so far, Sky. Not ones to let work get in the way of fun, we held a couple of amazing parties at our new HQ, which may have caused some complaints from our neighbours early on.';
-  const counters = [
-    { title: 'No of office dogs', value: '0' },
-    { title: 'Cups of tea drunk', value: '473' },
-    { title: 'Favourite tech', value: 'ElasticSearch' },
-  ];
+  const year = '2013';
+  const title = 'Winning Sky, moving to Mallow St and owning our first beer fridge';
+  const text = 'Our Clerkenwell office started to get a bit small, so we re-located to an amazing warehouse space just behind Old Street’s Tech Roundabout. We then took on Sky, our biggest client so far. During this year we also started to think about how Red Badger could operate for social good, and embarked on a pro-bono project to develop an app for Haller, a institution focused on sustainability through helping people in Africa learn essential farming skills.';
+  const fact = 'The police were called on two separate occasions during Red Badger’s first year in Mallow Street over noise complaints about our Summer and Christmas Party, respectively. Ooops.';
 
   return (
     <Content
+      year={year}
       title={title}
       text={text}
+      fact={fact}
       image={mobile}
       mobileImage={mobile}
-      counters={counters}
       flip
     />
   );
