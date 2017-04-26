b9f22c4d0fa8794c94471c75c4cfad102a8876a7
mveritym <mveritym@gmail.com>
2017-04-26 14:49:57 +0100

    2011 content

--- site/pages/about-us/timeline-slice/index.js
+++ site/pages/about-us/timeline-slice/index.js
@@ -14,7 +14,7 @@ import Six from './2016';
 import ImageMobile from './mobileImage';
 
 import Image2010 from './2010/2010.jpg';
-import Mobile2011 from './2011/2011-mobile.jpg';
+import Image2011 from './2011/2011.jpg';
 import Mobile2012 from './2012/2012-mobile.jpg';
 import Mobile2013 from './2013/2013-mobile.jpg';
 import Mobile2014 from './2014/2014-mobile.jpg';
@@ -60,7 +60,7 @@ class TimelineSlice extends React.Component {
           <div className={styles.content}>
             <img src={Image2010} alt="2010" className={styles.image} />
             <Zero />
-            <img src={Mobile2011} alt="2011" className={styles.image} />
+            <img src={Image2011} alt="2011" className={styles.image} />
             <One />
             <img src={Mobile2012} alt="2012" className={styles.image} />
             <Two />
--- site/pages/about-us/timeline-slice/mobileImage/index.js
+++ site/pages/about-us/timeline-slice/mobileImage/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SwipeableViews from 'react-swipeable-views';
 import styles from './style.css';
 import Image2010 from '../2010/2010.jpg';
-import Mobile2011 from '../2011/2011-mobile.jpg';
+import Image2011 from '../2011/2011.jpg';
 import Mobile2012 from '../2012/2012-mobile.jpg';
 import Mobile2013 from '../2013/2013-mobile.jpg';
 import Mobile2014 from '../2014/2014-mobile.jpg';
@@ -14,7 +14,7 @@ const MobileImage = ({ index, onChangeIndex }) => {
     <div className={styles.mobileImage}>
       <SwipeableViews index={index} onChangeIndex={onChangeIndex}>
         <img src={Image2010} alt="2010" className={styles.image} />
-        <img src={Mobile2011} alt="2011" className={styles.image} />
+        <img src={Image2011} alt="2011" className={styles.image} />
         <img src={Mobile2012} alt="2012" className={styles.image} />
         <img src={Mobile2013} alt="2013" className={styles.image} />
         <img src={Mobile2014} alt="2014" className={styles.image} />
