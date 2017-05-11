34eb5340035a6e901256c0d2789907e37f76d302
Dominik Stodolny <dominik.stodolny@red-badger.com>
2017-05-11 11:24:01 +0100

    update 2012 content

--- site/pages/about-us/timeline-slice/2012/index.js
+++ site/pages/about-us/timeline-slice/2012/index.js
@@ -4,8 +4,8 @@ import Content from '../content';
 
 const Two = () => {
   const year = '2012';
-  const title = 'Winning our first Node project and hiring a big gun';
-  const text = 'This is the year we decided to bring in a big gun in to help support our growth. So, Mike Altendorf, former Conchango CEO and industry veteran, was welcomed as RB’s first non-executive director. We also gained our first long-term retained client, international reinsurance broker JLT.';
+  const title = 'We won our first Node project and hired a big gun';
+  const text = 'This is the year we decided to bring in a big gun in to help support our growth.  So, Mike Altendorf, former Conchango CEO and industry veteran, was welcomed as RB’s first non-executive director.  We also gained our first long-term retained client, international insurance broker JLT.';
   const fact = 'During the course of our work with JLT, Stu wrote over 1.2 million lines of code.';
 
   return (
