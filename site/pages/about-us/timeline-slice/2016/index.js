3824ac3d229986881e409085ff9c66cf6eceb776
Dominik Stodolny <dominik.stodolny@red-badger.com>
2017-05-11 11:30:32 +0100

    update 2016 content

--- site/pages/about-us/timeline-slice/2016/index.js
+++ site/pages/about-us/timeline-slice/2016/index.js
@@ -4,9 +4,9 @@ import Content from '../content';
 
 const Six = () => {
   const year = '2016';
-  const title = 'Won our biggest client yet, although we can’t talk about it. Voted 44 in the Tech Track 100 & the arrival of Milo, the office dog';
-  const text = '2016 saw our fastest growth yet. We took on an array of new clients (but we can’t talk about the best one) brought in some incredible new members of staff and welcomed Milo, the office dog. We also reached number 44 on the Tech Track 100 list and won a lot more awards including XXX.';
-  const fact = 'To celebrate and reward staff for their hard work, Red Badger went to the countryside outside Berlin for a massive workshop/party in a ruined castle, and before celebrations kicked off we held a big workshop for all our staff to ensure they all had a voice in helping us plan 2017.';
+  const title = 'Our biggest client win, recognition in the Sunday Times and the arrival of Milo';
+  const text = '2016 saw our fastest growth yet as we reached number 44 on the Tech Track 100 list and featured in Sunday Times. We took on an array of new clients (but we can’t talk about the best one) brought in some incredible new members of staff and welcomed Milo, the office dog.';
+  const fact = 'To celebrate and reward staff for their hard work, Red Badger went to a castle near Leipzig in Germany. Before celebrations kicked off we held a workshop for all our staff to ensure they had a voice in helping us update our brand proposition and plan 2017.';
 
   return (
     <Content
