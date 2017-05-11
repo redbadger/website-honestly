eaa91cc7322d671b366392f012977b9c3e54892b
Dominik Stodolny <dominik.stodolny@red-badger.com>
2017-05-11 11:34:23 +0100

    update 2011 content

--- site/pages/about-us/timeline-slice/2011/index.js
+++ site/pages/about-us/timeline-slice/2011/index.js
@@ -4,7 +4,7 @@ import Content from '../content';
 
 const One = () => {
   const year = '2011';
-  const title = 'Our first office, our first employees and our first big client';
+  const title = 'Our first office, first employees and first client';
   const text = 'We moved into our first office in Clerkenwell and hired Sari Griffiths and Joe Stanton, our first permanent employees; both of whom continue to work and thrive at Red Badger. We also won our first big client project: developing the online presence for the BMW Museum in Munich.';
   const fact = 'This was the year that Sally, our Red Badger logo was born as a result of our first rebranding.';
 
