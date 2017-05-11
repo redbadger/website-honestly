fdbe5208fe07adea184cb043b5478148eade22b7
Dominik Stodolny <dominik.stodolny@red-badger.com>
2017-05-11 11:24:17 +0100

    update 2013 content

--- site/pages/about-us/timeline-slice/2013/index.js
+++ site/pages/about-us/timeline-slice/2013/index.js
@@ -4,9 +4,9 @@ import Content from '../content';
 
 const Three = () => {
   const year = '2013';
-  const title = 'Winning Sky, moving to Mallow St and owning our first beer fridge';
-  const text = 'Our Clerkenwell office started to get a bit small, so we re-located to an amazing warehouse space just behind Old Street’s Tech Roundabout. We then took on Sky, our biggest client so far. During this year we also started to think about how Red Badger could operate for social good, and embarked on a pro-bono project to develop an app for Haller, a institution focused on sustainability through helping people in Africa learn essential farming skills.';
-  const fact = 'The police were called on two separate occasions during Red Badger’s first year in Mallow Street over noise complaints about our Summer and Christmas Party, respectively. Ooops.';
+  const title = 'Winning Sky, moving to Mallow Street and socialising beyond the pub';
+  const text = 'Our Clerkenwell office started to get a bit small, so we re-located to an amazing warehouse space just behind Old Street’s Silicon Roundabout.  We then took on Sky, our biggest client so far. During this year we also started to think about how Red Badger could operate for social good, and embarked on a pro-bono project to develop an app for Haller, a institution focused on sustainability through helping people in Africa learn essential farming skills.';
+  const fact = 'With the team growing, we diversified from pub nights and found our voice at karaoke nights, started monthly dinner dates and formed a sporty badgers group.';
 
   return (
     <Content
