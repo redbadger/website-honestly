c57fe02ab6bec46b04f5a4990ee131732c2f7609
Tom Barwick <tom_barwick@hotmail.com>
2017-06-05 13:37:51 +0100

    Fixes lint issue

--- site/pages/technology/index.js
+++ site/pages/technology/index.js
@@ -5,20 +5,24 @@ import styles from './style.css';
 import techRoundTableImage from './images/techroundtable.png';
 import arrowSVG from '../../../assets/images/SVG/arrow.svg';
 
-export default function Technology() {
-  return (
-    <div className={styles.technologyPage}>
-      <section className={styles.intro}>
-        <img className={styles.techRoundTable} src={techRoundTableImage} alt="tech roundtable" />
-        <h1 className={styles.mainHeader}>Technology</h1>
-        <p className={styles.description}>
-          Technology does not stand still and neither do we. On a regular basis we get our whole
-          tech team together and review all the tech we have used across all our projects to derive
-          what is hot and what is not in our world of tech.
-        </p>
-        <a className={styles.latestRoundTableLink}>Read our latest tech round table</a>
-        <InlineSVG src={arrowSVG} className={styles.arrow} />
+export default () => (
+  <div className={styles.technologyPage}>
+    <section className={styles.intro}>
+      <section className={styles.leftContent}>
+        <div>
+          <h1 className={styles.mainHeader}>Technology</h1>
+          <p className={styles.description}>
+            Technology does not stand still and neither do we. On a regular basis we get our whole
+            tech team together and review all the tech we have used across all our projects to
+            derive what is hot and what is not in our world of tech.
+          </p>
+          <a className={styles.latestRoundTableLink}>Read our latest tech round table</a>
+          <InlineSVG src={arrowSVG} className={styles.arrow} />
+        </div>
       </section>
-    </div>
-  );
-}
+      <section className={styles.rightContent}>
+        <img className={styles.techRoundTable} src={techRoundTableImage} alt="tech roundtable" />
+      </section>
+    </section>
+  </div>
+);
