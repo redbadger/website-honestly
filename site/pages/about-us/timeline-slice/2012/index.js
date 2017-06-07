20746a7e683ad03f158f96e4e3046470c142590d
gregdorward <greg.dorward@red-badger.com>
2017-06-07 15:39:48 +0100

    small correction to a sentence in the Timeline slice

--- site/pages/about-us/timeline-slice/2012/index.js
+++ site/pages/about-us/timeline-slice/2012/index.js
@@ -6,7 +6,7 @@ const Two = () => {
   const year = '2012';
   const title = 'We won our first Node project and hired a big gun';
   const text =
-    'This is the year we decided to bring in a big gun in to help support our growth.  So, Mike Altendorf, former Conchango CEO and industry veteran, was welcomed as RB’s first non-executive director.  We also gained our first long-term retained client, international insurance broker JLT.';
+    'This is the year we decided to bring in a big gun to help support our growth.  So, Mike Altendorf, former Conchango CEO and industry veteran, was welcomed as RB’s first non-executive director.  We also gained our first long-term retained client, international insurance broker JLT.';
   const fact = 'During the course of our work with JLT, Stu wrote over 1.2 million lines of code.';
 
   return (
