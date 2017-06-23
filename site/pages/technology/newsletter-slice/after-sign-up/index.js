110079a76df34cc7f3cd8bfea534977003bb852e
Tom Barwick <tom_barwick@hotmail.com>
2017-06-23 14:53:10 +0100

    Starts adding newsletter form over webinar

---
+++ site/pages/technology/newsletter-slice/after-sign-up/index.js
@@ -0,0 +1,156 @@
+import React, { Component } from 'react';
+import classnames from 'classnames/bind';
+import styles from '../style.css';
+
+const cx = classnames.bind(styles);
+const { func, string, bool } = React.PropTypes;
+
+const AfterSignUpForm = ({ errorMessage, handleInputChange, submitting, handleSubmit }) =>
+  <div>
+    <h2 className={styles.subTitle}>
+      Help us make sure your BadgerNews is relevant by telling us a bit more about yourself
+    </h2>
+    <form className={styles.form}>
+      <div>
+        <label htmlFor="name" className={styles.formLabel}>Name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          placeholder="John"
+          className={cx({
+            formInput: true,
+            afterFormError: errorMessage,
+          })}
+          onChange={handleInputChange}
+        />
+        {errorMessage &&
+          !submitting &&
+          <div className={styles.afterErrorText}>
+            {errorMessage}
+          </div>}
+      </div>
+      <div>
+        <label htmlFor="surname" className={styles.formLabel}>Surname</label>
+        <input
+          id="surname"
+          name="surname"
+          type="text"
+          placeholder="Smith"
+          className={styles.formInput}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="company" className={styles.formLabel}>Company</label>
+        <input
+          id="company"
+          name="company"
+          type="text"
+          placeholder="Peter Pan Ltd."
+          className={styles.formInput}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="job" className={styles.formLabel}>Job role</label>
+        <input
+          id="role"
+          name="role"
+          type="text"
+          placeholder="Engineer"
+          className={styles.formInput}
+          onChange={handleInputChange}
+        />
+      </div>
+      <button
+        className={cx({
+          submitButton: true,
+          buttonSubmitting: submitting,
+        })}
+        onClick={e => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        {submitting ? 'Updating info....' : 'Update info'}
+      </button>
+    </form>
+  </div>;
+
+AfterSignUpForm.propTypes = {
+  errorMessage: string.isRequired,
+  handleInputChange: func.isRequired,
+  submitting: bool.isRequired,
+  handleSubmit: func.isRequired,
+};
+
+export default class AfterSignup extends Component {
+  static propTypes = {
+    onSubmit: func.isRequired,
+    updatedFormSubmitted: bool.isRequired,
+    errorMessage: string.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      company: '',
+      role: '',
+      submitting: false,
+    };
+  }
+
+  componentDidMount() {
+    this.triggerReflow();
+  }
+
+  componentWillReceiveProps() {
+    this.setState({
+      submitting: false,
+    });
+  }
+
+  handleInputChange = event => {
+    const newState = {};
+    newState[event.target.name] = event.target.value;
+    this.setState(newState);
+  };
+
+  handleSubmit = () => {
+    this.setState({
+      submitting: true,
+    });
+    this.props.onSubmit(this.state);
+  };
+
+  // fixes bug in safari where the component height wouldn't update
+  triggerReflow() {
+    this.element.style.display = 'none';
+    this.element.offsetHeight; // eslint-disable-line no-unused-expressions
+    this.element.style.display = '';
+  }
+
+  render() {
+    return (
+      <section
+        className={cx('newsletter', 'submitted')}
+        ref={c => {
+          this.element = c;
+        }}
+      >
+        <h1 className={styles.title}>
+          Thanks for signing up!
+        </h1>
+        {!this.props.updatedFormSubmitted &&
+          <AfterSignUpForm
+            errorMessage={this.props.errorMessage}
+            handleInputChange={this.handleInputChange}
+            submitting={this.state.submitting}
+            handleSubmit={this.handleSubmit}
+          />}
+      </section>
+    );
+  }
+}
---
+++ site/pages/technology/newsletter-slice/index.js
@@ -0,0 +1,96 @@
+/* eslint-disable camelcase */
+import React, { Component } from 'react';
+import NewsletterAfterSignUp from './after-sign-up/';
+import NewsletterBeforeSignUp from './before-sign-up/';
+
+export function isValidEmail(email) {
+  const regex = /.+@.+\..+/;
+  return regex.test(email);
+}
+
+const fetchFunction = ({ url, body, method }) =>
+  fetch(url, {
+    method,
+    headers: { 'Content-Type': 'application/json' },
+    mode: 'cors',
+    body,
+  });
+
+class NewsLetter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newsletterSubmitted: false,
+      email_address: '',
+      errorMessage: '',
+      updatedFormSubmitted: false,
+    };
+  }
+
+  submitForm = (formDataJSON, method, fetchFn = fetchFunction) => {
+    return fetchFn({
+      url: process.env.MAILING_LIST_SERVICE_URL,
+      method,
+      body: formDataJSON,
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(res => {
+        return this.setState(res);
+      })
+      .catch(() => {
+        return this.setState({
+          newsletterSubmitted: false,
+          errorMessage: 'Unfortunately there was an error, please try again later',
+        });
+      });
+  };
+
+  signUpUser = data => {
+    if (data.email_address === '') {
+      this.setState({
+        errorMessage: 'Please enter an email address',
+      });
+    } else if (!isValidEmail(data.email_address)) {
+      this.setState({
+        errorMessage: 'Please enter a valid email address',
+      });
+    } else {
+      return this.submitForm(JSON.stringify(data), 'POST');
+    }
+  };
+
+  updateUser = data => {
+    const object = Object.assign({}, data);
+    object.email_address = this.state.email_address;
+    if (!data.name) {
+      return this.setState({
+        newsletterSubmitted: true,
+        email_address: this.state.email_address,
+        errorMessage: 'Please tell us your name',
+      });
+    }
+    return this.submitForm(JSON.stringify(object), 'PATCH');
+  };
+
+  render() {
+    const AfterSignup = (
+      <NewsletterAfterSignUp
+        onSubmit={this.updateUser}
+        updatedFormSubmitted={this.state.updatedFormSubmitted}
+        errorMessage={this.state.errorMessage}
+      />
+    );
+    const BeforeSignup = (
+      <NewsletterBeforeSignUp onSubmit={this.signUpUser} errorMessage={this.state.errorMessage} />
+    );
+    return (
+      <div>
+        {this.state.newsletterSubmitted ? AfterSignup : BeforeSignup}
+      </div>
+    );
+  }
+}
+
+export default NewsLetter;
