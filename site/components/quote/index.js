b46be3f8800e5f05ab620a93c36275ff51bc70d0
Cristian Wilgenhoff <cristian.wilgenhoff@red-badger.com>
2017-07-12 17:48:21 +0100

    Extracted quote component

--- site/components/quote/index.js
+++ site/components/quote/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import classnames from 'classnames/bind';
 
 import styles from './style.css';
-
-const cx = classnames.bind(styles);
 
 const QuoteProps = {
   author: Object,
   text: String,
 };
 
-const Quote = ({ author, text } : QuoteProps) => {
+const Quote = ({ author, text }: QuoteProps) => {
   return (
     <div className={styles.quotation}>
       <blockquote>
@@ -23,11 +20,11 @@ const Quote = ({ author, text } : QuoteProps) => {
         <img src={author.image} alt={author.name} />
         <div className={styles.quotation__author__wrapper}>
           <span className={styles.quotation__author__name}>— {author.name}</span>
-          <span  className={styles.quotation__author__title}>{author.title}</span>
+          <span className={styles.quotation__author__title}>{author.title}</span>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Quote;
