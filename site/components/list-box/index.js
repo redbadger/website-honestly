fdb276e0d8de8bb2fc52edeb5ee28efb2d1e4d07
Martin Hartt <martinhartt1@gmail.com>
2017-07-17 17:09:28 +0100

    Add styling option to listbox

--- site/components/list-box/index.js
+++ site/components/list-box/index.js
@@ -9,20 +9,26 @@ const cx = classnames.bind(styles);
 type ListBoxProps = {
   title: string,
   items: Array<Object>,
+  itemClassName?: string,
+  labelClassName?: string,
+  valueClassName?: string,
 };
 
-const ListBox = ({ title, items }: ListBoxProps) => {
+const ListBox = ({ title, items, itemClassName, labelClassName, valueClassName }: ListBoxProps) => {
   return (
     <div className={styles.listBox}>
       <h3 className={styles.listBox__heading}>{title}</h3>
       <ul>
         {items.map((item, index) => {
           return (
-            <li key={index} className={cx('listBox__element', 'listBox__element--results')}>
-              <div>
+            <li
+              key={index}
+              className={cx('listBox__element', 'listBox__element--results', itemClassName)}
+            >
+              <div className={labelClassName}>
                 {item.label}
               </div>
-              <div>
+              <div className={valueClassName}>
                 {item.value}
               </div>
             </li>
