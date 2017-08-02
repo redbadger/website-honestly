3fe3794243d154a0db7eb4368bae16f5540a31ec
Matt Paul <mattpaul001@icloud.com>
2017-08-02 11:59:21 +0100

    Added console statement to verify that interests lambda is called with correct interest

--- services/mailchimp/sign-up/index.js
+++ services/mailchimp/sign-up/index.js
@@ -3,12 +3,13 @@
 import { mailchimpApi, formatFormInput, formatSignUpResponse } from '../utilities';
 
 export default function signUp(event, _, cb) {
+  console.log(event);
   const mailingListId = process.env.MAILING_LIST_ID;
   const body = formatFormInput(event);
   return mailchimpApi(
     `https://us6.api.mailchimp.com/3.0/lists/${mailingListId}/members/`,
     'POST',
-    JSON.stringify(body),
+    JSON.stringify(body)
   )
     .then(json => {
       console.log('sign up service post ok:', json);
