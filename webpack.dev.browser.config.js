a81d66046cf56e173ccb2681d0ab8e428485c75f
Chris Caller <callerc1@gmail.com>
2017-08-11 14:20:12 +0100

    add Named modules plugin to dev and disabled extract text plugin on dev for proper HMS with webpack dev server

--- webpack.dev.browser.config.js
+++ webpack.dev.browser.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const baseConfig = require('./webpack.base.config');
 const webpackMerge = require('webpack-merge').smart;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -5,17 +6,17 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const devAppConfig = webpackMerge(baseConfig, {
   entry: {
-    '/dev-app': ['babel-polyfill', './dev/browser-app/index.js'],
+    'dev-app': ['babel-polyfill', './dev/browser-app/index.js'],
   },
   target: 'web',
   plugins: [
+    new webpack.NamedModulesPlugin(),
     new HtmlWebpackPlugin({
       template: './site/index.ejs',
     }),
-    new ExtractTextPlugin(
-      '/assets-honestly/styles-[contenthash:base64:5].css',
-      { allChunks: true }
-    ),
+    new ExtractTextPlugin({
+      disable: true,
+    }),
   ],
 });
 
