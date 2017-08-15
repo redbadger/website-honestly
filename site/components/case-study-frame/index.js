94c998e04079e6852d6936052d3d62121d903ccc
Martin Hartt <martinhartt1@gmail.com>
2017-08-15 14:30:48 +0100

    Create frame component

---
+++ site/components/case-study-frame/index.js
@@ -0,0 +1,17 @@
+// @flow
+import React from 'react';
+
+import styles from './style.css';
+
+type CaseStudyFrameProps = {
+  children: Node,
+};
+
+const CaseStudyFrame = ({
+  children,
+}: CaseStudyFrameProps) =>
+  <div className={styles.caseStudy}>
+    {children}
+  </div>;
+
+export default CaseStudyFrame;
