3ea7f6d6ffae6863c03080d0612da7a3a28ab546
Martin Hartt <martinhartt1@gmail.com>
2017-08-15 14:31:35 +0100

    Integrate components

--- site/pages/our-work/case-study/bmw/index.js
+++ site/pages/our-work/case-study/bmw/index.js
@@ -1,7 +1,9 @@
 // @flow
 import React from 'react';
 
-import GenericCaseStudy from '../../../../components/generic-case-study';
+import CaseStudyFrame from '../../../../components/case-study-frame';
+import CaseStudyHeader from '../../../../components/case-study-header';
+import CaseStudySection from '../../../../components/case-study-section';
 import PullQuote from '../../../../components/pull-quote';
 import headerImage from './images/header.jpg';
 
@@ -17,15 +19,20 @@ const social = {
 };
 
 const BMWCaseStudy = ({ contactUsURL }: BMWCaseStudyProps) =>
-  <GenericCaseStudy
-    title="BMW Virtual Museum: The shortcut between you and the museum"
-    headerImage={headerImage}
-    headerImageAlt="image"
-    headerColor="#f4f1f2"
-    contactUsURL={contactUsURL}
-    social={social}
-  >
-    <p>Hello!</p>
+  <CaseStudyFrame>
+    <CaseStudyHeader
+      title="BMW Virtual Museum: The shortcut between you and the museum"
+      headerImage={headerImage}
+      headerImageAlt="image"
+      headerColor="#f4f1f2"
+      contactUsURL={contactUsURL}
+      social={social}
+    />
+    <CaseStudySection title="The challenge">
+      <p>The BMW Museum in Munich, Germany was opened in 1973 as one of the first brand museums. Displayed over 5,000 square metres of exhibition space are around 125 of the brand’s most precious and appealing cars, motorcycles and engines.
+The Museum is a living building, comprising many innovative and immersive exhibitions that exude BMW’s passion for technical innovation. BMW wanted to live that brand through the museum’s web presence, and turned to Red Badger to discover how.</p>
+    </CaseStudySection>
+
     <PullQuote
       author={{
         name: 'Martin Hartt',
@@ -33,6 +40,9 @@ const BMWCaseStudy = ({ contactUsURL }: BMWCaseStudyProps) =>
       }}
       text="I like coffee."
     />
-  </GenericCaseStudy>;
+    <CaseStudySection title="Okay">
+      <p>Nope</p>
+    </CaseStudySection>
+  </CaseStudyFrame>
 
 export default BMWCaseStudy;
