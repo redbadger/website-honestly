8552619949e88a4321b16fd00ecbc6caeecf6d17
Martin Hartt <martinhartt1@gmail.com>
2017-08-15 14:30:57 +0100

    Create section component

---
+++ site/components/case-study-section/index.js
@@ -0,0 +1,22 @@
+// @flow
+import React from 'react';
+
+import styles from './style.css';
+
+type CaseStudySectionProps = {
+  title: string,
+  children: Node[],
+}
+
+const CaseStudySection = ({
+  title,
+  children,
+}: CaseStudySectionProps) =>
+  <div className={styles.content}>
+    <div className={styles.content__wrapper}>
+      <h2>{title}</h2>
+      {children}
+    </div>
+  </div>;
+
+export default CaseStudySection;
