209811367890cfec1dab26ada7cf9dac897b8cc5
Martin Hartt <martinhartt1@gmail.com>
2017-08-23 16:12:38 +0100

    Run lint

--- site/components/events-banner/index.js
+++ site/components/events-banner/index.js
@@ -8,7 +8,11 @@ import mobileBanner from './images/events-mobile-banner.png';
 export default function EventsBanner() {
   return (
     <div className={styles.bannerContainer}>
-      <a href="https://attendee.gotowebinar.com/register/4215309162791382274?source=Rb+event+site+banner" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://attendee.gotowebinar.com/register/4215309162791382274?source=Rb+event+site+banner"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img
           src={desktopBanner}
           alt="Are you bold with technology? Join our webinar"
