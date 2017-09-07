cc02a040cbcc6265ed1fdcb994f860fb4cc5bdec
Chris Caller <callerc1@gmail.com>
2017-09-07 14:54:30 +0100

    Added some nice logging to deploy script

--- bin/ci-deploy.sh
+++ bin/ci-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+. ./bin/pretty-output.sh
 #
 # Create a new preview site namespaced under the current commit SHA
 #
@@ -10,15 +11,17 @@ createCommitSite() {
   export ENVIRONMENT_NAME="staging"
   export URL_BASENAME="$COMMIT_REF/"
 
-
-  echo Deploying commit preview site to $URL_BASENAME
+  print_block "Deploying commit preview site to $URL_BASENAME"
   make clean
   make fetch
   make dev-commit
+  success "Build complete!"
 
+  print_block "Setting Version"
   echo $LAST_COMMIT_INFO > ./dist/version.txt
+  success "Version Set: $cYellow $LAST_COMMIT_INFO $cNo"
 
-  echo Copying assets to S3
+  print_block "Copying assets to S3"
   aws s3 sync ./dist/assets-honestly s3://$BUCKET_NAME/$COMMIT_REF/assets-honestly
   aws s3 cp ./dist/manifest.json s3://$BUCKET_NAME/$COMMIT_REF/manifest.json
   aws s3 cp ./dist/sitemap.xml s3://$BUCKET_NAME/$COMMIT_REF/sitemap.xml
@@ -26,32 +29,52 @@ createCommitSite() {
   aws s3 cp ./dist/robots.txt s3://$BUCKET_NAME/$COMMIT_REF/robots.txt
   aws s3 sync ./dist/static-site/$COMMIT_REF/ s3://$BUCKET_NAME/$COMMIT_REF/
   aws s3 cp ./dist/version.txt s3://$BUCKET_NAME/$COMMIT_REF/version.txt
-  echo Done!
+  success "Assets Deployed to S3!"
 
-  echo Registering deployment with GitHub
+  print_block "Registering deployment with GitHub"
   ./bin/register-github-deployment.js $COMMIT_REF
-  echo Done!
+  success "Deployment Registered!\n"
+
+  print_block "  🦄     Done!    🦄   "
+  cat assets/badger-liftoff.txt
+  success "Preview Site Deployed 🚀 !"
 }
 
 deployMaster() {
   LAST_COMMIT_INFO=$(git log -1 --format='%h %cd')
-  echo Deploying current master to $1
+  print_block "Deploying current master to $1"
   make clean
   make build
+  success "Build complete!"
 
+  print_block "Setting Version"
   echo $LAST_COMMIT_INFO > ./dist/version.txt
+  success "Version Set: $cYellow $LAST_COMMIT_INFO $cNo"
 
-  echo Copying assets to S3
+  print_block "Copying assets to S3"
   aws s3 sync ./dist/assets-honestly s3://$BUCKET_NAME/assets-honestly
   aws s3 cp ./dist/manifest.json s3://$BUCKET_NAME/manifest.json
   aws s3 cp ./dist/sitemap.xml s3://$BUCKET_NAME/sitemap.xml
   aws s3 cp ./dist/googlef362fe4b545e4cfb.html s3://$BUCKET_NAME/googlef362fe4b545e4cfb.html
   aws s3 cp ./dist/robots.txt s3://$BUCKET_NAME/robots.txt
   aws s3 cp ./dist/version.txt s3://$BUCKET_NAME/version.txt
-  make services-deploy
+  success "Assets Deployed to S3!"
+
+  print_block "Uploading services to AWS Lambda"
+  make services-deploy #Note : since services.zip has already been built (via make build) it will not be built again.
+  success "Services Deployed!"
+
+  print_block "Invoking Publish Service on AWS Lambda"
   make publish-service-invoke
+  success "Publish Invoked!"
+
+  print_block "Registering release with GitHub"
   ./bin/register-github-release.js $1
-  echo Done!
+  success "Release Registered!\n"
+
+  print_block "  🦄     Done!    🦄   "
+  cat assets/badger-liftoff.txt
+  success "Deployed to $1! 🚀 \n"
 }
 
 case "$1" in
