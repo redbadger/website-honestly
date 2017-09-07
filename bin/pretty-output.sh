d6d2261e1a1a5721aae0dc9f87776737aada4cc5
Chris Caller <callerc1@gmail.com>
2017-09-07 17:40:39 +0100

    try defining colors via codes over tput

--- bin/pretty-output.sh
+++ bin/pretty-output.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# color output
+cNo="\e[0m"
+cRed="\e[31m"
+cGreen="\e[32m"
+cYellow="\e[33m"
+cBlue="\e[34m"
+cMagenta="\e[35m"
+cCyan="\e[36m"
+cWhite="\e[97m"
+
 print_block () {
   local i="$1" ; size=${#i}
   ((size+=2))
