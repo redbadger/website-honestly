ae4a4d51a721d8080768ac569656d973f61a404d
Martin Hartt <martinhartt1@gmail.com>
2017-09-14 16:23:26 +0100

    Add retry when sitemap gen fails

--- dev/generate-sitemap/generate.js
+++ dev/generate-sitemap/generate.js
@@ -12,22 +12,35 @@ const writeFile = promisify(fs.writeFile);
 const oldBadgerBrainHost = process.env.BADGER_BRAIN_HOST;
 process.env.BADGER_BRAIN_HOST = 'https://brain.red-badger.com/graphql';
 
-console.log('Generating sitemap...');
-getSiteState()
-  .then(state => {
-    return Promise.all([formatRoutes(getSiteRoutes(state, routeDefinitions)), getBlogXML()]);
-  })
-  .then(([siteXML, blogXML]) => {
-    const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
-${siteXML}
-${blogXML}
-</urlset>`;
+console.log('Fetching routes...');
+function generate(triesLeft = 5) {
+  getSiteState()
+    .then(state => {
+      return Promise.all([formatRoutes(getSiteRoutes(state, routeDefinitions)), getBlogXML()]);
+    })
+    .then(([siteXML, blogXML]) => {
+      console.log('Routes were fetched!');
+      const xml = `<?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+      ${siteXML}
+      ${blogXML}
+      </urlset>`;
 
-    console.log('Creating sitemap file...');
-    writeFile('assets/sitemap.xml', xml);
-    console.log('Sitemap completed ✅');
-  })
-  .catch(err => console.error('⚠️ Something went wrong during sitemap generation:\n', err));
+      console.log('Creating sitemap file...');
+      writeFile('assets/sitemap.xml', xml);
+      console.log('Sitemap completed ✅');
+    })
+    .catch(err => {
+      console.error(`⚠️ Sitemap generation error: ${err.message}.\n\n Trying again (${triesLeft} tries left)`);
+      if (triesLeft < 1) {
+        console.error('Unable to generate sitemap!');
+        process.exit(1);
+      } else {
+        return generate(triesLeft - 1);
+      }
+    })
+}
+
+generate();
 
 process.env.BADGER_BRAIN_HOST = oldBadgerBrainHost;
