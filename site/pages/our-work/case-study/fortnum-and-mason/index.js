b97e8f80ed2b45238c36765311a77054b582b11c
Dan Train <dan.train@red-badger.com>
2017-11-10 15:48:08 +0000

    Add preliminary fortnum case study copy

--- site/pages/our-work/case-study/fortnum-and-mason/index.js
+++ site/pages/our-work/case-study/fortnum-and-mason/index.js
@@ -3,8 +3,6 @@ import classnames from 'classnames/bind';
 import Social from '../../../../components/social';
 import styles from './style.css';
 
-import headerImage from './images/header.jpg';
-import techGraphImage from './images/tech-graph.jpg';
 import metaImage from './images/meta-image.png';
 
 import WhatToReadNext from '../what-to-read-next';
@@ -29,177 +27,126 @@ const CaseStudyFortnumAndMason = ({ contactUsURL }: CaseStudyFortnumAndMasonProp
   <div className={styles.caseStudy}>
     <Social {...social} />
     <div className={styles.header}>
-      <div className={styles.header__container}>
-        <div className={styles.header__imageContainer}>
-          <img src={headerImage} alt="Retailer shopping cart" className={styles.header__image} />
-        </div>
-        <div className={cx('header__outcome', 'header__outcome--right')}>
-          <span className={styles.header__outcome__percentage}>+54%</span>
-          <h3 className={styles.header__outcome__description}>Increased orders</h3>
-        </div>
-        <div className={cx('header__outcome', 'header__outcome--left')}>
-          <span className={styles.header__outcome__percentage}>+29%</span>
-          <h3 className={styles.header__outcome__description}>Uplift in visits</h3>
-        </div>
-      </div>
+      <div className={styles.header__container} />
     </div>
     <div className={styles.content}>
       <div className={styles.content__wrapper}>
         <h1 className={styles.content__mainTitle}>
-          Fortnum & Mason’s new, elegant website increases revenue and conversion rates{' '}
+          Fortnum & Mason’s new, elegant website increases revenue and conversion rates
         </h1>
         <h2 className={styles.content__title}>
           <span className={styles.content__redTitle}>{"Let's make things better."}</span>
-          Solving high drop-out rates
+          Scaling the site, decreasing drop-outs and bringing the in-store experience, online
         </h2>
         <p className={styles.content__paragraph}>
-          Our client, one of the largest retailers in the UK was experiencing lost revenue in the
-          millions, due to higher than expected drop-out rates on its online platform.
+          When you think of Fortnum & Mason, the iconic eau di nil colour, the beautiful store
+          layout, amazing customer service and the incredibly high quality of products, springs to
+          mind. This unrivalled reputation, held over 300 years, has always been led by the store
+          experience. But with more and more shoppers heading online to make purchases from over 130
+          different countries, Fortnum’s realised their digital experience didn’t match the service
+          delivered by the physical stores, and something needed to change, quickly.
         </p>
         <p className={styles.content__paragraph}>
-          This was because the online experience and functionality were inconsistent. The business
-          was spending huge sums on a piece of software that scraped its website, and then created
-          mobile optimised versions on the fly. This meant that some orders were easier to place
-          than others - depending on what device was being used - and amendments to orders were down
-          as they were incredibly complicated to change once booked.
+          The old site had an inconsistent design, was inflexible, difficult to update and the
+          underlying technology was nearing the end of its life. The out-dated technology meant that
+          high levels of customers were dropping out at certain stages in the buying process. For
+          example, the presentation of delivery options was confusing and if one element of a
+          customer’s order was unavailable, it could result in the entire order not going out for
+          delivery.
         </p>
         <p className={styles.content__paragraph}>
-          As a retailer dedicated to providing value to customers and seeing its online business
-          growing substantially - including a 100% year-on-year mobile traffic increase - there was
-          an urgent need for the site to work seamlessly. The business needed to build a modern
-          technology platform using cutting-edge, web-based tech to ensure all experiences for
-          customers were intuitive and easy to use, regardless of device. This would also have to
-          factor in processing a catalogue of hundreds-of-thousands of products, delving into
-          complex user journeys, and building a well executed international shipping function.
+          In addition, this site needed to solve the above problems as well as handle complex orders
+          and delivery to more than 130 countries. A scalable, highly flexible new site was needed,
+          and very quickly to avoid more lost revenue.
         </p>
-        <p className={styles.content__paragraph}>
-          As a business that championed innovation this retailer decided that Red Badger was the
-          right partner to help it make things better.
-        </p>
-        <h2 className={styles.content__title}>
-          <span className={styles.content__redTitle}>Do the right thing. Do the thing right.</span>
-          Streamlining teams and implementing cutting-edge tech
-        </h2>
-        <h3 className={styles.content__secondaryTitle}>Embracing organisational change</h3>
-        <p className={styles.content__paragraph}>
-          Although change is happening, big companies are still watching the speed at which today’s
-          start-ups move in awe and envy. What start-ups do right is work in a way that puts the
-          customer at the very heart of projects, from technology choices to design and content.
-          They then work in a collaborative and Lean way to get new products out at speeds that seem
-          staggering to large organisations.
-        </p>
-        <p className={styles.content__paragraph}>
-          Red Badger enabled this large retail client to work with the agility of a start-up:
-          re-engineering and revolutionising its workflow with Lean practices and helping it break
-          down department silos by keeping design, development and testing in one place. The
-          benefits of this included:
-        </p>
-        <ul className={styles.content__list}>
-          <li className={styles.content__list__element}>A shorter feedback loop</li>
-          <li className={styles.content__list__element}>
-            Team-wide responsibility for each decision
-          </li>
-          <li className={styles.content__list__element}>Elimination of waste</li>
-          <li className={styles.content__list__element}>
-            Driven determination and focus in order to deliver value to customers, faster
-          </li>
-          <li className={styles.content__list__element}>
-            A sped-up delivery, over four years early
-          </li>
-        </ul>
-      </div>
-      <div>
-        <img src={techGraphImage} className={styles.techGraphImage} alt="Tech graph" />
       </div>
       <div className={styles.content__wrapper}>
-        <h3 className={styles.content__secondaryTitle}>Finding the right tech for the job</h3>
-        <p className={styles.content__paragraph}>
-          Now working in a collaborative and Lean way with Red Badger, it was time to find the right
-          tech for the job.
-        </p>
-        <p className={styles.content__paragraph}>
-          Red Badger worked very closely with the retailer’s engineering team to recommend and
-          introduce an incredibly progressive selection of cutting-edge technology. The new mobile
-          site had to replace the existing screen-scraping solution and improve the customer
-          experience, making the new site adaptable, flexible and able to scale up to the
-          ever-growing demand that the business was seeing.
-        </p>
-        <ContactBox />
-        <p className={styles.content__paragraph}>
-          By using a combination of Node.js and React.js, the team were able to develop an app that
-          delivers a consistent experience across all devices. It also means that the application is
-          fast and responsive, delivering far better usability for customers.
-        </p>
-        <p className={styles.content__paragraph}>
-          On top of this, by moving into the Cloud, using advanced technology such as Docker
-          Containers, AWS ECS and Terraform, the ecommerce store was able to incorporate auto cloud
-          scaling which expands and contracts resource allocation to match customer demand. This is
-          vital in helping to cope with the huge growth in revenue and transaction volumes
-          experienced by the retailer, especially around times of peak demand such as Christmas and
-          Black Friday.
-        </p>
-        <p className={styles.content__paragraph}>
-          Finally, on the tech front, by building Automated Test and Continuous Deployment
-          Pipelines, new features are enabled to be constantly trickled into production, continually
-          delivering new value to customers and substantially reducing risk. Previously, the retail
-          giant released quarterly but can now release multiple times a day with deployments taking
-          minutes, rather than days.
-        </p>
         <h2 className={styles.content__title}>
-          <span className={styles.content__redTitle}>Creating lasting change.</span>
-          Improving conversion rates by 83%
+          <span className={styles.content__redTitle}>
+            {'Do the right thing. Do the thing right.'}
+          </span>
+          Embracing organisational change and implementing cutting-edge technology
         </h2>
         <p className={styles.content__paragraph}>
-          Successfully using the technologies above has solved long-standing issues and
-          fundamentally influenced the retailer’s next generation platform. Where legacy systems
-          previously ruled, cutting-edge innovation allows its online business to grow unencumbered
-          by technical limitations. The new platform now provides this retailer with the means to
-          scale for years to come.
+          We began with a two-day hackathon to produce a minimum viable product (MVP) to demonstrate
+          our thinking for the new website; a ‘show not tell’ approach.
         </p>
         <p className={styles.content__paragraph}>
-          And, by first focusing on delivering the application for mobile only, Red Badger has
-          delivered a completed application across seven countries in just eight months, the results
-          of which are staggering:
+          Working in a collaborative, Lean way with Fortnum & Mason, our talented design team and
+          knowledgeable tech team, incorporated a progressive selection of open source technology
+          including Spree Commerce, Wombat and Facebook’s React.
+        </p>
+        <p className={styles.content__paragraph}>
+          We also designed, implemented and managed the Cloud-based architecture upon which the new
+          platform runs. This design allows for a continuous delivery model, meaning that tiny
+          incremental changes are constantly tested and released to production multiple times a day
+          (instead of a few times a year), helping to bid farewell to the age of big bang releases.
+        </p>
+        <p className={styles.content__paragraph}>
+          The process and technology had the customer at the heart of the project but the key to the
+          success was engaging Fortnum’s customers to help drive the direction of the design and
+          development of the site by allowing them to provide feedback directly to the team.
+          Throughout the project, our design team ran guerrilla testing in the flagship store in
+          Piccadilly to get customer feedback on the look and feel of the site. The in-store
+          historian was also brought into the project to ensure the new design was as aligned to the
+          brand’s heritage as possible.
+        </p>
+        <p className={styles.content__paragraph}>
+          Once launched, the beta site ran in parallel to the old site to avoid disruption and a
+          number of Fortnum’s customers were gradually switched over to the new site over a
+          two-month period. New site visitors were able to provide feedback to enable further
+          customer-centric amends and builds.
+        </p>
+        <p className={styles.content__paragraph}>
+          These changes meant that when the full go-live launch took place the site already had
+          features and processes in place that made sense to the user, not just the business. This
+          approach also meant that overall the project has achieved a complete re-platform and
+          design from concept through to delivery, in just eight months and for under £1.3 milllion.
         </p>
       </div>
-      <div className={styles.overview}>
-        <div className={styles.listBox}>
-          <h3 className={styles.listBox__heading}>What we did</h3>
-          <ul>
-            <li className={styles.listBox__element}>Mobile first application across 7 countries</li>
-            <li className={styles.listBox__element}>Introduced Lean & Agile practices</li>
-            <li className={styles.listBox__element}>Built Continuous Deployment Pipelines</li>
-            <li className={styles.listBox__element}>Moved to the Cloud</li>
-            <li className={styles.listBox__element}>
-              Introduced automated testing & cutting edge tech
-            </li>
-          </ul>
-        </div>
-        <div className={styles.listBox}>
-          <h3 className={styles.listBox__heading}>Results</h3>
-          <ul>
-            <li className={cx('listBox__element', 'listBox__element--results')}>
-              <div>Uplift in visits</div>
-              <div>+29%</div>
-            </li>
-            <li className={cx('listBox__element', 'listBox__element--results')}>
-              <div>Increased orders</div>
-              <div>+54%</div>
-            </li>
-            <li className={cx('listBox__element', 'listBox__element--results')}>
-              <div>Customers amending orders</div>
-              <div>+443%</div>
-            </li>
-            <li className={cx('listBox__element', 'listBox__element--results')}>
-              <div>Conversion rates including amendments</div>
-              <div>+83%</div>
-            </li>
-            <li className={cx('listBox__element', 'listBox__element--results')}>
-              <div>Conversion rates excluding amendments</div>
-              <div>+18%</div>
-            </li>
-          </ul>
-        </div>
+      <div className={styles.content__wrapper}>
+        <h2 className={styles.content__title}>
+          <span className={styles.content__redTitle}>{'Creating lasting change.'}</span>
+          Increasing conversion, sales and mobile visits and winning multiple awards
+        </h2>
+        <p className={styles.content__paragraph}>
+          The new Badger-created site was one of the only fully responsive retail sites that was
+          live at the time, which ensures that the customer experience is equally superlative no
+          matter what device is used. The site is also built to be scalable and flexible enough to
+          support the company’s future ambitions and peaks of online traffic, such as Christmas.
+        </p>
+        <p className={styles.content__paragraph}>
+          As well as being beautiful to look at with a consistent design and the famous eau de nil
+          colour prominent throughout, the new site is fast, slick and easy to navigate - and truly
+          responsive, delivering optimal layouts across every view port.
+        </p>
+        <p className={styles.content__paragraph}>
+          The site reinforces the brand experience that Fortnum & Mason customers expect and since
+          it went fully live it has delivered incredible sales uplift (38% over the Christmas 2015
+          period) and dramatically increased conversion.
+        </p>
+        <p className={styles.content__paragraph}>
+          The results in the first few weeks were striking:
+        </p>
+        <p className={styles.content__paragraph}>
+          The drop-out rate at the delivery options stage was previously 18.8% and has now been
+          reduced to 12.9% (based figures from 17–25th February 2014 vs 2015.)
+        </p>
+        <p className={styles.content__paragraph}>Other key stats year on year*:</p>
+        <p className={styles.content__paragraph}>
+          * Statistics are based on launch week to 8th March and same date range last year.
+        </p>
+        <p className={styles.content__paragraph}>
+          With the site now being much easier to navigate there has been an increase in sales of
+          specific items, for example a 78% increase in grocery sales.
+        </p>
+        <p className={styles.content__paragraph}>
+          Further to the this, fortnumandmason.com won a BIMA Award for ‘Best Web Design & Build’
+          and an eCommerce Award for ‘Best eCommerce New Design or Re-Design (Under 1 Year)’ in
+          2015. The project was also nominated for the UK Digital Experience Awards in the Retail
+          sector award and as ‘Best Use of Website to Build Your Brand’. It was also nominated at
+          the Retail Week Tech and Ecommerce Awards in the ‘Best Customer Experience’ category.
+        </p>
       </div>
     </div>
     <WhatToReadNext currentPage="retailer" />
