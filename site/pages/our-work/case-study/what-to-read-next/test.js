77b7b21b6aee97764e6412144beb97e5d9ead61f
Mark Holland <mark.holland@red-badger.com>
2017-12-20 16:07:01 +0000

    Test with stub

--- site/pages/our-work/case-study/what-to-read-next/test.js
+++ site/pages/our-work/case-study/what-to-read-next/test.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
-import WhatToReadNext from './index';
+import proxyquire from 'proxyquire';
 import styles from './style.css';
+
+proxyquire.noCallThru();
+
+const mockSliceData = {
+  slice1: {
+    name: 'Slice One',
+    tagline: 'Tagline 1',
+    image: 'image.png',
+    link: '/url',
+  },
+  slice2: {
+    name: 'Slice Two',
+    tagline: 'Tagline 2',
+    image: 'image.png',
+    link: '/url',
+  },
+  slice3: {
+    name: 'Slice Three',
+    tagline: 'Tagline 3',
+    image: 'image.png',
+    link: '/url',
+  },
+};
+
+const WhatToReadNext = proxyquire('./index', {
+  './data': mockSliceData,
+}).default;
 
 describe.only('Case Study - What to read next', () => {
 
@@ -25,21 +51,19 @@ describe.only('Case Study - What to read next', () => {
   });
 
   it('component renders 1 chosen slice', () => {
-    const wrapper = shallow(<WhatToReadNext linkKeys={['retailer']} />);
+    const wrapper = shallow(<WhatToReadNext linkKeys={['slice1']} />);
     const rendered = wrapper.find('WhatToReadNextSlice');
-    expect(rendered.get(0).props).to.have.property('name', 'Retailer');
-    expect(rendered.get(1).props).to.not.have.property('name', 'Retailer');
-    expect(rendered.get(2).props).to.not.have.property('name', 'Retailer');
+    expect(rendered.get(0).props).to.have.property('name', 'Slice One');
+    expect(rendered.get(1).props).to.not.have.property('name', 'Slice One');
+    expect(rendered.get(2).props).to.not.have.property('name', 'Slice One');
   });
 
   it('component renders 3 chosen slices', () => {
-    const wrapper = shallow(
-      <WhatToReadNext linkKeys={['fortnumAndMason', 'financialTimes', 'retailer']} />,
-    );
+    const wrapper = shallow(<WhatToReadNext linkKeys={['slice1', 'slice2', 'slice3']} />);
     const rendered = wrapper.find('WhatToReadNextSlice');
-    expect(rendered.get(0).props).to.have.property('name', 'Fortnum & Mason');
-    expect(rendered.get(1).props).to.have.property('name', 'Financial Times');
-    expect(rendered.get(2).props).to.have.property('name', 'Retailer');
+    expect(rendered.get(0).props).to.have.property('name', 'Slice One');
+    expect(rendered.get(1).props).to.have.property('name', 'Slice Two');
+    expect(rendered.get(2).props).to.have.property('name', 'Slice Three');
   });
 
   it('component renders Link to our work page', () => {
