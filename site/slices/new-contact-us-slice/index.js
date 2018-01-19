37cb4a4aef035421e2015b63152968559fb3bad4
Pedro Martin <pataruco@gmail.com>
2018-01-19 17:19:08 +0000

    Add animation to help button

--- site/slices/new-contact-us-slice/index.js
+++ site/slices/new-contact-us-slice/index.js
@@ -1,37 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classnames from 'classnames/bind';
 import styles from './style.css';
 import Link from '../../components/link';
-import HelpImageS from './images/help.png';
-import HelpImageM from './images/help@2x.png';
-import HelpImageL from './images/help@3x.png';
-import Picture from '../../components/picture';
 
-const NewContactUs = () => (
-  <section className={styles.contactUsContainer}>
-    <h2 className={styles.header}>Ways we can help you</h2>
-    <div>
-      <ul className={styles.list}>
-        <li className={styles.item}>Create & validate new ideas</li>
-        <li className={styles.item}>Deliver great quality products & services, fast</li>
-        <li className={styles.item}>Be bold with technology</li>
-        <li className={styles.item}>Be more customer centric</li>
-        <li className={styles.item}>Improve efficiency with lean practices</li>
-        <li className={styles.item}>Build capability & confidence</li>
-      </ul>
-      <div className={styles.imgContainer}>
-        <Picture
-          className={styles.img}
-          largeSrc={HelpImageL}
-          mediumSrc={HelpImageM}
-          smallSrc={HelpImageS}
-          alt="Help alarm"
-        />
-      </div>
-    </div>
-    <Link to="ourWorkPage" className={styles.button}>
-      Talk to us
-    </Link>
-  </section>
-);
+const cx = classnames.bind(styles);
+
+class NewContactUs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHovered: false,
+    };
+  }
+
+  onHover = () => {
+    this.setState({
+      isHovered: true,
+    });
+  };
+
+  onBlur = () => {
+    this.setState({
+      isHovered: false,
+    });
+  };
+
+  render() {
+    return (
+      <section className={styles.contactUsContainer}>
+        <h2 className={styles.header}>Ways we can help you</h2>
+        <div>
+          <ul className={styles.list}>
+            <li className={styles.item}>Create & validate new ideas</li>
+            <li className={styles.item}>Deliver great quality products & services, fast</li>
+            <li className={styles.item}>Be bold with technology</li>
+            <li className={styles.item}>Be more customer centric</li>
+            <li className={styles.item}>Improve efficiency with lean practices</li>
+            <li className={styles.item}>Build capability & confidence</li>
+          </ul>
+          <div className={cx(styles.imgContainer, this.state.isHovered ? 'isHovered' : '')} />
+        </div>
+        <Link
+          to="ourWorkPage"
+          className={styles.button}
+          onMouseEnter={this.onHover}
+          onMouseLeave={this.onBlur}
+        >
+          Talk to us
+        </Link>
+      </section>
+    );
+  }
+}
 
 export default NewContactUs;
