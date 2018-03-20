5e1322d74bf20d77706bf23ee2612fb0d78a19be
Dominik Piatek <do.piatek@gmail.com>
2018-03-20 14:58:17 +0000

    wip

--- site/pages/what-we-do/header-slice/index.js
+++ site/pages/what-we-do/header-slice/index.js
@@ -6,27 +6,27 @@ const headerSlice = () => {
   return (
     <section className={styles.pageHeaderContainer}>
       <div className={styles.upperContainer}>
-        <h1 className={styles.screenReaderText}>
+        <div className={styles.screenReaderText}>
           Cyclic diagram of doing the right thing through innovation and delivery
+        </div>
+        <h1>
+          <div className={styles.alignLeft} role="presentation">
+            <div className={styles.flexibleUpper}>
+              <div className={styles.pageHeader}>
+                Do the<br />right thing
+              </div>
+              <img alt="" role="presentation" className={styles.arrowDown} src={arrowImg} />
+            </div>
+          </div>
+          <div className={styles.alignRight} role="presentation">
+            <div className={styles.flexibleLower}>
+              <img alt="" role="presentation" className={styles.arrowUp} src={arrowImg} />
+              <div className={styles.pageHeader}>
+                Do the<br />thing right
+              </div>
+            </div>
+          </div>
         </h1>
-        <div className={styles.alignLeft} role="presentation">
-          <div className={styles.flexible}>
-            <div className={styles.pageHeader}>
-              Do the<br />right thing
-            </div>
-            <img alt="" role="presentation" className={styles.arrowDown} src={arrowImg} />
-          </div>
-          <div className={styles.pageSubHeader}>Innovation</div>
-        </div>
-        <div className={styles.alignRight} role="presentation">
-          <div className={styles.flexible}>
-            <img alt="" role="presentation" className={styles.arrowUp} src={arrowImg} />
-            <div className={styles.pageHeader}>
-              Do the<br />thing right
-            </div>
-          </div>
-          <div className={styles.pageSubHeader}>Delivery</div>
-        </div>
       </div>
       <div className={styles.lowerContainer}>
         <p className={styles.subtitle}>
