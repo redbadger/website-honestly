538824d35f34194cbc1cbb5607dd7f8dd63f357b
Matt Paul <mattpaul001@icloud.com>
2018-03-20 12:23:48 +0000

    Update styles, fixes #713

--- site/pages/what-we-do/header-slice/index.js
+++ site/pages/what-we-do/header-slice/index.js
@@ -5,32 +5,37 @@ import arrowImg from './arrow.png';
 const headerSlice = () => {
   return (
     <section className={styles.pageHeaderContainer}>
-      <h1 className={styles.screenReaderText}>
-        Cyclic diagram of doing the right thing through innovation and delivery
-      </h1>
-      <div className={styles.alignLeft} role="presentation">
-        <div className={styles.flexible}>
-          <div className={styles.pageHeader}>
-            Do the<br />right thing
+      <div className={styles.upperContainer}>
+        <h1 className={styles.screenReaderText}>
+          Cyclic diagram of doing the right thing through innovation and delivery
+        </h1>
+        <div className={styles.alignLeft} role="presentation">
+          <div className={styles.flexible}>
+            <div className={styles.pageHeader}>
+              Do the<br />right thing
+            </div>
+            <img alt="" role="presentation" className={styles.arrowDown} src={arrowImg} />
           </div>
-          <img alt="" role="presentation" className={styles.arrowDown} src={arrowImg} />
+          <div className={styles.pageSubHeader}>Innovation</div>
         </div>
-        <div className={styles.pageSubHeader}>Innovation</div>
-      </div>
-      <div className={styles.alignRight} role="presentation">
-        <div className={styles.flexible}>
-          <img alt="" role="presentation" className={styles.arrowUp} src={arrowImg} />
-          <div className={styles.pageHeader}>
-            Do the<br />thing right
+        <div className={styles.alignRight} role="presentation">
+          <div className={styles.flexible}>
+            <img alt="" role="presentation" className={styles.arrowUp} src={arrowImg} />
+            <div className={styles.pageHeader}>
+              Do the<br />thing right
+            </div>
           </div>
+          <div className={styles.pageSubHeader}>Delivery</div>
         </div>
-        <div className={styles.pageSubHeader}>Delivery</div>
       </div>
-      <p className={styles.subtitle}>
-        We help you bring innovative products and services to market through nimble and robust ways
-        of working.
-      </p>
+      <div className={styles.lowerContainer}>
+        <p className={styles.subtitle}>
+          We help you bring innovative products and services to market through nimble and robust
+          ways of working.
+        </p>
+      </div>
     </section>
   );
 };
+
 export default headerSlice;
