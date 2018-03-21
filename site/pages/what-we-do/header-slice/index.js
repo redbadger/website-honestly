07a4aac6ed3967d05e1d143d089a9ea017a56e5a
Matt Paul <mattpaul001@icloud.com>
2018-03-21 17:16:58 +0000

    What we do style (#715)
    * Use badgerRed for subHeading
    
    * Update styles, fixes #713
    
    * wip
    
    * Jun Design Signoff
    
    * Add spaces within h1 for seo benefits

--- site/pages/what-we-do/header-slice/index.js
+++ site/pages/what-we-do/header-slice/index.js
@@ -5,32 +5,42 @@ import arrowImg from './arrow.png';
 const headerSlice = () => {
   return (
     <section className={styles.pageHeaderContainer}>
-      <h1 className={styles.screenReaderText}>
-        Cyclic diagram of doing the right thing through innovation and delivery
-      </h1>
-      <div className={styles.alignLeft} role="presentation">
-        <div className={styles.flexible}>
-          <div className={styles.pageHeader}>
-            Do the<br />right thing
-          </div>
-          <img alt="" role="presentation" className={styles.arrowDown} src={arrowImg} />
+      <div className={styles.upperContainer}>
+        <div className={styles.screenReaderText}>
+          Cyclic diagram of doing the right thing through innovation and delivery
         </div>
-        <div className={styles.pageSubHeader}>Innovation</div>
-      </div>
-      <div className={styles.alignRight} role="presentation">
-        <div className={styles.flexible}>
-          <img alt="" role="presentation" className={styles.arrowUp} src={arrowImg} />
-          <div className={styles.pageHeader}>
-            Do the<br />thing right
+        <h1 className={styles.headingWrapper}>
+          <div className={styles.alignLeft} role="presentation">
+            <div className={styles.flexibleUpper}>
+              <div className={styles.pageHeaderUpper}>
+                {/* Space added so that h1 can be crawled correctly */}
+                Do the{` `}
+                {/* Space added so that h1 can be crawled correctly */}
+                <br />right thing{' '}
+              </div>
+              <img alt="" role="presentation" className={styles.arrowDown} src={arrowImg} />
+            </div>
           </div>
-        </div>
-        <div className={styles.pageSubHeader}>Delivery</div>
+          <div className={styles.alignRight} role="presentation">
+            <div className={styles.flexibleLower}>
+              <img alt="" role="presentation" className={styles.arrowUp} src={arrowImg} />
+              <div className={styles.pageHeaderLower}>
+                {/* Space added so that h1 can be crawled correctly */}
+                Do the{` `}
+                <br />thing right
+              </div>
+            </div>
+          </div>
+        </h1>
       </div>
-      <p className={styles.subtitle}>
-        We help you bring innovative products and services to market through nimble and robust ways
-        of working.
-      </p>
+      <div className={styles.lowerContainer}>
+        <p className={styles.subtitle}>
+          We help you bring innovative products and services to market through nimble and robust
+          ways of working.
+        </p>
+      </div>
     </section>
   );
 };
+
 export default headerSlice;
