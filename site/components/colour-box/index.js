1ec4deb8768a6fa9e93b6c27b02388a449b82046
Dominik Piatek <do.piatek@gmail.com>
2018-03-21 10:28:36 +0000

    Revert "Revert "Cross functional team slice (#697)" (#716)"
    This reverts commit d7aff8b123c33101efdbaad396dff7b5d2f07fd9.

---
+++ site/components/colour-box/index.js
@@ -0,0 +1,41 @@
+// @flow
+import React from 'react';
+import classNames from 'classnames/bind';
+import Link from '../link';
+import LinkArrow from '../link-arrow';
+import styles from './style.css';
+
+export type LabelColour = 'blue' | 'green' | 'red' | 'yellow';
+
+type Props = {
+  children?: Node,
+  title: string,
+  description: string,
+  labelColour: LabelColour,
+  link?: string,
+};
+
+const ColourBox = ({ labelColour, title, description, link }: Props) => {
+  const styled = classNames({
+    [styles[`${labelColour}SubHeading`]]: labelColour,
+  });
+  return (
+    <div>
+      <h3 className={styled}>{title}</h3>
+      {link ? (
+        <Link to={link}>
+          <p>
+            <span className={styles.descriptionLink}>{description}</span>
+            <LinkArrow labelColour={labelColour} />
+          </p>
+        </Link>
+      ) : (
+        <p>
+          <span className={styles.description}>{description}</span>
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default ColourBox;
---
+++ site/pages/what-we-do/cross-functional-slice/index.js
@@ -0,0 +1,73 @@
+// @flow
+import React from 'react';
+import styles from './styles.css';
+import ColourBox from '../../../components/colour-box';
+import Image from '../../../components/image';
+
+import bigParty1x from './png/big-party.png';
+import bigParty2x from './png/big-party@2x.png';
+import bigParty3x from './png/big-party@3x.png';
+import crossFunctionalBadgers from './jpg/cross-functional-badgers.jpg';
+
+const content = [
+  {
+    title: 'Lean Agile',
+    labelColour: 'blue',
+    description: 'Drive efficiency and reduce risk through our lean methods.',
+  },
+  {
+    title: 'Tech',
+    labelColour: 'green',
+    link: 'technology',
+    description: 'Transform your business by being bold with tech.',
+  },
+  {
+    title: 'Design',
+    labelColour: 'yellow',
+    description: 'Improve customer experience, create delightful products and services.',
+  },
+  {
+    title: 'You (the client)',
+    labelColour: 'red',
+    description: 'Together we build a capability and lasting change.',
+  },
+];
+
+const CrossFunctionalSlice = () => (
+  <section className={styles.crossFunctionalSlice}>
+    <div className={styles.photoContainer}>
+      <Image
+        src={crossFunctionalBadgers}
+        alt="a photo showing Red Badger employees working in a cross functional team"
+        className={styles.image}
+      />
+    </div>
+    <div className={styles.sliceBody}>
+      <section className={styles.outerHeadingContainer}>
+        <div className={styles.headingContainer}>
+          <h2 className={styles.heading}>Working in cross functional teams</h2>
+        </div>
+      </section>
+      <section className={styles.mainContainer}>
+        <div className={styles.imageContainer}>
+          <Image
+            src={bigParty1x}
+            src2x={bigParty2x}
+            src3x={bigParty3x}
+            role="presentation"
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.colourBoxContainer}>
+          {content.map(item => (
+            <div key={item.title} className={styles.boxWrapper}>
+              <ColourBox {...item} />
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  </section>
+);
+
+export default CrossFunctionalSlice;
