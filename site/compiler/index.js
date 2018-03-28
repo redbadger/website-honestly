264985769e7c7ebe57ed8b4fc9d73b955263ca76
Matt Paul <mattpaul001@icloud.com>
2018-03-28 14:40:32 +0100

    Add description to compiler

--- site/compiler/index.js
+++ site/compiler/index.js
@@ -88,6 +88,8 @@ export function compileRoutes(state) {
 
     const title = `${route.title} | ${TITLE_SUFFIX}`;
 
+    const description = `${route.description} | ${TITLE_SUFFIX}`;
+
     stateNavigator.navigateLink(route.link, 'none');
     const renderStart = Date.now();
     const bodyContent = renderToString(route.component({ stateNavigator, title }, route.props));
@@ -97,6 +99,7 @@ export function compileRoutes(state) {
     const ejsStart = Date.now();
     const body = layoutTemplate({
       title,
+      description,
       tracking,
       bodyContent,
       cssPath,
