92388299deea7c522f73710fc7bd7741ca3b0f26
Matt Paul <mattpaul001@icloud.com>
2018-04-27 14:15:34 +0100

    Add 2017 data to /about-us 'timeline' slice (#744)
    * Add 2017 data
    
    * Fix html within noscript tags
    
    * Remove outline from navigator items on focus
    
    * Post design review

---
+++ site/pages/about-us/timeline-slice/2017/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import image from './2017.png';
+import Content from '../content';
+
+const Seven = () => {
+  const year = '2017';
+  const title = 'Doing the right thing and “hello Westminster”';
+  const text =
+    'Our Social Value task force launched with our team committing time and skills to Code Your Future and Pride in London. Thanks to our generous badgers, we earned the Platinum Award for Payroll Giving with the company matching employee donations to their charity of choice. We featured in Econsultancy’s Top 100 Digital Agencies for the first time, grew to #31 on the Tech Track 100 and were shortlisted for BIMA’s Entrepreneurial Business of the year. Whilst we didn’t win, Stu was recognised as one of the most influential digital people in the BIMA 100, well done Chief Scientist.';
+  const fact =
+    'In July we moved again - this time across the road to a brand new office in Old Street Yard, and we filled all but one seat on a flight to Croatia for our summer party. Oh, and did we say we hosted a conference at QE2 conference centre Westminster for 700 React fans?';
+
+  return <Content year={year} title={title} text={text} fact={fact} image={image} flip />;
+};
+
+export default Seven;
--- site/pages/about-us/timeline-slice/mobileImage/index.js
+++ site/pages/about-us/timeline-slice/mobileImage/index.js
@@ -9,6 +9,7 @@ import Image2013 from '../2013/2013.png';
 import Image2014 from '../2014/2014.png';
 import Image2015 from '../2015/2015.png';
 import Image2016 from '../2016/2016.png';
+import Image2017 from '../2017/2017.png';
 
 type MobileImageProps = {
   index: number,
@@ -39,6 +40,9 @@ const MobileImage = ({ index, onChangeIndex }: MobileImageProps) => {
         </div>
         <div className={styles.mobileImageWrapper}>
           <img src={Image2016} alt="year 2016" className={styles.image} />
+        </div>
+        <div className={styles.mobileImageWrapper}>
+          <img src={Image2017} alt="year 2017" className={styles.image} />
         </div>
       </SwipeableViews>
     </div>
