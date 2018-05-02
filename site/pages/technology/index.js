c06dbaf2496b8e2b8641356d34c8c0b3d4b89c88
Matt Paul <mattpaul001@icloud.com>
2018-05-02 13:18:33 +0100

    Update technology page with April 2018 roundtable

--- site/pages/technology/index.js
+++ site/pages/technology/index.js
@@ -1,15 +1,15 @@
 // @flow
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import styles from './style.css';
 
 import BlogSlice from './blog-slice';
 import WebinarSignupSlice from './webinar-signup-slice';
 import { TechChecklist } from '../../slices/checklist-contact-us-slice';
 
-import { LightGreySlice, GreenSlice } from '../../components/slice';
-import { XLargeHeading, MediumHeading } from '../../components/headings';
-import { HubspotButton } from '../../components/buttons';
+import { WhiteSlice, GreenSlice } from '../../components/slice';
+import { H1, H2 } from '../../components/headings';
+import { RoundtableHubspotButton } from '../../components/buttons';
 import { XSmallText } from '../../components/text';
 import Image from '../../components/image';
 
@@ -17,37 +17,40 @@ import roundTableImage from './images/2018-april-roundtable.png';
 import roundTableImage2x from './images/2018-april-roundtable@2x.png';
 import roundTableImage3x from './images/2018-april-roundtable@3x.png';
 
-export type TechPageProps = {
+type Props = {
   triedAndTestedBlogPosts: Array<Object>,
   growingTrendsBlogPosts: Array<Object>,
 };
 
-export default ({ triedAndTestedBlogPosts, growingTrendsBlogPosts }: TechPageProps) => (
-  <div>
-    <LightGreySlice>
-      <div className={styles.headingContainer}>
-        <XLargeHeading>We love tech but we only choose what‘s right for the job.</XLargeHeading>
+const TechnologyPage = ({ triedAndTestedBlogPosts, growingTrendsBlogPosts }: Props) => (
+  <Fragment>
+    <WhiteSlice>
+      <div className={styles.heading}>
+        <H1>We love tech but we only choose what‘s right for the job.</H1>
       </div>
-    </LightGreySlice>
+    </WhiteSlice>
     <GreenSlice>
-      <div className={styles.leftContainer}>
-        <div className={styles.roundTableImageContainer}>
-          <Image src={roundTableImage} src2x={roundTableImage2x} src3x={roundTableImage3x} />
-        </div>
+      <div className={styles.left}>
+        <Image src={roundTableImage} src2x={roundTableImage2x} src3x={roundTableImage3x} />
       </div>
-      <div className={styles.rightContainer}>
-        <MediumHeading>April 2018 Report</MediumHeading>
-        <XSmallText>
-          Technology doesn’t stand still and neither do we. Our team gets together on a regular
-          basis to discuss which technologies we’re using to solve difficult problems on our
-          projects, and which technologies are on our radar as ones to watch.
-        </XSmallText>
-        <HubspotButton
-          cta="Read PDF Report"
-          analyticsTitle="RoundtablePDFLink"
-          analyticsCategory="TechnologyPage"
-          hubspotTitle="roundtable"
-        />
+      <div className={styles.right}>
+        <div className={styles.roundtableHeading}>
+          <H2 type="fontM2">April 2018 Report</H2>
+        </div>
+        <div className={styles.description}>
+          <XSmallText>
+            Technology doesn’t stand still and neither do we. Find out which new and fully adopted
+            technologies our team think are adding value.
+          </XSmallText>
+        </div>
+        <div className={styles.hubspotButtonWrapper}>
+          <RoundtableHubspotButton
+            analyticsTitle="RoundtablePDFLink"
+            analyticsCategory="TechnologyPage"
+            hubspotTitle="roundtableApril2018"
+            className={styles.readPdfReportWrapper}
+          />
+        </div>
       </div>
     </GreenSlice>
     <section className={styles.pastAndFuture}>
@@ -60,5 +63,7 @@ export default ({ triedAndTestedBlogPosts, growingTrendsBlogPosts }: TechPagePro
     </section>
     <WebinarSignupSlice />
     <TechChecklist />
-  </div>
+  </Fragment>
 );
+
+export default TechnologyPage;
