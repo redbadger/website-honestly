92e5136d337697579c40c71a891ac39d93204fa0
Matt Paul <mattpaul001@icloud.com>
2018-05-04 15:21:36 +0100

    Add further heading styles

--- site/components/headings/index.js
+++ site/components/headings/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import styles from './style.css';
 
-type Font = 'fontXL' | 'fontL' | 'fontM' | 'fontM2';
+type Font = 'fontXL' | 'fontL' | 'fontM' | 'fontM2' | 'fontS2' | 'fontS';
 
 type Props = {
   children: string,
@@ -21,4 +21,10 @@ H2.defaultProps = {
   type: 'fontM',
 };
 
-export { H1, H2 };
+const H3 = ({ children, type }: Props) => <h3 className={styles[`${type}`]}>{children}</h3>;
+
+H2.defaultProps = {
+  type: 'fontM',
+};
+
+export { H1, H2, H3 };
