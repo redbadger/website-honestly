cbb1535bc3ec7313ac687464083ce263fba454f4
Dominik Piatek <do.piatek@gmail.com>
2018-05-09 14:44:03 +0100

    Fix flow issues

--- site/components/text/index.js
+++ site/components/text/index.js
@@ -6,33 +6,21 @@ type Font = 'fontXL' | 'fontL' | 'fontM' | 'fontM2' | 'fontS2' | 'fontS' | 'font
 
 type Props = {
   children: string,
-  type: Font,
+  type?: Font,
   customClass?: string,
 };
 
 const P = ({ children, type = 'fontXS', customClass = '' }: Props) => (
   <p className={`${styles[`${type}`]} ${customClass}`}>{children}</p>
 );
-
-P.defaultProps = {
-  type: 'fontXS',
-  customClass: '',
-};
-
-const H1 = ({ children, type }: Props) => <h1 className={styles[`${type}`]}>{children}</h1>;
-
-H1.defaultProps = {
-  type: 'fontXL',
-};
-
+const H1 = ({ children, type = 'fontXL' }: Props) => (
+  <h1 className={styles[`${type}`]}>{children}</h1>
+);
 const H2 = ({ children, type = 'fontM', customClass = '' }: Props) => (
   <h2 className={`${styles[`${type}`]} ${customClass}`}>{children}</h2>
 );
-
-const H3 = ({ children, type }: Props) => <h3 className={styles[`${type}`]}>{children}</h3>;
-
-H3.defaultProps = {
-  type: 'fontM',
-};
+const H3 = ({ children, type = 'fontM' }: Props) => (
+  <h3 className={styles[`${type}`]}>{children}</h3>
+);
 
 export { P, H1, H2, H3 };
