d96c676ff16f949c7dcda5ea77990f7efa88ffd8
Dominik Piatek <do.piatek@gmail.com>
2018-05-14 13:09:28 +0100

    Find baseUrl dynamiclly

--- site/components/social/index.js
+++ site/components/social/index.js
@@ -9,6 +9,22 @@ export type SocialProps = {
   url: string,
 };
 
+const baseUrl = () => {
+  if (window === 'undefined' || !!window.location) {
+    return 'https://red-badger.com';
+  }
+
+  const { host, pathname, origin } = window.location;
+  const staging = /^www-staging/.test(host);
+  const branch = pathname.match(/\/(\d+)\//);
+
+  if (staging && branch) {
+    return `${origin}/${branch}`;
+  }
+
+  return origin;
+};
+
 const Social = ({ title, description, metaImage, url }: SocialProps) => (
   <Helmet
     meta={[
@@ -16,11 +32,11 @@ const Social = ({ title, description, metaImage, url }: SocialProps) => (
       { name: 'twitter:site', content: '@redbadgerteam' },
       { name: 'twitter:title', content: title },
       { name: 'twitter:description', content: description },
-      { name: 'twitter:image', content: `https://red-badger.com${metaImage}` },
+      { name: 'twitter:image', content: `${baseUrl()}${metaImage}` },
       { property: 'og:type', content: 'website' },
       { property: 'og:url', content: url },
       { property: 'og:title', content: title },
-      { property: 'og:image', content: `https://red-badger.com${metaImage}` },
+      { property: 'og:image', content: `${baseUrl()}${metaImage}` },
       { property: 'og:description', content: description },
     ]}
   />
