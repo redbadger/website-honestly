15fc61e8c6bae574ac155ad8d370e66687e8a78c
Matt Paul <mattpaul001@icloud.com>
2018-05-18 16:30:23 +0100

    Add the rest of the cookies page

--- site/components/text/index.js
+++ site/components/text/index.js
@@ -1,11 +1,11 @@
 // @flow
-import React from 'react';
+import * as React from 'react';
 import styles from './style.css';
 
 type Font = 'fontXL' | 'fontL' | 'fontM' | 'fontM2' | 'fontS2' | 'fontS' | 'fontXS';
 
 type Props = {
-  children: string,
+  children: React.Node,
   type?: Font,
   customClass?: string,
 };
@@ -13,14 +13,14 @@ type Props = {
 const P = ({ children, type = 'fontXS', customClass = '' }: Props) => (
   <p className={`${styles[`${type}`]} ${customClass}`}>{children}</p>
 );
-const H1 = ({ children, type = 'fontXL' }: Props) => (
-  <h1 className={styles[`${type}`]}>{children}</h1>
+const H1 = ({ children, type = 'fontXL', customClass = '' }: Props) => (
+  <h1 className={`${styles[`${type}`]} ${customClass}`}>{children}</h1>
 );
 const H2 = ({ children, type = 'fontM', customClass = '' }: Props) => (
   <h2 className={`${styles[`${type}`]} ${customClass}`}>{children}</h2>
 );
-const H3 = ({ children, type = 'fontM' }: Props) => (
-  <h3 className={styles[`${type}`]}>{children}</h3>
+const H3 = ({ children, type = 'fontM', customClass = '' }: Props) => (
+  <h3 className={`${styles[`${type}`]} ${customClass}`}>{children}</h3>
 );
 
 export { P, H1, H2, H3 };
