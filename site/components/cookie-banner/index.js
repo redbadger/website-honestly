54aab2abd6462c046589ade62af57e520686ba45
Dominik Piatek <do.piatek@gmail.com>
2018-05-23 14:46:07 +0100

    Cross-browser fixes

--- site/components/cookie-banner/index.js
+++ site/components/cookie-banner/index.js
@@ -1,25 +1,52 @@
 // @flow
 
-import React from 'react';
+import * as React from 'react';
+
 import Link from '../link';
 
 import styles from './style.css';
 
-const CookieBanner = ({ closeBanner }: { closeBanner: Function }) => {
-  const acceptCookies = () => {
+type Props = { closeBanner: Function };
+
+class CookieBanner extends React.Component<Props> {
+  // This clones the banner and sets it's position to static, effectively pushing it behind
+  // the sticky banner in the DOM. Thanks to this, the component takes up space and pushes
+  // the down by the right amount always.
+  componentDidMount() {
+    if (!this.bannerNode) return;
+
+    const parent = this.bannerNode.parentElement;
+    const clone = this.bannerNode.cloneNode(true);
+    clone.style.cssText = 'position: static; z-index: 0';
+
+    if (parent) parent.appendChild(clone);
+  }
+
+  bannerNode: ?HTMLDivElement;
+
+  acceptCookies = (closeBanner: Function) => {
     document.cookie = '_cookies=true; path=/;  expires=Fri, 31 Dec 9999 23:59:59 GMT';
     closeBanner();
   };
 
-  return (
-    <div className={styles.banner}>
-      <p>
-        Welcome to Red Badger. Our website uses cookies to optimise your experience.{' '}
-        <Link to="cookiePolicy">View cookies policy here.</Link>
-      </p>
-      <button onClick={acceptCookies}>Accept and Close</button>
-    </div>
-  );
-};
+  render() {
+    const { closeBanner } = this.props;
+
+    return (
+      // The outer div provides a parent for the cloning function - see componentDidMount
+      <div>
+        <div ref={node => (this.bannerNode = node)} className={styles.bannerWrapper}>
+          <div className={styles.banner}>
+            <p>
+              Welcome to Red Badger. Our website uses cookies to optimise your experience.{' '}
+              <Link to="cookiePolicy">View cookies policy here.</Link>
+            </p>
+            <button onClick={this.acceptCookies.bind(this, closeBanner)}>Accept and Close</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default CookieBanner;
