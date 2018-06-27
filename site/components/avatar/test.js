18d13370bda03771967d42ad10d647f0717a8ab4
ajcumine <ajcumine@gmail.com>
2018-06-27 12:12:50 +0100

    configure enzyme for jest and test avatar component

--- site/components/avatar/test.js
+++ site/components/avatar/test.js
@@ -1,27 +1,27 @@
 // @flow
 import React from 'react';
-import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import Avatar from '.';
 
 describe('<Avatar />', () => {
   it('renders image child', () => {
     const wrapper = shallow(<Avatar image="" name="Testa Fiesta" />);
-    expect(wrapper.find('img').html()).to.equal('<img src="" alt="Testa Fiesta"/>');
+    expect(wrapper.find('img').html()).toEqual('<img src="" alt="Testa Fiesta"/>');
   });
 
   it('sets an img src', () => {
     const wrapper = shallow(<Avatar image="myAwesomeImage.png" name="" />);
-    expect(wrapper.containsMatchingElement(<img src="myAwesomeImage.png" alt="" />)).to.equal(true);
+    expect(wrapper.containsMatchingElement(<img src="myAwesomeImage.png" alt="" />)).toEqual(true);
   });
 
   it('sets name as alt text', () => {
     const wrapper = shallow(<Avatar image="" name="Testa Fiesta" />);
-    expect(wrapper.containsMatchingElement(<img src="" alt="Testa Fiesta" />)).to.equal(true);
+    expect(wrapper.containsMatchingElement(<img src="" alt="Testa Fiesta" />)).toEqual(true);
   });
 
   it('sets a size', () => {
     const wrapper = shallow(<Avatar image="" name="Testa Fiesta" size={120} />);
-    expect(wrapper.props().style).to.include({ width: 120, height: 120 });
+    expect(wrapper.props().style.width).toEqual(120);
+    expect(wrapper.props().style.height).toEqual(120);
   });
 });
