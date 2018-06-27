31630ba17a4de3c688eb773b0050857ff236e60e
ajcumine <ajcumine@gmail.com>
2018-06-27 14:29:54 +0100

    convert to jest

--- site/fetchers/twitter.test.js
+++ site/fetchers/twitter.test.js
@@ -29,39 +29,39 @@ const generateTweet = () => {
 describe('twitter fetcher tweet validation', () => {
   it('passes with valid tweet', () => {
     const result = isValidTweet(generateTweet());
-    expect(result).to.equal(true);
+    expect(result).toEqual(true);
   });
 
   it('fails with null value', () => {
     const result = isValidTweet(null);
-    expect(result).to.equal(false);
+    expect(result).toEqual(false);
   });
 
   it('fails with empty tweet', () => {
     const tweet = {};
     const result = isValidTweet(tweet);
-    expect(result).to.equal(false);
+    expect(result).toEqual(false);
   });
 
   it('fails with no text', () => {
     const tweet = generateTweet();
     tweet.text = '';
     const result = isValidTweet(tweet);
-    expect(result).to.equal(false);
+    expect(result).toEqual(false);
   });
 
   it('fails with no entities property', () => {
     const tweet = generateTweet();
     tweet.entities = null;
     const result = isValidTweet(tweet);
-    expect(result).to.equal(false);
+    expect(result).toEqual(false);
   });
 
   it('fails with no urls property', () => {
     const tweet = generateTweet();
     tweet.entities.urls = null;
     const result = isValidTweet(tweet);
-    expect(result).to.equal(false);
+    expect(result).toEqual(false);
   });
 
   it('fails with no url and no retweet', () => {
@@ -69,20 +69,20 @@ describe('twitter fetcher tweet validation', () => {
     tweet.entities.urls[0] = null;
     tweet.retweeted_status = null;
     const result = isValidTweet(tweet);
-    expect(result).to.equal(false);
+    expect(result).toEqual(false);
   });
 
   it('fails with invalid retweet count value', () => {
     const tweet = generateTweet();
     tweet.retweet_count = undefined;
     const result = isValidTweet(tweet);
-    expect(result).to.equal(false);
+    expect(result).toEqual(false);
   });
 
   it('fails with invalid favourite count value', () => {
     const tweet = generateTweet();
     tweet.favorite_count = undefined;
     const result = isValidTweet(tweet);
-    expect(result).to.equal(false);
+    expect(result).toEqual(false);
   });
 });
