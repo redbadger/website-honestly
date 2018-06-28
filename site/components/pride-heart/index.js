21047b7ee5e37f0d8786dc41ad296e9f1b8969ad
Andrew Cumine <ajcumine@gmail.com>
2018-06-28 14:48:06 +0100

    Add pride heart to homepage (#785)
    * add pride heart to homepage
    
    * cleanup animation conditionals
    
    * use dark heart
    
    * try on touch start

---
+++ site/components/pride-heart/index.js
@@ -0,0 +1,37 @@
+// @flow
+import React from 'react';
+import lottie from 'lottie-web';
+import animationData from './data.json';
+import styles from './style.css';
+
+type Props = {
+  direction: string,
+  play: boolean,
+};
+
+export default class PrideHeart extends React.Component<Props> {
+  componentDidMount() {
+    lottie.loadAnimation({
+      animationData,
+      autoplay: false,
+      container: document.getElementById('prideHeart'),
+      loop: false,
+      renderer: 'svg',
+    });
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const { direction, play } = this.props;
+    if (direction !== prevProps.direction && play === true) {
+      lottie.setDirection(direction === 'forward' ? 1 : -1);
+      lottie.play();
+    }
+    if (play !== prevProps.play && play === true) {
+      lottie.play();
+    }
+  }
+
+  render() {
+    return <div className={styles.prideHeart} id="prideHeart" />;
+  }
+}
