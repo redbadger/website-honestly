a00b90e8c4b2591f1ac022c67e59e196d8e78628
ajcumine <ajcumine@gmail.com>
2018-06-29 10:32:43 +0100

    convert to jest

--- dev/jest/file-transform.js
+++ dev/jest/file-transform.js
@@ -1,7 +1,5 @@
 const path = require('path');
 
-console.log('HAI');
-
 // This is a custom Jest transformer turning file imports into filenames.
 // http://facebook.github.io/jest/docs/tutorial-webpack.html
 
