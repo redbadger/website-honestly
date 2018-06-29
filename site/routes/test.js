aad48e9c93e85d7b542e2c9f9c99c889bfd0154c
ajcumine <ajcumine@gmail.com>
2018-06-29 10:38:29 +0100

    convert to jest

--- site/routes/test.js
+++ site/routes/test.js
@@ -1,17 +1,17 @@
-import createStateNavigator from '.';
+import createStateNavigator from './';
 
 describe('Routes', () => {
   describe('Home Page', () => {
     it('should accept contact us boolean', () => {
       const stateNavigator = createStateNavigator();
       const url = stateNavigator.getNavigationLink('homePage', { contactUs: true });
-      expect(url).to.equal('/?contactUs=true');
+      expect(url).toEqual('/?contactUs=true');
     });
 
     it('should turn contact us into hash', () => {
       const stateNavigator = createStateNavigator();
       const href = stateNavigator.historyManager.getHref('/?contactUs=true');
-      expect(href).to.equal('/#contactUs');
+      expect(href).toEqual('/#contactUs');
     });
 
     it('should turn hash into contact us', () => {
@@ -22,7 +22,7 @@ describe('Routes', () => {
         hash: '#contactUs',
       };
       const url = stateNavigator.historyManager.getUrl(anchor);
-      expect(url).to.equal('/?contactUs=true');
+      expect(url).toEqual('/?contactUs=true');
     });
   });
 });
