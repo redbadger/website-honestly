28cbd0f10b14f4953a6aae19a8e7e989945ec768
Dominik Piatek <do.piatek@gmail.com>
2018-07-10 14:05:46 +0100

    Track logo clicks

--- site/components/case-study-company-logos/index.js
+++ site/components/case-study-company-logos/index.js
@@ -1,5 +1,9 @@
 // @flow
+
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+
 import React from 'react';
+import ReactGA from 'react-ga';
 
 import bbcPNG from './PNG/bbc.png';
 import pridePNG from './PNG/pride.png';
@@ -10,8 +14,15 @@ import tescoPNG from './PNG/tesco.png';
 
 import styles from './style.css';
 
+const trackLogoClicks = () => () =>
+  ReactGA.event({
+    category: 'Homepage logos',
+    action: 'click',
+    label: 'Case Study',
+  });
+
 const CaseStudyCompanyLogos = () => (
-  <div className={styles.caseCompanies}>
+  <div className={styles.caseCompanies} onClick={trackLogoClicks}>
     <div className={styles.caseCompaniesLogo}>
       <img alt="The logo of Selfridges" src={selfridgesPNG} />
     </div>
