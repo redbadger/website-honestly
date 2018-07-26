6ed7ad2057775bd1bc05e3c5163c329e6a1713f5
Dominik Piatek <do.piatek@gmail.com>
2018-07-26 11:36:42 +0100

    Small fixes

--- site/pages/our-work/slices/shared/header/test.js
+++ site/pages/our-work/slices/shared/header/test.js
@@ -7,7 +7,7 @@ import Header from '.';
 
 describe('site/pages/our-work/slices/shared/header', () => {
   it('renders correctly', () => {
-    const component = shallow(<Header />);
+    const component = shallow(<Header>heading</Header>);
     expect(component).toMatchSnapshot();
   });
 });
