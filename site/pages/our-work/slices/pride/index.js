bf2aae0ea9b448231df1459027d37d8c6713b89b
Dominik Piatek <do.piatek@gmail.com>
2018-11-14 09:45:59 +0000

    Add pride to landing page

---
+++ site/pages/our-work/slices/pride/index.js
@@ -0,0 +1,22 @@
+// @flow
+
+import React from 'react';
+
+import { Container, Description, ReadMore, Header, Text, Image } from '../shared';
+import prideImg from './images/listing.jpg';
+
+export default function Pride() {
+  return (
+    <Container to="prideCaseStudy" layout="row-reverse">
+      <Text layout="rightCol">
+        <Header>Delivering a best-in-class digital experience</Header>
+        <Description>
+          Building a 5 star digital experience for Pride in London’s mobile app using React Native,
+          promoting inclusion and accessibility for an improved user experience.
+        </Description>
+        <ReadMore />
+      </Text>
+      <Image src={prideImg} alt="People (Badgers) sticking post-its on a blue wall" />
+    </Container>
+  );
+}
