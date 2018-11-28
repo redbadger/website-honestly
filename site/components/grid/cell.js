a3465efe371c27571ab9a51b17737401ed72ab98
Joseph Popoola <josephpopoola@Josephs-MacBook-Pro.local>
2018-11-28 16:58:56 +0000

    updated css to align media queries

--- site/components/grid/cell.js
+++ site/components/grid/cell.js
@@ -6,11 +6,11 @@ import styles from './style.css';
 type CellProps = {
   breakOn?: string,
   children?: React.Node,
-  hideOn?: 'mobileS' | 'mobileSM' | 'mobile' | 'tablet' | 'dont',
+  hideOn?: 'smallestScreen' | 'smallScreen' | 'mediumScreen' | 'dont',
   size: number,
 };
 
-const Cell = ({ breakOn = 'mobile', children, hideOn = 'dont', size }: CellProps) => {
+const Cell = ({ breakOn = 'smallestScreen', children, hideOn = 'dont', size }: CellProps) => {
   const cellClassNames = classNames({
     [styles[`responsive-cell-${breakOn}`]]: true,
     [styles.cell]: true,
