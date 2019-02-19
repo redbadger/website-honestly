5f3e461839353c74c16de486a82cbdc7dca5b588
Simon Ashbery <simonashbery@Simons-MacBook-Pro.local>
2019-02-19 17:31:07 +0000

    reworks mobile breakpoints for techlab slice

--- site/slices/techlab-slice/index.js
+++ site/slices/techlab-slice/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import styles from './style.css';
 import Image from '../../components/image';
 
+import glassesImage from './images/glasses.jpg';
 import glassesX2Image from './images/glassesX2.jpg';
 import glassesX3Image from './images/glassesX3.jpg';
 
@@ -14,22 +15,26 @@ type Props = {
 export default function TechLabSlice({ linkUrl }: Props) {
   return (
     <section className={styles.techLabSlice}>
+      <h2 className={[styles.heading, styles.heading__mobile].join(' ')}>
+        TechLab_Report<sup>19</sup>
+      </h2>
       <Image
         alt="laboratory safety glasses"
         className={styles.techLabImage}
-        src={glassesX2Image}
+        src={glassesImage}
         src2x={glassesX2Image}
         src3x={glassesX3Image}
       />
       <div className={styles.headingContainer}>
-        <h2 className={styles.heading}>
+        <h2 className={[styles.heading, styles.heading__desktop].join(' ')}>
           TechLab_Report<sup>19</sup>
-          <p>– our view on what matters in tech</p>
+          <br />– our view on what matters in tech
         </h2>
+        <p>Our view on what matters in tech</p>
+        <a className={styles.exploreBtn} href={linkUrl}>
+          Explore the report
+        </a>
       </div>
-      <a className={styles.exploreBtn} href={linkUrl}>
-        Explore the report
-      </a>
     </section>
   );
 }
