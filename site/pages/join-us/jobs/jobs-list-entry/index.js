01d4668ad15a48c0ba5597db06170a9a6c9d19a7
Simon Ashbery <si.ashbery@gmail.com>
2019-03-05 11:35:35 +0000

    Jobs page accordion fix (#844)
    * Makes jobs and benefits accordions consistent
    - Now clicking anywhere in the title will expand and collapse the accordion.
    - Removes JobTitle component as it is now redundant
    
    * only applies pointer to category section headers at mobile screen size
    
    * only applies pointer to benefit questions on medium screen and larger

--- site/pages/join-us/jobs/jobs-list-entry/index.js
+++ site/pages/join-us/jobs/jobs-list-entry/index.js
@@ -4,7 +4,6 @@ import React, { Component } from 'react';
 import HR from '../../../../components/hr';
 import Link from '../../../../components/link';
 import { Grid, Cell } from '../../../../components/grid';
-import JobTitle from '../job-title';
 import RawHtml from '../../../../components/raw-html';
 import styles from '../jobs-list/style.css';
 import type { JobProps } from '../';
@@ -43,42 +42,46 @@ class JobsListEntry extends Component<JobProps, JobsListEntryState> {
         itemScope
         itemType="http://schema.org/JobPosting"
       >
-        <div itemProp="jobLocation" itemScope itemType="http://schema.org/Place">
-          <meta itemProp="streetAddress" content="4th Floor, 2 Old Street Yard" />
-          <meta itemProp="addressLocality" content="London" />
-          <meta itemProp="addressRegion" content="Greater London" />
-          <meta itemProp="postalCode" content="EC1Y 8AF" />
-        </div>
-        <meta itemProp="hiringOrganization" content="Red Badger" />
-        <meta itemProp="datePosted" content={new Date(datePosted)} />
-        <Grid fit={false}>
-          <Cell size={12} breakOn="smallScreen">
-            <button
-              onClick={this.handleClick}
-              aria-expanded={open}
-              aria-label={`expand ${title} job description`}
-              className={styles.jobEntry__more}
-            >
-              <span className={open ? styles.jobEntry__arrowDown : styles.jobEntry__arrow} />
-            </button>
-            <div className={styles.jobEntryHeaderContainer}>
-              <div className={styles.jobEntryHeader} itemProp="title">
-                <JobTitle jobLink={eventLink} jobTitle={title} />
-              </div>
-              <div
-                className={open ? styles.jobDescription__visible : styles.jobDescription__hidden}
+        <div className={styles.jobEntryWrapper} onClick={this.handleClick}>
+          <div itemProp="jobLocation" itemScope itemType="http://schema.org/Place">
+            <meta itemProp="streetAddress" content="4th Floor, 2 Old Street Yard" />
+            <meta itemProp="addressLocality" content="London" />
+            <meta itemProp="addressRegion" content="Greater London" />
+            <meta itemProp="postalCode" content="EC1Y 8AF" />
+          </div>
+          <meta itemProp="hiringOrganization" content="Red Badger" />
+          <meta itemProp="datePosted" content={new Date(datePosted)} />
+          <Grid fit={false}>
+            <Cell size={12} breakOn="smallScreen">
+              <button
+                onClick={this.handleClick}
+                aria-expanded={open}
+                aria-label={`expand ${title} job description`}
+                className={styles.jobEntry__more}
               >
-                <div className={styles.jobDescription} itemProp="description">
-                  <RawHtml>{description}</RawHtml>
+                <span className={open ? styles.jobEntry__arrowDown : styles.jobEntry__arrow} />
+              </button>
+              <div className={styles.jobEntryHeaderContainer}>
+                <div className={styles.jobEntryHeader} itemProp="title">
+                  <h3 className={styles.jobTitle} itemProp="name">
+                    <span>{title}</span>
+                  </h3>
                 </div>
-                <Link to="job" navigationData={eventLink} className={styles.applyBtn}>
-                  Read more
-                </Link>
+                <div
+                  className={open ? styles.jobDescription__visible : styles.jobDescription__hidden}
+                >
+                  <div className={styles.jobDescription} itemProp="description">
+                    <RawHtml>{description}</RawHtml>
+                  </div>
+                  <Link to="job" navigationData={eventLink} className={styles.applyBtn}>
+                    Read more
+                  </Link>
+                </div>
               </div>
-            </div>
-            <HR color="black" customClassName={styles.mobileHorizontalLine} />
-          </Cell>
-        </Grid>
+              <HR color="black" customClassName={styles.mobileHorizontalLine} />
+            </Cell>
+          </Grid>
+        </div>
       </li>
     );
   }
