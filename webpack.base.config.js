c64ee82afcd132acb81a355ecc81a8c505d3fc22
Simon Ashbery <simonashbery@Simons-MacBook-Pro.local>
2019-03-18 17:03:34 +0000

    splits node_modules into vendor.js

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -46,6 +46,32 @@ const baseConfig = {
     publicPath,
   },
 
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+
   module: {
     rules: [
       {
