211ecf4bcc8bd737715a9de7d2b3694323f969c5
Simon Ashbery <simonashbery@Simons-MacBook-Pro.local>
2019-03-22 14:21:50 +0000

    Removes vendor splitting

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -46,32 +46,6 @@ const baseConfig = {
     publicPath,
   },
 
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      minSize: 30000,
-      maxSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 5,
-      maxInitialRequests: 3,
-      automaticNameDelimiter: '~',
-      name: true,
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          chunks: 'all',
-          priority: -10,
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
-      },
-    },
-  },
-
   module: {
     rules: [
       {
