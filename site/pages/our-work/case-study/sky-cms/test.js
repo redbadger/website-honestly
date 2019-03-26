974f27e6d303d9e5c71a74bf9ba107dffd78af50
Simon Ashbery <simonashbery@Simons-MacBook-Pro.local>
2019-03-26 16:59:38 +0000

    adds sky cms case study tests

--- site/pages/our-work/case-study/sky-cms/test.js
+++ site/pages/our-work/case-study/sky-cms/test.js
@@ -3,16 +3,16 @@ import { mount } from 'enzyme';
 
 import { Context } from '../../../../components/link/test-helper';
 import MockedWhatToReadNext from '../shared/test-helper';
-import CaseStudyRetailer from './index';
+import SkyCmsCaseStudy from './index';
 
 jest.mock('../shared/what-to-read-next', () => MockedWhatToReadNext);
 
-describe('site/pages/our-work/case-study/retailer/', () => {
+describe('site/pages/our-work/case-study/sky-cms/', () => {
   it('renders correctly', () => {
     expect(
       mount(
         <Context>
-          <CaseStudyRetailer />
+          <SkyCmsCaseStudy />
         </Context>,
       ),
     ).toMatchSnapshot();
