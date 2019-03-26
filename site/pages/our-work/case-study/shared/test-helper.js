81093ead028e2452fc7836bba8cb7a8b664bd860
Simon Ashbery <simonashbery@Simons-MacBook-Pro.local>
2019-03-26 16:08:31 +0000

    Refactors the mocked what to read test slice

--- site/pages/our-work/case-study/shared/test-helper.js
+++ site/pages/our-work/case-study/shared/test-helper.js
@@ -1,119 +1,50 @@
 // @no-flow
 import React from 'react';
 
+import styles from './what-to-read-next/style.css';
+import Link from '../../../../components/link';
+import WhatToReadNextSlice from './what-to-read-next/slice';
+
 const MockedWhatToReadNext = () => {
+  const nextSlices = [
+    {
+      name: 'Pride in London',
+      tagline: 'Delivering a best-in-class digital experience',
+      image: 'pride.gif',
+      linkKey: 'prideCaseStudy',
+    },
+    {
+      name: 'Financial Times',
+      tagline: 'Lasting change for a media giant',
+      image: 'financialTimesReadMore.jpg',
+      linkKey: 'financialTimesCaseStudy',
+    },
+    {
+      name: 'Car Trawler',
+      tagline: 'Creating complete CMS control for travel brands',
+      image: 'carTrawler.jpg',
+      linkKey: 'carTrawlerCaseStudy',
+    },
+  ];
   return (
-    <div className="style__whatNext _divider__dividerGrey _divider__divider">
-      <div className="style__whatNext__tilesContainer">
-        <div>
-          <figure>
-            <a className="" href="/our-work/case-study/financial-services-digital-transformation">
-              <img
-                src="/assets-honestly/bank-1xM8X.jpg"
-                alt="Financial services"
-                className="style__whatNext__image"
-              />
-            </a>
-            <figcaption className="style__whatNext__caption _fonts__fontXS _fonts__boldSansSerif">
-              Financial services
-            </figcaption>
-          </figure>
-          <a
-            className="style__whatNext__link"
-            href="/our-work/case-study/financial-services-digital-transformation"
-          >
-            <h2 className="style__whatNext__title _fonts__fontM2 _fonts__serif">
-              Digital transformation in retail banking
-              <span className="style__whatNext__arrow_wrapper">
-                <svg
-                  viewBox="0 0 22 22"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="style__whatNext__arrow"
-                >
-                  <path
-                    d="M11 1l10 10-10 10-1.754-1.754 6.959-7.018H1V9.772h15.205l-6.96-7.018z"
-                    fill="#FC1D42"
-                    stroke="#FC1D42"
-                    fillRule="evenodd"
-                  />
-                </svg>
-              </span>
-            </h2>
-          </a>
-        </div>
-        <div>
-          <figure>
-            <a className="" href="/our-work/case-study/fortnum-and-mason">
-              <img
-                src="/assets-honestly/fortnumReadMore-3LiWI.jpg"
-                alt="Fortnum &amp; Mason"
-                className="style__whatNext__image"
-              />
-            </a>
-            <figcaption className="style__whatNext__caption _fonts__fontXS _fonts__boldSansSerif">
-              Fortnum &amp; Mason
-            </figcaption>
-          </figure>
-          <a className="style__whatNext__link" href="/our-work/case-study/fortnum-and-mason">
-            <h2 className="style__whatNext__title _fonts__fontM2 _fonts__serif">
-              Elegant e-commerce in eight months
-              <span className="style__whatNext__arrow_wrapper">
-                <svg
-                  viewBox="0 0 22 22"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="style__whatNext__arrow"
-                >
-                  <path
-                    d="M11 1l10 10-10 10-1.754-1.754 6.959-7.018H1V9.772h15.205l-6.96-7.018z"
-                    fill="#FC1D42"
-                    stroke="#FC1D42"
-                    fillRule="evenodd"
-                  />
-                </svg>
-              </span>
-            </h2>
-          </a>
-        </div>
-        <div>
-          <figure>
-            <a className="" href="/our-work/case-study/car-trawler">
-              <img
-                src="/assets-honestly/car-trawler-my-account-BSWfN.jpg"
-                alt="Car Trawler"
-                className="style__whatNext__image"
-              />
-            </a>
-            <figcaption className="style__whatNext__caption _fonts__fontXS _fonts__boldSansSerif">
-              Car Trawler
-            </figcaption>
-          </figure>
-          <a className="style__whatNext__link" href="/our-work/case-study/car-trawler">
-            <h2 className="style__whatNext__title___1xI37 _fonts__fontM2___3sSED _fonts__serif___1u07k">
-              Building a self-servicing customer account
-              <span className="style__whatNext__arrow_wrapper">
-                <svg
-                  viewBox="0 0 22 22"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="style__whatNext__arrow"
-                >
-                  <path
-                    d="M11 1l10 10-10 10-1.754-1.754 6.959-7.018H1V9.772h15.205l-6.96-7.018z"
-                    fill="#FC1D42"
-                    stroke="#FC1D42"
-                    fillRule="evenodd"
-                  />
-                </svg>
-              </span>
-            </h2>
-          </a>
-        </div>
+    <div className={styles.whatNext}>
+      <div className={styles.whatNext__tilesContainer}>
+        {nextSlices.map((slice, index) => (
+          /* disabled because list is static */
+          /* eslint-disable react/no-array-index-key */
+          <WhatToReadNextSlice
+            key={`${slice.name}-${index}`}
+            name={slice.name}
+            tagline={slice.tagline}
+            image={slice.image}
+            linkKey={slice.linkKey}
+          />
+          /* eslint-enable react/no-array-index-key */
+        ))}
       </div>
-      <a
-        className="style__whatNext__button _links__primaryButton _fonts__boldSansSerif"
-        href="/our-work"
-      >
+      <Link to="ourWorkPage" className={styles.whatNext__button}>
         See more of our work
-      </a>
+      </Link>
     </div>
   );
 };
