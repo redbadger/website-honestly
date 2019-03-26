5c1d8589922938bfac157500aca44a5d48d9e4b3
Simon Ashbery <simonashbery@Simons-MacBook-Pro.local>
2019-03-26 16:13:22 +0000

    adds bbc case study tests

--- site/pages/our-work/case-study/bbc/test.js
+++ site/pages/our-work/case-study/bbc/test.js
@@ -3,16 +3,16 @@ import { mount } from 'enzyme';
 
 import { Context } from '../../../../components/link/test-helper';
 import MockedWhatToReadNext from '../shared/test-helper';
-import CaseStudyBank from './index';
+import BBCCaseStudy from './index';
 
 jest.mock('../shared/what-to-read-next', () => MockedWhatToReadNext);
 
-describe('site/pages/our-work/case-study/bank/', () => {
+describe('site/pages/our-work/case-study/bbc/', () => {
   it('renders correctly', () => {
     expect(
       mount(
         <Context>
-          <CaseStudyBank />
+          <BBCCaseStudy />
         </Context>,
       ),
     ).toMatchSnapshot();
