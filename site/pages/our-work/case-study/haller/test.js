ba72a853950431239d32b5a5d091719ff323e32b
Simon Ashbery <simonashbery@Simons-MacBook-Pro.local>
2019-04-01 14:45:15 +0100

    removes loadable-component loadable loader, gosh that is a lot of loading

--- site/pages/our-work/case-study/haller/test.js
+++ site/pages/our-work/case-study/haller/test.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { render } from 'enzyme';
 
 import { Context } from '../../../../components/link/test-helper';
-import MockedWhatToReadNext from '../shared/test-helper';
 import HallerCaseStudy from './index';
 
-jest.mock('../shared/what-to-read-next', () => MockedWhatToReadNext);
+jest.mock(
+  '../shared/what-to-read-next',
+  () => require.requireActual('../shared/test-helper').default,
+);
 
 describe('site/pages/our-work/case-study/haller/', () => {
   it('renders correctly', () => {
