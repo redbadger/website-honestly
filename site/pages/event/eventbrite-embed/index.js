a0d86e959b279c956806233c470358bcff946cc5
Simon Ashbery <simonashbery@Simons-MacBook-Pro.local>
2019-05-08 15:46:47 +0100

    only shows eventbrite button when window has loaded

--- site/pages/event/eventbrite-embed/index.js
+++ site/pages/event/eventbrite-embed/index.js
@@ -1,13 +1,30 @@
 // @no-flow
 import React from 'react';
 
+/* The modal popup will only work on pages served over HTTPS
+   the fallback behaviour will take users to the
+   eventbrite ticket page */
+
 export default class EventbriteEmbed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowLoaded: false,
+    };
+  }
+
   componentDidMount() {
+    // The widget scripts will only work when the window has loaded
+    window.addEventListener('load', () => {
+      this.setState({ windowLoaded: true });
+    });
+
     const { eventbriteId } = this.props;
     const script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = 'https://www.eventbrite.co.uk/static/widgets/eb_widgets.js';
     script.async = true;
+
     const widgetScript = document.createElement('script');
     widgetScript.type = 'text/javascript';
     widgetScript.innerHTML = `window.onload = function() {
@@ -22,22 +39,28 @@ export default class EventbriteEmbed extends React.Component {
           })
       }`;
     widgetScript.async = false;
+
     document.body.appendChild(script);
     document.body.appendChild(widgetScript);
   }
 
   render() {
     const { eventbriteId, url } = this.props;
+    const { windowLoaded } = this.state;
     return (
       <div className="testyTestTest">
-        <noscript>
-          <a href={url} rel="noopener noreferrer" target="_blank">
-            Buy Tickets on Eventbrite
-          </a>
-        </noscript>
-        <button id={`eventbrite-modal-${eventbriteId}`} type="button">
-          Buy Tickets
-        </button>
+        {windowLoaded && (
+          <div>
+            <noscript>
+              <a href={url} rel="noopener noreferrer" target="_blank">
+                Buy Tickets on Eventbrite
+              </a>
+            </noscript>
+            <button id={`eventbrite-modal-${eventbriteId}`} type="button">
+              Buy Tickets
+            </button>
+          </div>
+        )}
       </div>
     );
   }
