7af1ad49f8ae53825ef70363fafe537cc53961bd
Simon Ashbery <simonashbery@Simons-MacBook-Pro.local>
2019-05-13 14:18:49 +0100

    Only adds widgets once the scripts are loaded into the window.

--- site/pages/event/eventbrite-embed/index.js
+++ site/pages/event/eventbrite-embed/index.js
@@ -7,77 +7,57 @@ import styles from './style.css';
    eventbrite ticket page */
 
 export default class EventbriteEmbed extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      windowLoaded: false,
-    };
-  }
-
   componentDidMount() {
     this.addEventbriteScript();
   }
 
-  componentWillUnmount() {
-    window.removeEventListener('load', this.setWindowLoaded.bind(this));
-  }
-
-  setWindowLoaded() {
-    this.setState({ windowLoaded: true });
+  loadWidgets() {
+    const { eventbriteId } = this.props;
+    const widgetScript = document.createElement('script');
+    widgetScript.type = 'text/javascript';
+    widgetScript.innerHTML = `
+      window.EBWidgets.createWidget({
+        widgetType: 'checkout',
+        eventId: '${eventbriteId}',
+        modal: true,
+        modalTriggerElementId: 'eventbrite-modal-${eventbriteId}',
+        onOrderComplete: () => {
+          console.log('Order complete!');
+        },
+      })`;
+    widgetScript.async = false;
+    document.body.appendChild(widgetScript);
   }
 
   addEventbriteScript() {
-    // The widget scripts will only work when the window has loaded
-    this.setWindowLoaded();
-
-    const { eventbriteId } = this.props;
     const script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = 'https://www.eventbrite.co.uk/static/widgets/eb_widgets.js';
     script.async = true;
-
-    const widgetScript = document.createElement('script');
-    widgetScript.type = 'text/javascript';
-    widgetScript.innerHTML = `document.addEventListener('readystatechange', function() {
-      if (document.readyState === 'complete') {
-        window.EBWidgets.createWidget({
-            widgetType: 'checkout',
-            eventId: '${eventbriteId}',
-            modal: true,
-            modalTriggerElementId: 'eventbrite-modal-${eventbriteId}',
-            onOrderComplete: () => {
-              console.log('Order complete!');
-            },
-          })
-        }
-      })`;
-    widgetScript.async = false;
-
+    /* widgets will only work once the eventbrite
+       scripts are loaded into the window object. */
+    script.onload = this.loadWidgets.bind(this);
     document.body.appendChild(script);
-    document.body.appendChild(widgetScript);
   }
 
   render() {
     const { eventbriteId, url } = this.props;
-    const { windowLoaded } = this.state;
     return (
       <div className="eventbrite-modal">
-        {windowLoaded && (
-          <div>
-            <noscript>
-              <a href={url} rel="noopener noreferrer" target="_blank">
-                Get tickets on Eventbrite
-              </a>
-            </noscript>
-            <button
-              className={styles.getTickets__button}
-              id={`eventbrite-modal-${eventbriteId}`}
-              type="button"
-            >
-              Get tickets
-            </button>
-          </div>
-        )}
+        <div>
+          <noscript>
+            <a href={url} rel="noopener noreferrer" target="_blank">
+              Get tickets on Eventbrite
+            </a>
+          </noscript>
+          <button
+            className={styles.getTickets__button}
+            id={`eventbrite-modal-${eventbriteId}`}
+            type="button"
+          >
+            Get tickets
+          </button>
+        </div>
       </div>
     );
   }
