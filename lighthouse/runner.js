1e10ce8286b5afa5cdb267b8b9897409c33b8d93
Sam Rowe <sam.rowe@red-badger.com>
2019-05-21 13:39:18 +0100

    Add lighthouse runner

---
+++ lighthouse/runner.js
@@ -0,0 +1,45 @@
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+const fs = require('fs');
+
+function readArgumentsFromEnvironment() {
+  const { deployment } = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH));
+
+  return {
+    targetUrl: deployment.url,
+  };
+}
+
+function runLighthouse(url) {
+  const chromeFlags = [
+    '--headless',
+    '--no-sandbox', // chrome sandboxing requires docker container to have the
+    // `SYS_ADMIN` capability added which is not supported by GitHub actions
+  ];
+
+  return chromeLauncher.launch({ chromeFlags }).then(chrome => {
+    const opts = {
+      port: chrome.port,
+    };
+
+    const config = null;
+
+    return lighthouse(url, opts, config).then(results => {
+      // use results.lhr for the JS-consumable output
+      // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
+      // use results.report for the HTML/JSON/CSV output as a string
+      // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
+      return chrome.kill().then(() => results.lhr);
+    });
+  });
+}
+
+function run() {
+  const args = readArgumentsFromEnvironment();
+
+  runLighthouse(args).then(results => {
+    console.log(results);
+  });
+}
+
+run();
