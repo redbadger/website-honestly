155dd58391f253b7808342cbaa98ef35aed8a608
Simon Ashbery <simonashbery@Simons-MacBook-Pro.local>
2019-07-26 15:45:47 +0100

    enlargens assets at desktop size

--- site/slices/sharethyme-slice/index.js
+++ site/slices/sharethyme-slice/index.js
@@ -5,8 +5,6 @@ import styles from './style.css';
 import ShareThymeLogo from './sharethyme-logo';
 
 import Image from '../../components/image';
-
-import logoImage from './images/group-21_1x.png';
 import logoX2Image from './images/group-21_2x.png';
 import logoX3Image from './images/group-21_3x.png';
 
@@ -25,7 +23,7 @@ export default function ShareThymeSlice() {
       <Image
         alt="Illustration of hands carrying a pot of food with a love heart on it"
         className={styles.shareThymeImage}
-        src={logoImage}
+        src={logoX2Image}
         src2x={logoX2Image}
         src3x={logoX3Image}
       />
