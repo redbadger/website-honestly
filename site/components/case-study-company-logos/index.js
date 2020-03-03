bd18f8a225b192c0f679b5dcd102be80d23dcfac
Simon Ashbery <si.ashbery@gmail.com>
2020-03-03 10:42:25 +0000

    removes the hsbc logo from the site (#915)

--- site/components/case-study-company-logos/index.js
+++ site/components/case-study-company-logos/index.js
@@ -6,7 +6,6 @@ import React from 'react';
 import bbcPNG from './PNG/bbc.png';
 import pridePNG from './PNG/pride.png';
 import cartrawlerPNG from './PNG/cartrawler.png';
-import hsbcPNG from './PNG/hsbc.png';
 import selfridgesPNG from './PNG/selfridges.png';
 import tescoPNG from './PNG/tesco.png';
 
@@ -28,9 +27,6 @@ const CaseStudyCompanyLogos = () => (
     </div>
     <div className={styles.caseCompaniesLogo}>
       <img alt="The logo of Pride" src={pridePNG} />
-    </div>
-    <div className={styles.caseCompaniesLogo}>
-      <img alt="The logo of HSBC" src={hsbcPNG} />
     </div>
   </div>
 );
