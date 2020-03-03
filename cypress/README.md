c6ca4748b12b11c160c63268cf32fa1a86dce494
Greg <greg.dorward@red-badger.com>
2020-03-03 15:42:10 +0000

    adding readme and removing unused import

---
+++ cypress/README.md
@@ -0,0 +1,11 @@
+# Running Cypress Tests
+
+## To open the Cypress runner
+```
+yarn cypress:open
+```
+
+## To run Cypress tests from the command line
+```
+yarn cypress:run
+```
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,3 @@
-// import "cypress-graphql-mock";
-
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
