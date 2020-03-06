076b9ff26bad111134ca018d70398da152971a90
Greg <greg.dorward@red-badger.com>
2020-03-06 17:08:48 +0000

    formatting

--- site/components/header/index.js
+++ site/components/header/index.js
@@ -18,34 +18,64 @@ const Header = () => {
       <nav className={styles.mediumScreenNavContainer}>
         <ul role="listbox" className={styles.mediumScreenNav}>
           <li className={styles.navItemWithChild}>
-            <Link to="whatWeDoPage" data-link="header" activeCssClass={styles.activeNavLink} data-cy="whatWeDoLink">
+            <Link
+              to="whatWeDoPage"
+              data-link="header"
+              activeCssClass={styles.activeNavLink}
+              data-cy="whatWeDoLink"
+            >
               What we do
             </Link>
             <ul className={styles.mediumScreenChildList}>
               <li>
-                <Link to="experienceUs" data-link="header" activeCssClass={styles.activeNavLink} data-cy="experienceUsLink">
+                <Link
+                  to="experienceUs"
+                  data-link="header"
+                  activeCssClass={styles.activeNavLink}
+                  data-cy="experienceUsLink"
+                >
                   Experience us
                 </Link>
               </li>
               <li>
-                <Link to="technology" data-link="header" activeCssClass={styles.activeNavLink} data-cy="technologyLink">
+                <Link
+                  to="technology"
+                  data-link="header"
+                  activeCssClass={styles.activeNavLink}
+                  data-cy="technologyLink"
+                >
                   Technology
                 </Link>
               </li>
               <li>
-                <Link to="ourWorkPage" data-link="header" activeCssClass={styles.activeNavLink} data-cy="ourWorkLink">
+                <Link
+                  to="ourWorkPage"
+                  data-link="header"
+                  activeCssClass={styles.activeNavLink}
+                  data-cy="ourWorkLink"
+                >
                   Our work
                 </Link>
               </li>
             </ul>
           </li>
           <li className={styles.navItemWithChild}>
-            <Link to="aboutUsPage" data-link="header" activeCssClass={styles.activeNavLink} data-cy="aboutUsLink">
+            <Link
+              to="aboutUsPage"
+              data-link="header"
+              activeCssClass={styles.activeNavLink}
+              data-cy="aboutUsLink"
+            >
               About us
             </Link>
             <ul className={styles.mediumScreenChildList}>
               <li>
-                <Link to="badgers" data-link="header" activeCssClass={styles.activeNavLink} data-cy="meetOurTeamLink">
+                <Link
+                  to="badgers"
+                  data-link="header"
+                  activeCssClass={styles.activeNavLink}
+                  data-cy="meetOurTeamLink"
+                >
                   Our team
                 </Link>
               </li>
@@ -57,12 +87,22 @@ const Header = () => {
             </a>
           </li>
           <li>
-            <Link to="events" data-link="header" activeCssClass={styles.activeNavLink} data-cy="eventsLink">
+            <Link
+              to="events"
+              data-link="header"
+              activeCssClass={styles.activeNavLink}
+              data-cy="eventsLink"
+            >
               Events
             </Link>
           </li>
           <li>
-            <Link to="joinUs" data-link="header" activeCssClass={styles.activeNavLink} data-cy="joinUsLink">
+            <Link
+              to="joinUs"
+              data-link="header"
+              activeCssClass={styles.activeNavLink}
+              data-cy="joinUsLink"
+            >
               Jobs
             </Link>
           </li>
