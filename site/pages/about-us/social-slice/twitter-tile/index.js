f0100f6c938f632f264d4e9638c84b11146d2f92
Simon Ashbery <simon.ashbery@gmail.com>
2020-10-21 12:49:48 +0100

    adds types to text

--- site/pages/about-us/social-slice/twitter-tile/index.js
+++ site/pages/about-us/social-slice/twitter-tile/index.js
@@ -44,7 +44,7 @@ const ensureEscapedText = text => {
   }
 };
 
-const truncate = text =>
+const truncate = (text: string) =>
   text.length > maxTextLengthWithImage ? text.slice(0, maxTextLengthWithImage) + '...' : text;
 
 const colours = [styles.blue, styles.mauve, styles.green];
@@ -76,7 +76,7 @@ const Twitter = ({ tweet, index }: TweetProps) => {
             shrinkTextSize ? styles.tweetWithLargeImage : styles.tweetWithNoImage,
           )}
         >
-          {tweet.image ? truncate(parsedText) : parsedText}
+          {tweet.image && parsedText ? truncate(parsedText) : parsedText}
         </div>
         <div className={styles.meta}>
           <span>
