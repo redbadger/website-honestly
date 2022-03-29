e6d96c798604e6201965ba01e1098f25fe4bfd72
Simon Ashbery <si.ashbery@gmail.com>
2022-03-29 12:23:12 +0100

    Add nats homepage slice (#1026)
    * Adds the NATS slice to the homepage
    
    * corrects missing CSS
    
    * Changes nats slice to more closely match wasm slice
    
    * Remakes the nats slice using individual assets
    
    * corrects divider

--- site/components/nats-slice/index.js
+++ site/components/nats-slice/index.js
@@ -3,30 +3,54 @@ import * as React from 'react';
 
 import styles from './style.css';
 import backgroundImg from './images/background.png';
+import titleImg from './images/title.png';
+import natsLogo from './images/nats-logo.png';
 
 const NatsSlice = () => {
   return (
     <div className={`${styles.NatsSliceWrapper}`} id="genz">
       <div className={styles.NatsSliceContent}>
         <div className={styles.NatsSlice}>
-          <img
-            className={styles.NatsSliceTitleImage}
-            src={backgroundImg}
-            alt="text reading: We Love Tech"
-          />
-
           <div className={styles.NatsSliceText}>
-            <p>
-              Join Red Badger, Synadia, FLEETCOR and Form3 to explore why NATS is the next
-              generation of cloud connectivity for financial blue chips. More than a message broker,
-              NATS is THE connective substrate for a global organisation–or even the whole planet.
-            </p>
-            <a
-              href="https://content.red-badger.com/events/we-love-tech/we-love-nats"
-              className={styles.NatsSliceLink}
-            >
-              Register →
-            </a>
+            <img
+              className={styles.NatsSliceTitleImage}
+              src={titleImg}
+              alt="text reading: We Love Tech"
+            />
+            <div className={styles.NatsSliceTextContent}>
+              <div className={styles.NatsSliceBodyheader}>
+                <h4>We love</h4>
+                <img src={natsLogo} alt="N.A.T.S logo" />
+              </div>
+              <div className={styles.NatsSliceCTA}>
+                <div>
+                  <p>
+                    Wednesday 20th April - 18:30
+                    <br />
+                    Red Badger HQ &amp; Virtual
+                  </p>
+                </div>
+              </div>
+              <p>
+                Join Red Badger, Synadia, FLEETCOR and Form3 to explore why NATS is the next
+                generation of cloud connectivity for financial blue chips. More than a message
+                broker, NATS is THE connective substrate for a global organisation–or even the whole
+                planet.
+              </p>
+              <a
+                href="https://content.red-badger.com/events/we-love-tech/we-love-nats"
+                className={styles.NatsSliceLink}
+              >
+                Register
+              </a>
+            </div>
+          </div>
+          <div className={styles.NatsSliceImageWrapper}>
+            <img
+              className={styles.NatsSliceImage}
+              src={backgroundImg}
+              alt="An illustration of the letters n.a.t.s floating above a cloud"
+            />
           </div>
         </div>
       </div>
