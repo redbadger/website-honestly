e5a1247209a44bb15ebed9f7e2757c9ccfa730ef
Tim Lee <tim.lee@red-badger.com>
2022-04-25 10:30:56 +0100

    Use correct text

--- site/components/wasm-whitepaper-slice/index.js
+++ site/components/wasm-whitepaper-slice/index.js
@@ -17,14 +17,22 @@ const WasmWhitepaperSlice = () => {
               alt="text reading: We Love Tech"
             />
             <div className={styles.WasmWhitepaperSliceTextContent}>
-              <div className={styles.WasmWhitepaperSliceCTA}>
+              {/* <div className={styles.WasmWhitepaperSliceCTA}>
                 <h5>DOWNLOAD THE WHITE PAPER</h5>
-              </div>
+              </div> */}
               <div className={styles.WasmWhitepaperSliceBodyheader}>
-                <h3>Multi-cloud platforms are here:</h3>
+                <h3>
+                  Multi-cloud platforms are here: How WebAssembly, NATS and wasmCloud can move us
+                  beyond the cloud
+                </h3>
               </div>
               <div className={styles.WasmWhitepaperSliceBodySubHeader}>
-                <h4>How WebAssembly, NATS and wasmCloud can move us beyond the cloud</h4>
+                <h4>
+                  With contributions from Liam Randall, Cosmonic’s CEO and Derek Collison, CEO of
+                  Synadia and creator of <a href="http://nats.io/">NATS.io</a>, our latest white
+                  paper written by our Chief Scientist, Stuart Harris, maps out the journey to a
+                  truly resilient multi-cloud platform.
+                </h4>
               </div>
               <div>
                 <a href="https://bit.ly/393vkLn" className={styles.WasmWhitepaperSliceLink}>
