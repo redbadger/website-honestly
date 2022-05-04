9bc7a245b23d277f58735219a91a77149463e0d3
Tim Lee <tim.lee@red-badger.com>
2022-05-04 14:10:40 +0100

    Remove Selfridges logo from home page and our work page

--- site/components/case-study-company-logos/index.js
+++ site/components/case-study-company-logos/index.js
@@ -6,16 +6,12 @@ import React from 'react';
 import bbcPNG from './PNG/bbc.png';
 import nhsPNG from './PNG/nhs.png';
 import cartrawlerPNG from './PNG/cartrawler.png';
-import selfridgesPNG from './PNG/selfridges.png';
 import tescoPNG from './PNG/tesco.png';
 
 import styles from './style.css';
 
 const CaseStudyCompanyLogos = () => (
   <div className={styles.caseCompanies}>
-    <div className={styles.caseCompaniesLogo}>
-      <img alt="The logo of Selfridges" src={selfridgesPNG} />
-    </div>
     <div className={styles.caseCompaniesLogo}>
       <img alt="The logo of Car Trawler" src={cartrawlerPNG} />
     </div>
