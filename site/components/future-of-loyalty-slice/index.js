b02169a8f85986bc3364a65a693294ea777821d3
Tim Lee <tim.lee@red-badger.com>
2022-05-25 12:29:42 +0100

    Add new loyalty event banner (#1033)
    * Add new loyalty event banner
    
    * Add aria-hidden and update snapshot

---
+++ site/components/future-of-loyalty-slice/index.js
@@ -0,0 +1,46 @@
+// @flow
+import * as React from 'react';
+
+import styles from './style.css';
+import titleImg from './images/title.png';
+import loyaltyImage from './images/loyalty-image.png';
+
+const Slice = () => {
+  return (
+    <div className={`${styles.SliceWrapper}`}>
+      <div>
+        <div className={styles.Slice}>
+          <div className={styles.SliceText}>
+            <img
+              className={styles.SliceTitleImage}
+              src={titleImg}
+              alt="text reading: Badger Sessions"
+            />
+
+            <div className={styles.SliceTextContent}>
+              <div className={styles.SliceBodyheader}>
+                <h3>Is the future of digital loyalty point-less?</h3>
+              </div>
+              <div className={styles.SliceBodySubHeader}>
+                <h4>
+                  Join us on Wednesday 6th July at 4pm for a virtual panel discussion to find out
+                  how and why loyalty is evolving
+                </h4>
+              </div>
+              <div>
+                <a href="https://bit.ly/3GbWdcw" className={styles.SliceLink}>
+                  Register now
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className={styles.SliceImageWrapper}>
+            <img className={styles.SliceImage} src={loyaltyImage} aria-hidden="true" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slice;
