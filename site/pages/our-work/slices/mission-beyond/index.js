15cff817c7904b3806ddb5cd0cb266cfab4cd9e6
Sean <sean.gabriel@red-badger.com>
2022-06-06 11:30:06 +0100

    Fix typo on Mission Beyond case study snippet (#1035)
    * Fix typo on Mission Beyond case study snippet
    
    * Update typo in slice
    
    Co-authored-by: Tim Lee <tim.lee@red-badger.com>

--- site/pages/our-work/slices/mission-beyond/index.js
+++ site/pages/our-work/slices/mission-beyond/index.js
@@ -14,7 +14,7 @@ export default function MissionBeyond({ layoutRight }: Props) {
   return (
     <Container to="missionBeyondCaseStudy" layoutRight={layoutRight}>
       <Text layout={layoutRight ? 'rightCol' : 'leftCol'}>
-        <Header>Building a mission-oriented product business from the group up</Header>
+        <Header>Building a mission-oriented product business from the ground up</Header>
         <Description>
           Find out how a Red Badger team set about tackling social inequality by building a digital
           platform to connect employers with a diverse talent pool from poorer socioeconomic
